335dace8b53d4053ce85c379c0b72750f4c778cf
ebaumann <ebaumann@localhost>
2008-10-07 19:22:40 +0200

    [svn] parameter accecptHidden

--- JavaStandardLibrary/src/de/elmar_baumann/lib/io/DirectoryFilter.java
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/io/DirectoryFilter.java
@@ -1,17 +1,33 @@
 package de.elmar_baumann.lib.io;
 
 import java.io.File;
+import javax.swing.filechooser.FileSystemView;
 
 /**
- * Dateifilter, der nur Verzeichnisse akzeptiert.
+ * Accepts only directories.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/07/23
  */
 public class DirectoryFilter implements java.io.FileFilter {
 
+    boolean accecptHidden = true;
+    FileSystemView fsv = FileSystemView.getFileSystemView();
+
+    /**
+     * Constructor.
+     * 
+     * @param accecptHidden  true if accept hidden directories
+     */
+    public DirectoryFilter(boolean accecptHidden) {
+        this.accecptHidden = accecptHidden;
+    }
+
     @Override
     public boolean accept(File file) {
-        return file.isDirectory();
+        boolean isDirectory = file.isDirectory();
+        return accecptHidden
+            ? isDirectory
+            : isDirectory && !fsv.isHiddenFile(file);
     }
 }
