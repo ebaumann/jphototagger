5a72f70189e6433bebe094460f8391309c78fd6d
ebaumann <ebaumann@localhost>
2008-10-09 22:33:43 +0200

    [svn] start

---
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/componentutil/ComponentColors.java
@@ -0,0 +1,36 @@
+package de.elmar_baumann.lib.componentutil;
+
+import java.awt.Color;
+
+/**
+ * Defines colors of some components.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2008/10/09
+ */
+public class ComponentColors {
+
+    public Color defaultBackground;
+    public Color labelBackground;
+    public Color labelForeground;
+    public Color inputBackground;
+    public Color inputForeground;
+    public Color inputSelectedBackground;
+    public Color inputSelectedForeground;
+    public Color inputBorder;
+    public Color buttonBackground;
+    public Color buttonForeground;
+    public Color buttonBorder;
+    public Color editorPaneBackground;
+    public Color editorPaneForeground;
+    public Color editorPaneBorder;
+    public Color menuBarBackground;
+    public Color menuBarForeground;
+    public Color menuBackground;
+    public Color menuForeground;
+    public Color menuSelectedBackground;
+    public Color menuItemBackground;
+    public Color menuItemForeground;
+    public Color menuItemSelectedBackground;
+    public Color tabSelectedBackground;
+}
