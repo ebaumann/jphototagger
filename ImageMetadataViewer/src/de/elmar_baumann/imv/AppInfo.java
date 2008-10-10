aa6e7205bad70e73e1b6ff5cfd313b2933e2d5c3
ebaumann <ebaumann@localhost>
2008-10-10 20:52:23 +0200

    [svn] new distribution, added help page

--- ImageMetadataViewer/src/de/elmar_baumann/imv/AppInfo.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/AppInfo.java
@@ -11,7 +11,7 @@ import de.elmar_baumann.imv.resource.Bundle;
 public class AppInfo {
 
     /** Version der Anwendung */
-    public static final String appVersion = "0.5.13 2008/10/10";  // NOI18N
+    public static final String appVersion = "0.5.14 2008/10/10";  // NOI18N
     /** Name der Anwendung */
     public static final String appName = "Bilder";  // NOI18N
     /** Beschreibung der Anwendung */
