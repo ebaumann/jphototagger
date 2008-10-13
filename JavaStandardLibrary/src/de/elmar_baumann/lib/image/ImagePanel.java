4d17cb07b530cf230ef6564b5bd4aebca4bb76c4
ebaumann <ebaumann@localhost>
2008-10-13 10:56:14 +0200

    [svn] replaced proxy with an image panel

---
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/image/ImagePanel.java
@@ -0,0 +1,44 @@
+package de.elmar_baumann.lib.image;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JPanel;
+
+/**
+ * Displays an image centered whitin it's previous set area. Draws the empty
+ * background if the image is null.
+ * 
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2008/07/20
+ */
+public class ImagePanel extends JPanel {
+
+    private Image image;
+
+    /**
+     * Sets the image.
+     * 
+     * @param image  image, can be null
+     */
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(getBackground());
+        int width = getWidth();
+        int height = getHeight();
+        if (image != null) {
+            int imgHeight = image.getHeight(this);
+            int imgWidth = image.getWidth(this);
+            int x = (imgWidth < width ? (width - imgWidth) / 2 : 0);
+            int y = (imgHeight < height ? (height - imgHeight) / 2 : 0);
+
+            g.fillRect(0, 0, width, height);
+            g.drawImage(image, x, y, this);
+        } else {
+            g.drawRect(0, 0, width, height);
+        }
+    }
+}
