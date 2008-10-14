fcba474bee8f00065645e2239d98477ab402816d
ebaumann <ebaumann@localhost>
2008-10-14 15:33:48 +0200

    [svn] compares filenames if postfixes equal

--- JavaStandardLibrary/src/de/elmar_baumann/lib/util/ComparatorFilesPostfixes.java
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/util/ComparatorFilesPostfixes.java
@@ -21,18 +21,26 @@ public class ComparatorFilesPostfixes implements Comparator<File> {
 
     @Override
     public int compare(File o1, File o2) {
-        String postfix1 = o1.getName();
-        String postfix2 = o2.getName();
-        int index1 = postfix1.lastIndexOf(".");
-        int index2 = postfix2.lastIndexOf(".");
-        postfix1 = index1 < postfix1.length() - 1 ? postfix1.substring(index1 + 1) : "";
-        postfix2 = index2 < postfix2.length() - 1 ? postfix2.substring(index1 + 1) : "";
+        String compare1 = o1.getName();
+        String compare2 = o2.getName();
+        int index1 = compare1.lastIndexOf(".");
+        int index2 = compare2.lastIndexOf(".");
+        compare1 = index1 >= 0 && index1 < compare1.length() - 1 ? compare1.substring(index1 + 1) : "";
+        compare2 = index2 >= 0 && index2 < compare2.length() - 1 ? compare2.substring(index2 + 1) : "";
+        boolean postfixesEquals = compare1.isEmpty() || compare1.isEmpty() ||
+            ccase.equals(CompareCase.Ignore)
+            ? compare1.equalsIgnoreCase(compare2)
+            : compare1.equals(compare2);
+        if (postfixesEquals) {
+            compare1 = o1.getAbsolutePath();
+            compare2 = o2.getAbsolutePath();
+        }
         return order.equals(CompareOrder.Ascending)
             ? ccase.equals(CompareCase.Ignore)
-            ? postfix1.compareToIgnoreCase(postfix2)
-            : postfix1.compareTo(postfix2)
+            ? compare1.compareToIgnoreCase(compare2)
+            : compare1.compareTo(compare2)
             : ccase.equals(CompareCase.Ignore)
-            ? postfix2.compareToIgnoreCase(postfix1)
-            : postfix2.compareTo(postfix1);
+            ? compare2.compareToIgnoreCase(compare1)
+            : compare2.compareTo(compare1);
     }
 }
