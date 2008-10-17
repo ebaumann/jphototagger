76a71e86cc91da694217df3c086f850c34bdbbf6
ebaumann <ebaumann@localhost>
2008-10-17 10:28:55 +0200

    [svn] excluded columns for fast searches and image collections

--- ImageMetadataViewer/src/de/elmar_baumann/imv/database/metadata/selections/AdvancedSearchColumns.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/database/metadata/selections/AdvancedSearchColumns.java
@@ -2,9 +2,11 @@ package de.elmar_baumann.imv.database.metadata.selections;
 
 import de.elmar_baumann.imv.database.metadata.Column;
 import de.elmar_baumann.imv.database.metadata.Table;
+import de.elmar_baumann.imv.database.metadata.collections.ColumnCollectionnamesName;
 import de.elmar_baumann.imv.database.metadata.collections.ColumnCollectionsSequenceNumber;
 import de.elmar_baumann.imv.database.metadata.file.ColumnFilesLastModified;
 import de.elmar_baumann.imv.database.metadata.file.ColumnFilesThumbnail;
+import de.elmar_baumann.imv.database.metadata.savedsearches.ColumnSavedSearchesName;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +27,8 @@ public class AdvancedSearchColumns {
         excludeColumns.add(ColumnFilesLastModified.getInstance());
         excludeColumns.add(ColumnFilesThumbnail.getInstance());
         excludeColumns.add(ColumnCollectionsSequenceNumber.getInstance());
+        excludeColumns.add(ColumnCollectionnamesName.getInstance());
+        excludeColumns.add(ColumnSavedSearchesName.getInstance());
 
         List<Table> tables = AllTables.get();
         for (Table table : tables) {
