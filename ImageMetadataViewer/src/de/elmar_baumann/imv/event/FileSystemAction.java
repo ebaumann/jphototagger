3f6dd641a3222faac3edb0f8d8cf77017fdfe73b
ebaumann <ebaumann@localhost>
2008-10-20 09:57:12 +0200

    [svn] start

---
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/event/FileSystemAction.java
@@ -0,0 +1,15 @@
+package de.elmar_baumann.imv.event;
+
+/**
+ * 
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2008/10/20
+ */
+public enum FileSystemAction {
+
+    Copy,
+    Delete,
+    Move,
+    Rename,
+}
---
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/event/FileSystemError.java
@@ -0,0 +1,13 @@
+package de.elmar_baumann.imv.event;
+
+/**
+ *
+ * @author Elmar Baumann <eb@elmar-baumann.de>
+ */
+public enum FileSystemError {
+
+    MissingPrivileges,
+    ReadOnly,
+    Locked,
+    MoveRenameExists,
+}
