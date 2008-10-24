38ab176285b55bc4332fd62ac60bb95e9c3c3769
ebaumann <ebaumann@localhost>
2008-10-24 09:53:16 +0200

    [svn] renamed

---
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/dnd/ObjectTransferable.java
@@ -0,0 +1,43 @@
+package de.elmar_baumann.lib.dnd;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+/**
+ * Transferable for Objects of an arbitrary type. The data flavor is
+ * <code>java.awt.datatransfer.DataFlavor.strinFlavor</code>.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2008/10/17
+ */
+public class ObjectTransferable implements Transferable {
+
+    private Object data;
+    private DataFlavor[] flavors = new DataFlavor[1];
+    
+    {
+        flavors[0] = DataFlavor.stringFlavor;
+    }
+
+    public ObjectTransferable(Object data) {
+        super();
+        this.data = data;
+    }
+
+    @Override
+    public DataFlavor[] getTransferDataFlavors() {
+        return flavors;
+    }
+
+    @Override
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+        return true;
+    }
+
+    @Override
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        return data;
+    }
+}
