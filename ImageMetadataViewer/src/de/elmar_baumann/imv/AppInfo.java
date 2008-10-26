11c5ae489d0fa56f036b50e624ba79b61b520baf
ebaumann <ebaumann@localhost>
2008-10-26 16:23:27 +0100

    [svn] Bugfix: actionPerformed(), popup.getRotateAngle(): parameter is source instead of action command

--- ImageMetadataViewer/src/de/elmar_baumann/imv/AppInfo.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/AppInfo.java
@@ -11,7 +11,7 @@ import de.elmar_baumann.imv.resource.Bundle;
 public class AppInfo {
 
     /** Version der Anwendung */
-    public static final String appVersion = "0.5.29 2008/10/25";  // NOI18N
+    public static final String appVersion = "0.5.30 2008/10/26";  // NOI18N
     /** Name der Anwendung */
     public static final String appName = "Bilder";  // NOI18N
     /** Beschreibung der Anwendung */
