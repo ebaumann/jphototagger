b77bc1477bcb59d9efadfd30b75f255ab92ee1ce
ebaumann <ebaumann@localhost>
2008-10-26 19:03:10 +0100

    [svn] added support for KDE and Gnome

--- JavaStandardLibrary/src/de/elmar_baumann/lib/datatransfer/TransferableFileList.java
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/datatransfer/TransferableFileList.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 
@@ -14,36 +16,55 @@ import java.util.List;
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/10/17
  */
-public class TransferableFileList  implements Transferable {
- 
-	private static final DataFlavor dataFlavor = DataFlavor.javaFileListFlavor;
-	private static final DataFlavor[] flavors = new DataFlavor[]{dataFlavor};
-	private List fileList;
- 
-	public TransferableFileList(final File[] files) {
-		fileList = Arrays.asList(files);
-	}
-	
+public class TransferableFileList implements Transferable {
+
+    private static final DataFlavor fileListFlavor = DataFlavor.javaFileListFlavor;
+    private static DataFlavor uriListFlavor;
+    private static DataFlavor[] flavors = new DataFlavor[]{fileListFlavor};
+    private List files;
+    private String fileUris;
+    
+
+    static {
+        try {
+            uriListFlavor = new DataFlavor("text/uri-list;class=java.lang.String");
+            flavors = new DataFlavor[]{fileListFlavor, uriListFlavor};
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TransferableFileList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public TransferableFileList(File[] files) {
+        this.files = Arrays.asList(files);
+        createUriList(files);
+    }
+
+    private void createUriList(File[] files) {
+        StringBuffer buffer = new StringBuffer();
+        for (File file : files) {
+            buffer.append("file://" + file.getAbsolutePath() + "\r\n");
+        }
+        fileUris = buffer.toString();
+    }
+
     @Override
-	public DataFlavor[] getTransferDataFlavors() {
-		return(flavors);
-	}
- 
+    public DataFlavor[] getTransferDataFlavors() {
+        return (flavors);
+    }
+
     @Override
-	public boolean isDataFlavorSupported(DataFlavor flavor) {
-		if (flavor.equals(dataFlavor)) {
-			return(true);
-		}
-		return(false);
-	}
- 
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+        return flavor.equals(fileListFlavor) || flavor.equals(uriListFlavor);
+    }
+
     @Override
-	public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
-		if (flavor.equals(dataFlavor)) {
-			return(fileList);
-		} 
-		else {
-			throw new UnsupportedFlavorException(flavor);
-		}
-	}
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        if (flavor.equals(fileListFlavor)) {
+            return files;
+        } else if (flavor.equals(uriListFlavor)) {
+            return fileUris;
+        } else {
+            throw new UnsupportedFlavorException(flavor);
+        }
+    }
 }
