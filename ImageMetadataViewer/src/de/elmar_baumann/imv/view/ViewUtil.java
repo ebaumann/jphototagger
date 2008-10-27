84ab312fe1091e5886df3ea6f485c935e8b427ce
ebaumann <ebaumann@localhost>
2008-10-27 11:27:43 +0100

    [svn] added getSelectedDirectoryFromFavoriteDirectories()

--- ImageMetadataViewer/src/de/elmar_baumann/imv/view/ViewUtil.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/view/ViewUtil.java
@@ -1,7 +1,10 @@
 package de.elmar_baumann.imv.view;
 
+import de.elmar_baumann.imv.data.FavoriteDirectory;
+import de.elmar_baumann.imv.resource.Panels;
 import de.elmar_baumann.lib.io.DirectoryTreeModelFile;
 import java.io.File;
+import javax.swing.JList;
 import javax.swing.JTree;
 import javax.swing.tree.TreePath;
 
@@ -16,10 +19,10 @@ public class ViewUtil {
     /**
      * Returns the selected directory in the directories tree.
      * 
-     * @param  treeDirectories  directories treee
+     * @param  treeDirectories  directories tree
      * @return directory or null if no directory is selected
      */
-    public static File getTargetDirectory(JTree treeDirectories) {
+    public static File getSelectedDirectory(JTree treeDirectories) {
         File directory = null;
         TreePath path = treeDirectories.getSelectionPath();
         if (path != null) {
@@ -29,5 +32,20 @@ public class ViewUtil {
             }
         }
         return directory;
+    }
+
+    /**
+     * Returns the selected directory in the list with favorite directories.
+     * 
+     * @return directory or null if no directory is selected
+     */
+    public static File getSelectedDirectoryFromFavoriteDirectories() {
+        JList list = Panels.getInstance().getAppPanel().getListFavoriteDirectories();
+        Object o = list.getSelectedValue();
+        if (o instanceof FavoriteDirectory) {
+            FavoriteDirectory favoriteDirectory = (FavoriteDirectory) o;
+            return new File(favoriteDirectory.getDirectoryName());
+        }
+        return null;
     }
 }
