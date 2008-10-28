f388a7f616128fb78860a089fd82f77deaebcdcd
ebaumann <ebaumann@localhost>
2008-10-28 09:44:01 +0100

    [svn] start

---
+++ JavaStandardLibrary/src/de/elmar_baumann/lib/component/InputVerifierMaxLength.java
@@ -0,0 +1,66 @@
+package de.elmar_baumann.lib.component;
+
+import de.elmar_baumann.lib.image.icon.IconUtil;
+import de.elmar_baumann.lib.resource.Settings;
+import java.text.MessageFormat;
+import javax.swing.Icon;
+import javax.swing.InputVerifier;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ * Verifies Input in a <code>JTextField</code> or <code>JTextArea</code> against
+ * a maximum allowed length. Shows a message dialog on errors.
+ * 
+ * To use other components, enhance the private method <code>lengthOk()</code>.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2008/10/28
+ */
+public class InputVerifierMaxLength extends InputVerifier {
+
+    int maxLength;
+
+    public InputVerifierMaxLength(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public boolean verify(JComponent input) {
+        boolean lengthOk = lengthOk(input);
+        if (!lengthOk) {
+            errorMessage(input);
+        }
+        return lengthOk;
+    }
+
+    private boolean lengthOk(JComponent input) {
+        if (input instanceof JTextField) {
+            return ((JTextField) input).getText().length() <= maxLength;
+        } else if (input instanceof JTextArea) {
+            return ((JTextArea) input).getText().length() <= maxLength;
+        }
+        return true;
+    }
+
+    private void errorMessage(JComponent input) {
+        MessageFormat msg = new MessageFormat("Bitte geben Sie maximal {0} Zeichen ein!");
+        Object[] params = {maxLength};
+        JOptionPane.showMessageDialog(
+            input,
+            msg.format(params),
+            "Eingabe überprüfen",
+            JOptionPane.ERROR_MESSAGE,
+            getIcon());
+    }
+
+    private Icon getIcon() {
+        Settings settings = Settings.getInstance();
+        if (settings.hasIconImages()) {
+            return IconUtil.getImageIcon(settings.getIconImagesPaths().get(0));
+        }
+        return null;
+    }
+}
