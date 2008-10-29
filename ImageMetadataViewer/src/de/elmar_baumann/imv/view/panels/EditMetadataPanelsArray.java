6361828bc40615c2725ae9402f22a0d276d1409b
ebaumann <ebaumann@localhost>
2008-10-29 21:49:26 +0100

    [svn] setXmpPropertyInfos(): used StringBuffer and set text only when not empty

--- ImageMetadataViewer/src/de/elmar_baumann/imv/view/panels/EditMetadataPanelsArray.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/view/panels/EditMetadataPanelsArray.java
@@ -215,12 +215,15 @@ public class EditMetadataPanelsArray implements FocusListener, DatabaseListener,
             IPTCEntryMeta iptcEntryMeta = mapping.getIptcEntryMetaOfXmpColumn(xmpColumn);
             List<XMPPropertyInfo> matchingInfos =
                 xmpMetadata.getFilteredPropertyInfosOfIptcEntryMeta(iptcEntryMeta, infos);
-            int countMatchingInfos = matchingInfos.size();
 
+            int countMatchingInfos = matchingInfos.size();
+            StringBuffer buffer = new StringBuffer();
             for (int i = 0; i < countMatchingInfos; i++) {
-                textEntry.setText(textEntry.getText() +
-                    (i > 0 ? XmpMetadata.getArrayItemDelimiter() : "") + // NOI18N
+                buffer.append((i > 0 ? XmpMetadata.getArrayItemDelimiter() : "") + // NOI18N
                     matchingInfos.get(i).getValue().toString().trim());
+            }
+            if (buffer.length() > 0) {
+                textEntry.setText(buffer.toString());
             }
         }
     }
