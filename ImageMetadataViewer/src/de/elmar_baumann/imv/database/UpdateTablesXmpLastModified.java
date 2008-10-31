445409daf7f0b910496281a46ceda0d23fd54ab4
ebaumann <ebaumann@localhost>
2008-10-31 17:43:37 +0100

    [svn] package class access instead of public

--- ImageMetadataViewer/src/de/elmar_baumann/imv/database/UpdateTablesXmpLastModified.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/database/UpdateTablesXmpLastModified.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/10/29
  */
-public class UpdateTablesXmpLastModified {
+class UpdateTablesXmpLastModified {
 
     private UpdateTablesMessages messages = UpdateTablesMessages.getInstance();
     private ProgressDialog dialog = messages.getProgressDialog();
