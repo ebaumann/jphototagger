6961937311a61a3df764a09e126ab66bf354fd6d
ebaumann <ebaumann@localhost>
2009-01-23 18:21:26 +0100

    [svn] Changed Comparator

--- ImageMetadataViewer/src/de/elmar_baumann/imv/io/FileSort.java
+++ ImageMetadataViewer/src/de/elmar_baumann/imv/io/FileSort.java
@@ -2,7 +2,7 @@ package de.elmar_baumann.imv.io;
 
 import de.elmar_baumann.lib.util.ComparatorFilesLastModified;
 import de.elmar_baumann.lib.util.ComparatorFilesNames;
-import de.elmar_baumann.lib.util.ComparatorFilesPostfixes;
+import de.elmar_baumann.lib.util.ComparatorFilesSuffixes;
 import java.io.File;
 import java.util.Comparator;
 
@@ -15,8 +15,8 @@ public enum FileSort {
 
     NamesAscending(ComparatorFilesNames.COMPARE_ASCENDING_IGNORE_CASE),
     NamesDescending(ComparatorFilesNames.COMPARE_DESCENDING_IGNORE_CASE),
-    TypesAscending(ComparatorFilesPostfixes.COMPARE_ASCENDING_IGNORE_CASE),
-    TypesDescending(ComparatorFilesPostfixes.COMPARE_DESCENDING_IGNORE_CASE),
+    TypesAscending(ComparatorFilesSuffixes.COMPARE_ASCENDING_IGNORE_CASE),
+    TypesDescending(ComparatorFilesSuffixes.COMPARE_DESCENDING_IGNORE_CASE),
     LastModifiedAscending(ComparatorFilesLastModified.COMPARE_ASCENDING),
     LastModifiedDescending(ComparatorFilesLastModified.COMPARE_DESCENDING);
     private final Comparator<File> comparator;
