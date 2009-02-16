c44404e05d93300b7a062111d02607e080e2c960
ebaumann <ebaumann@localhost>
2009-02-16 14:08:19 +0100

    [svn] added version info 0.5.47 2009/02/16

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/AppInfo.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/AppInfo.java
@@ -11,7 +11,7 @@ import de.elmar_baumann.imv.resource.Bundle;
 public final class AppInfo {
 
     /** Version der Anwendung */
-    public static final String appVersion = "0.5.46 2008/11/11";  // NOI18N
+    public static final String appVersion = "0.5.47 2009/02/16";  // NOI18N
     /** Name der Anwendung */
     public static final String appName = "Bilder";  // NOI18N
     /** Beschreibung der Anwendung */
