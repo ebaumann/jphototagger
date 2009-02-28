0c31ba69506371841ebc4b9fc092dcfb76d1511d
ebaumann <ebaumann@localhost>
2009-02-28 20:46:39 +0100

    [svn] version information: 0.5.53 2009/02/28

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/AppInfo.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/AppInfo.java
@@ -12,7 +12,7 @@ public final class AppInfo {
 
     private static final String projectName = "ImageMetaDataViewer"; // NOI18N NEVER CHANGE!
     /** Version der Anwendung */
-    public static final String appVersion = "0.5.52 2009/02/26";  // NOI18N
+    public static final String appVersion = "0.5.53 2009/02/28";  // NOI18N
     /** Name der Anwendung */
     public static final String appName = "Bilder";  // NOI18N
     /** Beschreibung der Anwendung */
