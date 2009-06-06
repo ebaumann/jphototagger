3ba5bf8a230927f7d6e811a5185e12613a69a47c
ebaumann <ebaumann@localhost>
2009-06-06 22:30:27 +0200

    [svn] setting label text from the column parameter instead of a map (less error prone)

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/view/renderer/RendererTableColumn.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/view/renderer/RendererTableColumn.java
@@ -1,10 +1,7 @@
 package de.elmar_baumann.imv.view.renderer;
 
 import de.elmar_baumann.imv.database.metadata.Column;
-import de.elmar_baumann.imv.database.metadata.selections.DatabaseInfoRecordCountColumns;
 import de.elmar_baumann.imv.database.metadata.selections.TableIcons;
-import java.util.HashMap;
-import java.util.Map;
 import javax.swing.JLabel;
 
 /**
@@ -15,18 +12,6 @@ import javax.swing.JLabel;
  */
 public final class RendererTableColumn {
 
-    private static final Map<Column, String> textOfColumn = new HashMap<Column, String>();
-
-    static {
-        for (Column column : DatabaseInfoRecordCountColumns.get()) {
-            String description = column.getDescription();
-            if (!column.isPrimaryKey() && !column.isForeignKey() &&
-                !description.isEmpty()) {
-                textOfColumn.put(column, column.getDescription());
-            }
-        }
-    }
-
     /**
      * Setzt Icon und Text eines Labels, das eine Tabellenspalte darstellt.
      * 
@@ -35,7 +20,7 @@ public final class RendererTableColumn {
      */
     public static void setLabelText(JLabel label, Column column) {
         label.setIcon(TableIcons.getIcon(column.getTable()));
-        label.setText(textOfColumn.get(column));
+        label.setText(column.getDescription());
     }
 
     protected RendererTableColumn() {
