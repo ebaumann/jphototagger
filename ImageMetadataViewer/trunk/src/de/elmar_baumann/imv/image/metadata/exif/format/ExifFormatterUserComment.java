6dd4abd25795db4be44296fdf6ee6f658a1f1c85
ebaumann <ebaumann@localhost>
2009-06-10 12:13:39 +0200

    [svn] moved ExifUserComment into ExifFormatterUserComment

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/image/metadata/exif/format/ExifFormatterUserComment.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/image/metadata/exif/format/ExifFormatterUserComment.java
@@ -1,8 +1,9 @@
 package de.elmar_baumann.imv.image.metadata.exif.format;
 
 import de.elmar_baumann.imv.image.metadata.exif.ExifTag;
-import de.elmar_baumann.imv.image.metadata.exif.ExifUserComment;
 import de.elmar_baumann.imv.image.metadata.exif.IdfEntryProxy;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 /**
  * Formats an EXIF entry of the type {@link ExifTag# }.
@@ -14,6 +15,19 @@ public final class ExifFormatterUserComment extends ExifFormatter {
 
     public static final ExifFormatterUserComment INSTANCE =
             new ExifFormatterUserComment();
+    private static final byte[] CODE_ASCII = {0x41, 0x53, 0x43, 0x49, 0x49, 0x00,
+        0x00, 0x00};
+    private static final byte[] CODE_JIS = {0x4A, 0x49, 0x53, 0x00, 0x00, 0x00,
+        0x00, 0x00};
+    private static final byte[] CODE_UNICODE = {0x55, 0x4E, 0x49, 0x43, 0x4F,
+        0x44, 0x45, 0x00};
+    private static final byte[] CODE_UNDEFINED = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0};
+
+    private enum CharCode {
+
+        ASCII, JIS, UNICODE, UNDEFINED, UNREGOGNIZED
+    }
 
     private ExifFormatterUserComment() {
     }
@@ -22,6 +36,26 @@ public final class ExifFormatterUserComment extends ExifFormatter {
     public String format(IdfEntryProxy entry) {
         if (entry.getTag() != ExifTag.USER_COMMENT.getId())
             throw new IllegalArgumentException("Wrong tag: " + entry);
-        return ExifUserComment.decode(entry.getRawValue());
+        byte[] rawValue = entry.getRawValue();
+        if (rawValue.length <= 8) return "";
+        CharCode charCode = getEncoding(rawValue);
+        byte[] rawComment = Arrays.copyOfRange(rawValue, 8, rawValue.length);
+        if (charCode.equals(CharCode.ASCII))
+            return new String(rawComment, Charset.forName("US-ASCII"));
+        if (charCode.equals(CharCode.JIS))
+            return new String(rawComment, Charset.forName("JISAutoDetect"));
+        if (charCode.equals(CharCode.UNICODE))
+            return new String(rawComment, Charset.forName("UTF-8"));
+        return "";
+    }
+
+    private static CharCode getEncoding(byte[] rawValue) {
+        assert rawValue.length >= 8 : rawValue.length;
+        byte[] code = Arrays.copyOf(rawValue, 8);
+        if (Arrays.equals(code, CODE_ASCII)) return CharCode.ASCII;
+        if (Arrays.equals(code, CODE_JIS)) return CharCode.JIS;
+        if (Arrays.equals(code, CODE_UNICODE)) return CharCode.UNICODE;
+        if (Arrays.equals(code, CODE_UNDEFINED)) return CharCode.UNDEFINED;
+        return CharCode.UNREGOGNIZED;
     }
 }
