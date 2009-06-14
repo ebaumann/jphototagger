c8845873272757881492b30ff5546969b8300250
ebaumann <ebaumann@localhost>
2009-06-14 22:17:08 +0200

    [svn] * bugfix: after deleting not existing images from db null pointer because of missing icon information * when dialog gets visible removing completed icons

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/view/panels/DatabaseMaintainancePanel.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/view/panels/DatabaseMaintainancePanel.java
@@ -2,6 +2,7 @@ package de.elmar_baumann.imv.view.panels;
 
 import de.elmar_baumann.imv.app.AppIcons;
 import de.elmar_baumann.imv.UserSettings;
+import de.elmar_baumann.imv.database.DatabaseImageFiles;
 import de.elmar_baumann.imv.event.ProgressEvent;
 import de.elmar_baumann.imv.event.ProgressListener;
 import de.elmar_baumann.imv.resource.Bundle;
@@ -25,21 +26,22 @@ public final class DatabaseMaintainancePanel extends javax.swing.JPanel
     private static final Icon iconFinished = AppIcons.getIcon(
             "icon_finished.png"); // NOI18N
     private final Stack<Runnable> runnables = new Stack<Runnable>();
-    private final Map<Runnable, JLabel> finishedLabelOfRunnable =
-            new HashMap<Runnable, JLabel>();
-    private boolean stop = false;
-    private boolean canClose = true;
+    private final Map<Class, JLabel> finishedLabelOfRunnable =
+            new HashMap<Class, JLabel>();
+    private volatile boolean stop = false;
+    private volatile boolean canClose = true;
 
     /** Creates new form DatabaseMaintainancePanel */
     public DatabaseMaintainancePanel() {
         initComponents();
-        setEnabledButtonStartMaintain();
+        postInitComponents();
     }
 
-    private void removeFinishedIcons() {
-        for (JLabel label : finishedLabelOfRunnable.values()) {
-            label.setIcon(null);
-        }
+    private void postInitComponents() {
+        finishedLabelOfRunnable.put(DatabaseCompress.class,
+                labelFinishedCompressDatabase);
+        finishedLabelOfRunnable.put(DatabaseImageFiles.class,
+                labelFinishedDeleteRecordsOfNotExistingFilesInDatabase);
     }
 
     private void setProgressbarStart(ProgressEvent evt) {
@@ -59,8 +61,15 @@ public final class DatabaseMaintainancePanel extends javax.swing.JPanel
 
     public void getsVisible(boolean visible) {
         if (visible) {
+            resetIcons();
             setEnabledButtonStartMaintain();
         }
+    }
+
+    private void resetIcons() {
+        labelFinishedCompressDatabase.setIcon(null);
+        labelFinishedDeleteRecordsOfNotExistingFilesInDatabase.setIcon(null);
+        progressBar.setValue(0);
     }
 
     private void setEnabledButtonStartMaintain() {
@@ -94,7 +103,6 @@ public final class DatabaseMaintainancePanel extends javax.swing.JPanel
     }
 
     private void startMaintain() {
-        removeFinishedIcons();
         addRunnables();
         setRunnablesAreRunning(true);
         stop = false;
@@ -107,16 +115,12 @@ public final class DatabaseMaintainancePanel extends javax.swing.JPanel
         if (checkBoxCompressDatabase.isSelected()) {
             DatabaseCompress databaseCompress = new DatabaseCompress();
             databaseCompress.addProgressListener(this);
-            finishedLabelOfRunnable.put(databaseCompress,
-                    labelFinishedCompressDatabase);
             runnables.push(databaseCompress);
         }
         if (checkBoxDeleteRecordsOfNotExistingFilesInDatabase.isSelected()) {
             RecordsWithNotExistingFilesDeleter deleter =
                     new RecordsWithNotExistingFilesDeleter();
             deleter.addProgressListener(this);
-            finishedLabelOfRunnable.put(deleter,
-                    labelFinishedDeleteRecordsOfNotExistingFilesInDatabase);
             runnables.push(deleter);
         }
     }
@@ -147,7 +151,13 @@ public final class DatabaseMaintainancePanel extends javax.swing.JPanel
     public void progressEnded(ProgressEvent evt) {
         setProgressbarEnd(evt);
         labelMessage.setText(evt.getInfo().toString());
-        finishedLabelOfRunnable.get(evt.getSource()).setIcon(iconFinished);
+        Object source = evt.getSource();
+        assert source != null;
+        Class sourceClass = source.getClass();
+        JLabel labelFinished = finishedLabelOfRunnable.get(sourceClass);
+        if (labelFinished != null) {
+            labelFinished.setIcon(iconFinished);
+        }
         if (runnables.size() > 0) {
             startNextThread();
         } else {
@@ -159,6 +169,7 @@ public final class DatabaseMaintainancePanel extends javax.swing.JPanel
         stop = true;
         synchronized (runnables) {
             runnables.clear();
+            setRunnablesAreRunning(false);
         }
     }
 
