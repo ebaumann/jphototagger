d549d9e07caa9abbceee587909cd71b391354c89
ebaumann <ebaumann@localhost>
2009-06-16 13:49:46 +0200

    [svn] updated comment

--- JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/io/ExecutableFileChooserFileFilter.java
+++ JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/io/ExecutableFileChooserFileFilter.java
@@ -7,7 +7,7 @@ import javax.swing.filechooser.FileFilter;
 /**
  * BUG: Does not work.
  *
- * @author  Elmar Baumann <ebaumann@feitsch.de>
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009/04/01
  */
 public final class ExecutableFileChooserFileFilter extends FileFilter {
