1b2652b4b4fc45bdb2bab97f6e8ae38e03faade9
ebaumann <ebaumann@localhost>
2009-06-17 10:05:01 +0200

    [svn] action performs only on specific content of the thumbnails panel

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/filesystem/ControllerDeleteFiles.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/filesystem/ControllerDeleteFiles.java
@@ -5,6 +5,7 @@ import de.elmar_baumann.imv.database.DatabaseImageFiles;
 import de.elmar_baumann.imv.resource.Bundle;
 import de.elmar_baumann.imv.resource.GUI;
 import de.elmar_baumann.imv.tasks.FileSystemDeleteImageFiles;
+import de.elmar_baumann.imv.types.Content;
 import de.elmar_baumann.imv.types.DeleteOption;
 import de.elmar_baumann.imv.view.panels.ImageFileThumbnailsPanel;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuPanelThumbnails;
@@ -12,6 +13,7 @@ import de.elmar_baumann.lib.io.FileUtil;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -23,9 +25,25 @@ import java.util.List;
  */
 public final class ControllerDeleteFiles implements ActionListener {
 
-    private final ImageFileThumbnailsPanel thumbnailsPanel = GUI.INSTANCE.getAppPanel().getPanelThumbnails();
+    private final ImageFileThumbnailsPanel thumbnailsPanel = GUI.INSTANCE.
+            getAppPanel().getPanelThumbnails();
     private final DatabaseImageFiles db = DatabaseImageFiles.INSTANCE;
-    private final PopupMenuPanelThumbnails popupMenu = PopupMenuPanelThumbnails.INSTANCE;
+    private final PopupMenuPanelThumbnails popupMenu =
+            PopupMenuPanelThumbnails.INSTANCE;
+    private static final List<Content> deleteableContent =
+            new ArrayList<Content>();
+
+
+    static {
+        deleteableContent.add(Content.CATEGORY);
+        deleteableContent.add(Content.DIRECTORY);
+        deleteableContent.add(Content.FAST_SEARCH);
+        deleteableContent.add(Content.FAVORITE_DIRECTORY);
+        deleteableContent.add(Content.KEYWORD);
+        deleteableContent.add(Content.MISC_METADATA);
+        deleteableContent.add(Content.SAFED_SEARCH);
+        deleteableContent.add(Content.TIMELINE);
+    }
 
     public ControllerDeleteFiles() {
         listen();
@@ -38,21 +56,32 @@ public final class ControllerDeleteFiles implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        deleteSelectedFiles();
-        thumbnailsPanel.repaint();
+        if (isDeletable(thumbnailsPanel.getContent())) {
+            deleteSelectedFiles();
+            thumbnailsPanel.repaint();
+        }
+    }
+
+    private boolean isDeletable(Content content) {
+        return deleteableContent.contains(content);
     }
 
     private void deleteSelectedFiles() {
+        int selCount = thumbnailsPanel.getSelectionCount();
         List<File> deletedImageFiles = FileSystemDeleteImageFiles.delete(
-            thumbnailsPanel.getSelectedFiles(), EnumSet.of(
-            DeleteOption.CONFIRM_DELETE,
-            DeleteOption.MESSAGES_ON_FAILURES));
+                thumbnailsPanel.getSelectedFiles(), EnumSet.of(
+                DeleteOption.CONFIRM_DELETE,
+                DeleteOption.MESSAGES_ON_FAILURES));
+        int delCount = deletedImageFiles.size();
 
-        if (deletedImageFiles.size() > 0) {
+        if (delCount > 0) {
             db.deleteImageFiles(FileUtil.getAsFilenames(deletedImageFiles));
             thumbnailsPanel.remove(deletedImageFiles);
-        } else {
-            AppLog.logWarning(ControllerDeleteFiles.class, Bundle.getString("ControllerDeleteFiles.ErrorMessage.NoImagesDeleted"));
+        }
+        if (delCount != selCount) {
+            AppLog.logWarning(ControllerDeleteFiles.class, Bundle.getString(
+                    "ControllerDeleteFiles.ErrorMessage.NotAllImagesDeleted",
+                    selCount, delCount));
         }
     }
 }
