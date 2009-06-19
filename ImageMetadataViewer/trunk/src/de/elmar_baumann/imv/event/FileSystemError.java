b1099df415b51797043402882e979cf6cafbecf8
ebaumann <ebaumann@localhost>
2009-06-19 17:44:54 +0200

    [svn] localized messages of FileSystemError

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/FileSystemError.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/FileSystemError.java
@@ -1,23 +1,24 @@
 package de.elmar_baumann.imv.event;
 
+import de.elmar_baumann.imv.resource.Bundle;
+
 /**
  *
  * @author Elmar Baumann <eb@elmar-baumann.de>
  */
 public enum FileSystemError {
 
-    LOCKED("Die Datei ist gesperrt."),
-    MISSING_PRIVILEGES("Es fehlen die erforderlichen Rechte."),
-    MOVE_RENAME_EXISTS("Die Zieldatei existiert bereits."),
-    READ_ONLY("Die Datei kann nur gelesen werden"),
-    UNKNOWN("Die Usache ist unbekannt.");
-    
+    LOCKED(Bundle.getString("FileSystemError.LOCKED")),
+    MISSING_PRIVILEGES(Bundle.getString("FileSystemError.MISSING_PRIVILEGES")),
+    MOVE_RENAME_EXISTS(Bundle.getString("FileSystemError.MOVE_RENAME_EXISTS")),
+    READ_ONLY(Bundle.getString("FileSystemError.READ_ONLY")),
+    UNKNOWN(Bundle.getString("FileSystemError.UNKNOWN"));
     private final String message;
-    
+
     private FileSystemError(String message) {
         this.message = message;
     }
-    
+
     public String getLocalizedMessage() {
         return message;
     }
