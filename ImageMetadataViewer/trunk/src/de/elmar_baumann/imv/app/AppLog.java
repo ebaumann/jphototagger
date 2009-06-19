186e7f14a837376dd894327d945ee355ee249f55
ebaumann <ebaumann@localhost>
2009-06-19 18:32:53 +0200

    [svn] moved listener interfaces from de.elmar_baumann.imv.event to de.elmar_baumann.imv.event.listener

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/AppLog.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/AppLog.java
@@ -2,7 +2,7 @@ package de.elmar_baumann.imv.app;
 
 import de.elmar_baumann.imv.UserSettings;
 import de.elmar_baumann.imv.event.ErrorEvent;
-import de.elmar_baumann.imv.event.listener.ErrorListeners;
+import de.elmar_baumann.imv.event.listener.impl.ErrorListeners;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
