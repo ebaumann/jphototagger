e5f9ad22903a2e86aef4b7ed0105ea505f480479
ebaumann <ebaumann@localhost>
2009-06-25 13:22:20 +0200

    [svn] added embedTableCellTextInHtml()

--- JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/componentutil/TableUtil.java
+++ JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/componentutil/TableUtil.java
@@ -1,6 +1,9 @@
 package de.elmar_baumann.lib.componentutil;
 
+import de.elmar_baumann.lib.util.StringUtil;
 import java.awt.Component;
+import java.util.List;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
@@ -17,6 +20,48 @@ import javax.swing.table.TableModel;
  * @version 2008/02/17
  */
 public final class TableUtil {
+
+    private static final String HTML_LINE_BREAK = "<br />";
+
+    /**
+     * Embeds text into HTML, breaks the text into multiple lines if necessary
+     * and sets it to the label. Sets the heigt of the label's row to fit it's
+     * content.
+     *
+     * @param table           the label's table
+     * @param row             the label's row
+     * @param label           label, gets the HTML formatted text
+     * @param text            text to set
+     * @param maxCharsPerLine count of maximum characters per line
+     * @param style           a CSS style, e.g.
+     *                        <code>"padding-left:5px;padding-right:3px;"</code>
+     */
+    public static void embedTableCellTextInHtml(JTable table, int row,
+            JLabel label, String text, int maxCharsPerLine, String style) {
+        if (table == null)
+            throw new NullPointerException("table == null");
+        if (label == null)
+            throw new NullPointerException("label == null");
+
+        List<String> lines = StringUtil.wrapWords(text, maxCharsPerLine);
+        StringBuilder sb =
+                new StringBuilder(
+                "<html><style type=\"text/css\">body { " + style +
+                " }</style></head><body>");
+        int lineCount = lines.size();
+        for (int i = 0; i < lineCount; i++) {
+            sb.append(i == 0
+                      ? lines.get(i)
+                      : HTML_LINE_BREAK + lines.get(i));
+        }
+        sb.append("</body></html>");
+        System.out.println(sb.toString());
+        label.setText(sb.toString());
+        int preferredHeight = label.getPreferredSize().height;
+        if (preferredHeight > table.getRowHeight(row)) {
+            table.setRowHeight(row, preferredHeight);
+        }
+    }
 
     /**
      * Setzt die Spaltenbreiten so, dass auch die größte Zelle vollständig
@@ -44,8 +89,9 @@ public final class TableUtil {
                     continue;
                 }
                 Component cell = table.getDefaultRenderer(
-                    model.getColumnClass(colIndex)).getTableCellRendererComponent(
-                    table, value, false, false, rowIndex, colIndex);
+                        model.getColumnClass(colIndex)).
+                        getTableCellRendererComponent(
+                        table, value, false, false, rowIndex, colIndex);
 
                 int width = cell.getPreferredSize().width;
 
