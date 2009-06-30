1f9a7a5bbb004ae5dbcb1a625b92aa6719779c6f
ebaumann <ebaumann@localhost>
2009-06-30 10:24:08 +0200

    [svn] Hints what to insert into the database

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/data/ImageFile.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/data/ImageFile.java
@@ -1,7 +1,10 @@
 package de.elmar_baumann.imv.data;
 
+import de.elmar_baumann.imv.tasks.InsertImageFilesIntoDatabase;
 import java.awt.Image;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Daten einer Bilddatei.
@@ -17,6 +20,8 @@ public final class ImageFile {
     private Iptc iptc;
     private Xmp xmp;
     private Exif exif;
+    private Set<InsertImageFilesIntoDatabase.Insert> insertIntoDb =
+            new HashSet<InsertImageFilesIntoDatabase.Insert>();
 
     /**
      * Liefert den Namen der Bilddatei.
@@ -26,9 +31,9 @@ public final class ImageFile {
     public String getFilename() {
         return filename;
     }
-    
+
     public File getFile() {
-        return new File (filename);
+        return new File(filename);
     }
 
     /**
@@ -131,16 +136,51 @@ public final class ImageFile {
     }
 
     /**
+     * Returns wheter a specific metadadata is to insert into the database.
+     *
+     * @param  insert Is that to insert?
+     * @return        true if <code>insert</code> is to insert into the database
+     *                (that means at least one call was made to
+     *                {@link #addInsertIntoDb(de.elmar_baumann.imv.tasks.InsertImageFilesIntoDatabase.Insert)})
+     *
+     */
+    public boolean isInsertIntoDb(InsertImageFilesIntoDatabase.Insert insert) {
+        return insertIntoDb.contains(insert);
+    }
+
+    public boolean isInsertXmpIntoDb() {
+        return insertIntoDb.contains(InsertImageFilesIntoDatabase.Insert.XMP);
+    }
+
+    public boolean isInsertExifIntoDb() {
+        return insertIntoDb.contains(InsertImageFilesIntoDatabase.Insert.EXIF);
+    }
+
+    public boolean isInsertThumbnailIntoDb() {
+        return insertIntoDb.contains(
+                InsertImageFilesIntoDatabase.Insert.THUMBNAIL);
+    }
+
+    /**
+     * Adds metadata to add into the database.
+     *
+     * @param insert metadata to insert
+     */
+    public void addInsertIntoDb(InsertImageFilesIntoDatabase.Insert insert) {
+        insertIntoDb.add(insert);
+    }
+
+    /**
      * Liefert, ob keine Daten vorhanden sind.
      * 
      * @return true, wenn leer
      */
     public boolean isEmpty() {
         return filename == null &&
-            lastmodified == -1 &&
-            thumbnail == null &&
-            iptc == null &&
-            xmp == null &&
-            exif == null;
+                lastmodified == -1 &&
+                thumbnail == null &&
+                iptc == null &&
+                xmp == null &&
+                exif == null;
     }
 }
