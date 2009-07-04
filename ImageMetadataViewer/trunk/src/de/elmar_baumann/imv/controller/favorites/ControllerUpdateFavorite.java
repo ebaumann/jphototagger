77b1a8f4f0cc71b5034b5410b0498d5d02b42bbf
ebaumann <ebaumann@localhost>
2009-07-04 20:46:37 +0200

    [svn] keys Ctrl+R triggers edit action in favorite directorie's tree

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/favorites/ControllerUpdateFavorite.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/favorites/ControllerUpdateFavorite.java
@@ -6,22 +6,32 @@ import de.elmar_baumann.imv.resource.GUI;
 import de.elmar_baumann.imv.view.dialogs.FavoriteDirectoryPropertiesDialog;
 import de.elmar_baumann.imv.view.panels.AppPanel;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuFavorites;
+import de.elmar_baumann.lib.event.util.KeyEventUtil;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JTree;
 import javax.swing.SwingUtilities;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
- * Listens to the {@link PopupMenuFavorites} and renames the
- * the selected favorite directory or set's a different directory when the
+ * Listens to the {@link PopupMenuFavorites} and let's edit the selected
+ * favorite directory: Rename or set's a different directory when the
  * special menu item was clicked.
+ *
+ * Also listens to the {@link JTree}'s key events and let's edit the selected
+ * file favorite directory if the keys <code>Strg+E</code> were pressed.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/09/23
  */
-public final class ControllerUpdateFavorite implements ActionListener {
+public final class ControllerUpdateFavorite
+        implements ActionListener, KeyListener {
 
     private final PopupMenuFavorites popupMenu = PopupMenuFavorites.INSTANCE;
     private final AppPanel appPanel = GUI.INSTANCE.getAppPanel();
+    private final JTree tree = appPanel.getTreeFavorites();
 
     public ControllerUpdateFavorite() {
         listen();
@@ -29,15 +39,29 @@ public final class ControllerUpdateFavorite implements ActionListener {
 
     private void listen() {
         popupMenu.getItemUpdateFavorite().addActionListener(this);
+        tree.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (KeyEventUtil.isControl(e, KeyEvent.VK_E) && !tree.isSelectionEmpty()) {
+            Object node = tree.getSelectionPath().getLastPathComponent();
+            if (node instanceof DefaultMutableTreeNode) {
+                Object userObject =
+                        ((DefaultMutableTreeNode) node).getUserObject();
+                if (userObject instanceof FavoriteDirectory) {
+                    updateFavorite((FavoriteDirectory) userObject);
+                }
+            }
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        updateFavorite();
+        updateFavorite(popupMenu.getFavoriteDirectory());
     }
 
-    private void updateFavorite() {
-        final FavoriteDirectory favorite = popupMenu.getFavoriteDirectory();
+    private void updateFavorite(final FavoriteDirectory favorite) {
         FavoriteDirectoryPropertiesDialog dialog =
                 new FavoriteDirectoryPropertiesDialog();
         dialog.setFavoriteName(favorite.getFavoriteName());
@@ -51,8 +75,7 @@ public final class ControllerUpdateFavorite implements ActionListener {
                 @Override
                 public void run() {
                     TreeModelFavorites model =
-                            (TreeModelFavorites) appPanel.
-                            getTreeFavorites().
+                            (TreeModelFavorites) appPanel.getTreeFavorites().
                             getModel();
                     model.replaceFavorite(favorite, new FavoriteDirectory(
                             favoriteName,
@@ -61,5 +84,15 @@ public final class ControllerUpdateFavorite implements ActionListener {
                 }
             });
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
