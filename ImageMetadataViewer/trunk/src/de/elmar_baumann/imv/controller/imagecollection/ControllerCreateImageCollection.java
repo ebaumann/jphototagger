7a94b366eaf851c70026e48ea11f96b412c25e51
ebaumann <ebaumann@localhost>
2009-07-04 21:05:33 +0200

    [svn] keys Ctrl+N triggers create action in image collection list

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/imagecollection/ControllerCreateImageCollection.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/imagecollection/ControllerCreateImageCollection.java
@@ -10,27 +10,37 @@ import de.elmar_baumann.imv.view.panels.AppPanel;
 import de.elmar_baumann.imv.view.panels.ImageFileThumbnailsPanel;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuThumbnails;
 import de.elmar_baumann.lib.componentutil.ListUtil;
+import de.elmar_baumann.lib.event.util.KeyEventUtil;
 import de.elmar_baumann.lib.io.FileUtil;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JList;
+import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 
 /**
  * Kontrolliert Aktion: Erzeuge eine Bildsammlung, ausgelöst von
  * {@link de.elmar_baumann.imv.view.popupmenus.PopupMenuThumbnails}.
+ *
+ * Also listens to the {@link JTree}'s key events and creates a new image
+ * when the keys <code>Ctrl+N</code> were pressed.
  * 
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/09/10
  */
-public final class ControllerCreateImageCollection implements ActionListener {
+public final class ControllerCreateImageCollection
+        implements ActionListener, KeyListener {
 
     private final PopupMenuThumbnails popupMenu =
             PopupMenuThumbnails.INSTANCE;
     private final AppPanel appPanel = GUI.INSTANCE.getAppPanel();
-    private final ListModelImageCollections model = (ListModelImageCollections) appPanel.getListImageCollections().
-            getModel();
-    private final ImageFileThumbnailsPanel thumbnailsPanel = GUI.INSTANCE.
-            getAppPanel().getPanelThumbnails();
+    private final JList list = appPanel.getListImageCollections();
+    private final ListModelImageCollections model =
+            (ListModelImageCollections) list.getModel();
+    private final ImageFileThumbnailsPanel thumbnailsPanel =
+            GUI.INSTANCE.getAppPanel().getPanelThumbnails();
 
     public ControllerCreateImageCollection() {
         listen();
@@ -38,6 +48,14 @@ public final class ControllerCreateImageCollection implements ActionListener {
 
     private void listen() {
         popupMenu.getItemCreateImageCollection().addActionListener(this);
+        list.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (KeyEventUtil.isControl(e, KeyEvent.VK_N)) {
+            createImageCollectionOfSelectedFiles();
+        }
     }
 
     @Override
@@ -63,5 +81,15 @@ public final class ControllerCreateImageCollection implements ActionListener {
                     getString(
                     "ControllerCreateImageCollection.ErrorMessage.Create"));
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
