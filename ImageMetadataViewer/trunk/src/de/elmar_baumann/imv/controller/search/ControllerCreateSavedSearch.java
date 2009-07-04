3c8533afd8dddb00adf82e2fff009d6cac00b509
ebaumann <ebaumann@localhost>
2009-07-04 22:17:26 +0200

    [svn] edited class' javadoc

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/search/ControllerCreateSavedSearch.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/search/ControllerCreateSavedSearch.java
@@ -11,10 +11,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.JList;
 
 /**
- * Controller für die Aktion: Erzeuge eine gespeicherte Suche, ausgelöst von
- * {@link de.elmar_baumann.imv.view.popupmenus.PopupMenuSavedSearches}.
+ * Creates a saved search when the
+ * {@link de.elmar_baumann.imv.view.popupmenus.PopupMenuSavedSearches} fires
+ * the appropriate action.
+ *
+ * Also listens to the {@link JList}'s key events and creates a new saved
+ * search when the keys <code>Ctrl+N</code> were pressed.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/09/10
