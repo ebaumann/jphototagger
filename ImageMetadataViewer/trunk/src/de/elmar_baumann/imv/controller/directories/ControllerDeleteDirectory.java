0e40553c85268940cf105fd76f8b7294260f548f
ebaumann <ebaumann@localhost>
2009-07-04 17:33:29 +0200

    [svn] if the key DEL was pressed in the directorie's tree the selected folder will be deleted

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/directories/ControllerDeleteDirectory.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/directories/ControllerDeleteDirectory.java
@@ -6,19 +6,27 @@ import de.elmar_baumann.imv.view.popupmenus.PopupMenuDirectories;
 import de.elmar_baumann.lib.io.TreeFileSystemDirectories;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  * Listens to {@link PopupMenuDirectories#getItemDeleteDirectory()} and
  * deletes a directory when the action fires.
  *
+ * Also listens to the directorie's {@link JTree} key events and deletes the
+ * selected tree node if the delete key was typed.
+ *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009/06/19
  */
-public final class ControllerDeleteDirectory implements ActionListener {
+public final class ControllerDeleteDirectory
+        implements ActionListener, KeyListener {
 
     PopupMenuDirectories popup = PopupMenuDirectories.INSTANCE;
+    JTree tree = GUI.INSTANCE.getAppPanel().getTreeDirectories();
 
     public ControllerDeleteDirectory() {
         listen();
@@ -26,16 +34,26 @@ public final class ControllerDeleteDirectory implements ActionListener {
 
     private void listen() {
         popup.getItemDeleteDirectory().addActionListener(this);
+        tree.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_DELETE && tree.getSelectionCount() > 0) {
+            Object node = tree.getSelectionPath().getLastPathComponent();
+            if (node instanceof DefaultMutableTreeNode) {
+                deleteDirectory((DefaultMutableTreeNode) node);
+            }
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        deleteDirectory();
+        deleteDirectory(TreeFileSystemDirectories.getNodeOfLastPathComponent(
+                popup.getTreePath()));
     }
 
-    private void deleteDirectory() {
-        DefaultMutableTreeNode node = TreeFileSystemDirectories.
-                getNodeOfLastPathComponent(popup.getTreePath());
+    private void deleteDirectory(DefaultMutableTreeNode node) {
         File dir = node == null
                    ? null
                    : TreeFileSystemDirectories.getFile(node);
@@ -46,5 +64,15 @@ public final class ControllerDeleteDirectory implements ActionListener {
                         node);
             }
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
