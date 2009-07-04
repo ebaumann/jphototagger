f27a05dd0cfa7ad87500b11fd474527e506072b4
ebaumann <ebaumann@localhost>
2009-07-04 17:57:30 +0200

    [svn] if the key F5 was pressed in the directorie's tree the view will be refreshed

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/directories/ControllerRefreshDirectoryTree.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/directories/ControllerRefreshDirectoryTree.java
@@ -5,6 +5,9 @@ import de.elmar_baumann.imv.view.popupmenus.PopupMenuDirectories;
 import de.elmar_baumann.lib.model.TreeModelAllSystemDirectories;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JTree;
 
 /**
  * Listens to {@link PopupMenuTreeDirectories#getItemRefresh()} and
@@ -13,9 +16,11 @@ import java.awt.event.ActionListener;
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009/06/30
  */
-public final class ControllerRefreshDirectoryTree implements ActionListener {
+public final class ControllerRefreshDirectoryTree
+        implements ActionListener, KeyListener {
 
     PopupMenuDirectories popup = PopupMenuDirectories.INSTANCE;
+    JTree tree = GUI.INSTANCE.getAppPanel().getTreeDirectories();
 
     public ControllerRefreshDirectoryTree() {
         listen();
@@ -23,13 +28,35 @@ public final class ControllerRefreshDirectoryTree implements ActionListener {
 
     private void listen() {
         popup.getItemRefresh().addActionListener(this);
+        tree.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_F5) {
+            refresh();
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        refresh();
+    }
+
+    private void refresh() {
         TreeModelAllSystemDirectories model =
                 (TreeModelAllSystemDirectories) GUI.INSTANCE.getAppPanel().
                 getTreeDirectories().getModel();
         model.update();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
