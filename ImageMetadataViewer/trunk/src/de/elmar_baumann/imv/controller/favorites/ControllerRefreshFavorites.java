24dd83fba657799daa61823039898cd9740fcd59
ebaumann <ebaumann@localhost>
2009-07-04 20:10:10 +0200

    [svn] if the key F5 was pressed in the favorite's tree the view will be refreshed

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/favorites/ControllerRefreshFavorites.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/favorites/ControllerRefreshFavorites.java
@@ -5,18 +5,24 @@ import de.elmar_baumann.imv.resource.GUI;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuFavorites;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JTree;
 
 /**
  * Refreshes the favorite directories tree: Adds new folders and removes
  * deleted.
  *
+ * Also listens to the {@link JTree}'s key events and refreshes the view if
+ * the key <code>F5</code> was pressed.
+ *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009/06/28
  */
-public final class ControllerRefreshFavorites implements
-        ActionListener {
+public final class ControllerRefreshFavorites
+        implements ActionListener, KeyListener {
 
-    private PopupMenuFavorites popup = PopupMenuFavorites.INSTANCE;
+    private final PopupMenuFavorites popup = PopupMenuFavorites.INSTANCE;
 
     public ControllerRefreshFavorites() {
         listen();
@@ -24,15 +30,37 @@ public final class ControllerRefreshFavorites implements
 
     private void listen() {
         popup.getItemRefresh().addActionListener(this);
+        GUI.INSTANCE.getAppPanel().getTreeFavorites().addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_F5) {
+            refresh();
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (popup.getItemRefresh().equals(e.getSource())) {
-            TreeModelFavorites model =
-                    (TreeModelFavorites) GUI.INSTANCE.getAppPanel().
-                    getTreeFavorites().getModel();
-            model.update();
+            refresh();
         }
+    }
+
+    private void refresh() {
+        TreeModelFavorites model =
+                (TreeModelFavorites) GUI.INSTANCE.getAppPanel().
+                getTreeFavorites().getModel();
+        model.update();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
