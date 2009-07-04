aff536ca51f086ef5854626be022df90c1b3ba80
ebaumann <ebaumann@localhost>
2009-07-04 22:13:48 +0200

    [svn] keys Ctrl+N triggers create action in saved searches' list

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/search/ControllerCreateSavedSearch.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/search/ControllerCreateSavedSearch.java
@@ -3,11 +3,14 @@ package de.elmar_baumann.imv.controller.search;
 import de.elmar_baumann.imv.event.listener.impl.ListenerProvider;
 import de.elmar_baumann.imv.event.SearchEvent;
 import de.elmar_baumann.imv.event.listener.SearchListener;
+import de.elmar_baumann.imv.resource.GUI;
 import de.elmar_baumann.imv.tasks.SavedSearchesModifier;
 import de.elmar_baumann.imv.view.dialogs.AdvancedSearchDialog;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuSavedSearches;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Controller für die Aktion: Erzeuge eine gespeicherte Suche, ausgelöst von
@@ -16,8 +19,8 @@ import java.awt.event.ActionListener;
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008/09/10
  */
-public final class ControllerCreateSavedSearch implements ActionListener,
-                                                          SearchListener {
+public final class ControllerCreateSavedSearch
+        implements ActionListener, KeyListener, SearchListener {
 
     public ControllerCreateSavedSearch() {
         listen();
@@ -27,6 +30,14 @@ public final class ControllerCreateSavedSearch implements ActionListener,
         PopupMenuSavedSearches.INSTANCE.getItemCreate().addActionListener(
                 this);
         ListenerProvider.INSTANCE.addSearchListener(this);
+        GUI.INSTANCE.getAppPanel().getListSavedSearches().addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_N) {
+            showAdvancedSearchDialog();
+        }
     }
 
     @Override
@@ -49,5 +60,15 @@ public final class ControllerCreateSavedSearch implements ActionListener,
         } else {
             dialog.setVisible(true);
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
