9570ad9c0d11c7d578fe798165b1e625e26e439d
ebaumann <ebaumann@localhost>
2009-07-04 20:25:35 +0200

    [svn] keys F2 and Ctrl+R triggers rename action in favorite directorie's tree

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/favorites/ControllerFavoritesRenameFilesystemFolder.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/favorites/ControllerFavoritesRenameFilesystemFolder.java
@@ -3,23 +3,31 @@ package de.elmar_baumann.imv.controller.favorites;
 import de.elmar_baumann.imv.io.FileSystemDirectories;
 import de.elmar_baumann.imv.resource.GUI;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuFavorites;
+import de.elmar_baumann.lib.event.util.KeyEventUtil;
 import de.elmar_baumann.lib.io.TreeFileSystemDirectories;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  * Listens to {@link PopupMenuFavorites#getItemRenameFilesystemFolder()} and
  * renames a directory in the file system when the action fires.
  *
+ * Also listens to the {@link JTree}'s key events and renames the selected
+ * file system directory if the keys <code>Strg+R</code> were pressed.
+ *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009/06/19
  */
-public final class ControllerFavoritesRenameFilesystemFolder implements
-        ActionListener {
+public final class ControllerFavoritesRenameFilesystemFolder
+        implements ActionListener, KeyListener {
 
-    PopupMenuFavorites popup = PopupMenuFavorites.INSTANCE;
+    private final PopupMenuFavorites popup = PopupMenuFavorites.INSTANCE;
+    private final JTree tree = GUI.INSTANCE.getAppPanel().getTreeFavorites();
 
     public ControllerFavoritesRenameFilesystemFolder() {
         listen();
@@ -27,16 +35,31 @@ public final class ControllerFavoritesRenameFilesystemFolder implements
 
     private void listen() {
         popup.getItemRenameFilesystemFolder().addActionListener(this);
+        tree.addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (isRename(e) && !tree.isSelectionEmpty()) {
+            Object node = tree.getSelectionPath().getLastPathComponent();
+            if (node instanceof DefaultMutableTreeNode) {
+                renameDirectory((DefaultMutableTreeNode) node);
+            }
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        renameDirectory();
+        renameDirectory(TreeFileSystemDirectories.getNodeOfLastPathComponent(
+                popup.getTreePath()));
     }
 
-    private void renameDirectory() {
-        DefaultMutableTreeNode node = TreeFileSystemDirectories.
-                getNodeOfLastPathComponent(popup.getTreePath());
+    private boolean isRename(KeyEvent e) {
+        return KeyEventUtil.isControl(e, KeyEvent.VK_R) ||
+                e.getKeyCode() == KeyEvent.VK_F2;
+    }
+
+    private void renameDirectory(DefaultMutableTreeNode node) {
         File dir = node == null
                    ? null
                    : TreeFileSystemDirectories.getFile(node);
@@ -49,5 +72,15 @@ public final class ControllerFavoritesRenameFilesystemFolder implements
                         getModel(), node);
             }
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
