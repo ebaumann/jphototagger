96031236bc34f66694bcee07ac39c181aa91dd8c
ebaumann <ebaumann@localhost>
2009-07-07 15:12:26 +0200

    [svn] Start

---
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/FilenameFormatListener.java
@@ -0,0 +1,25 @@
+package de.elmar_baumann.imv.event.listener;
+
+/**
+ * Listens while formatting files.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2009/07/07
+ */
+public interface FilenameFormatListener {
+
+    public enum Request {
+
+        /**
+         * A sequencial format shall restart it's sequence
+         */
+        RESTART_SEQUENCE
+    }
+
+    /**
+     * Request from a filename formatter.
+     *
+     * @param request request
+     */
+    public void request(Request request);
+}
