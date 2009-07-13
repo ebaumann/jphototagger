9e425c579b4107fb82ece51f643efe7ef0c10b23
ebaumann <ebaumann@localhost>
2009-07-13 13:18:55 +0200

    [svn] * Default title * Removed methods without varargs

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/MessageDisplayer.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/MessageDisplayer.java
@@ -1,6 +1,8 @@
 package de.elmar_baumann.imv.app;
 
 import de.elmar_baumann.imv.resource.Bundle;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JOptionPane;
 
 /**
@@ -11,23 +13,28 @@ import javax.swing.JOptionPane;
  */
 public final class MessageDisplayer {
 
-    /**
-     * Displays an error message.
-     *
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
-     */
-    public static void error(String propertyKey) {
-        message(propertyKey, JOptionPane.ERROR_MESSAGE);
+    private static final Map<Integer, String> defaultTitleOfMessageType =
+            new HashMap<Integer, String>();
+
+    {
+        defaultTitleOfMessageType.put(JOptionPane.ERROR_MESSAGE,
+                Bundle.getString("MessageDisplayer.DefaultTitle.ErrorMessage"));
+        defaultTitleOfMessageType.put(JOptionPane.WARNING_MESSAGE,
+                Bundle.getString("MessageDisplayer.DefaultTitle.WarningMessage"));
+        defaultTitleOfMessageType.put(JOptionPane.INFORMATION_MESSAGE,
+                Bundle.getString(
+                "MessageDisplayer.DefaultTitle.InformationMessage"));
+        defaultTitleOfMessageType.put(JOptionPane.QUESTION_MESSAGE,
+                Bundle.getString("MessageDisplayer.DefaultTitle.QuestionMessage"));
     }
 
     /**
      * Displays an error message.
      * 
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
+     * @param propertyKey property key for {@link Bundle}. If in the property
+     *                    file exists a key having the same name plus the
+     *                    postfix <code>.Title</code> this key is used for the
+     *                    title. Else a default title will be set.
      * @param params      parameters for message format placeholders
      */
     public static void error(String propertyKey, Object... params) {
@@ -37,20 +44,10 @@ public final class MessageDisplayer {
     /**
      * Displays an warning message.
      *
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
-     */
-    public static void warning(String propertyKey) {
-        message(propertyKey, JOptionPane.WARNING_MESSAGE);
-    }
-
-    /**
-     * Displays an warning message.
-     *
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
+     * @param propertyKey property key for {@link Bundle}. If in the property
+     *                    file exists a key having the same name plus the
+     *                    postfix <code>.Title</code> this key is used for the
+     *                    title. Else a default title will be set.
      * @param params      parameters for message format placeholders
      */
     public static void warning(String propertyKey, Object... params) {
@@ -60,47 +57,14 @@ public final class MessageDisplayer {
     /**
      * Displays an information message.
      *
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
-     */
-    public static void information(String propertyKey) {
-        message(propertyKey, JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    /**
-     * Displays an information message.
-     *
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
+     * @param propertyKey property key for {@link Bundle}. If in the property
+     *                    file exists a key having the same name plus the
+     *                    postfix <code>.Title</code> this key is used for the
+     *                    title. Else a default title will be set.
      * @param params      parameters for message format placeholders
      */
     public static void information(String propertyKey, Object... params) {
         message(propertyKey, JOptionPane.INFORMATION_MESSAGE, params);
-    }
-
-    /**
-     * Displays a confirm message.
-     *
-     * @param propertyKey property key for {@link Bundle}. There also a key for
-     *                    the title has to be in the properties file with the
-     *                    same name and the postfix <code>.Title</code>
-     * @param cancel      true if a cancel button shall be displayed
-     * @return            one of these:
-     *                    <ul>
-     *                    <li>{@link JOptionPane#YES_OPTION}</li>
-     *                    <li>{@link JOptionPane#NO_OPTION}</li>
-     *                    <li>{@link JOptionPane#CANCEL_OPTION}</li>
-     *                    </ul>
-     */
-    public static int confirm(String propertyKey, boolean cancel) {
-        return JOptionPane.showConfirmDialog(null,
-                Bundle.getString(propertyKey),
-                propertyKey + ".Title",
-                cancel
-                ? JOptionPane.YES_NO_CANCEL_OPTION
-                : JOptionPane.YES_NO_OPTION);
     }
 
     /**
@@ -122,7 +86,7 @@ public final class MessageDisplayer {
             String propertyKey, boolean cancel, Object... params) {
         return JOptionPane.showConfirmDialog(null,
                 Bundle.getString(propertyKey, params),
-                propertyKey + ".Title",
+                getTitle(propertyKey, JOptionPane.QUESTION_MESSAGE),
                 cancel
                 ? JOptionPane.YES_NO_CANCEL_OPTION
                 : JOptionPane.YES_NO_OPTION);
@@ -131,15 +95,15 @@ public final class MessageDisplayer {
     private static void message(String propertyKey, int type, Object... params) {
         JOptionPane.showMessageDialog(null,
                 Bundle.getString(propertyKey, params),
-                Bundle.getString(propertyKey + ".Title"),
+                getTitle(propertyKey, type),
                 type);
+
     }
 
-    private static void message(String propertyKey, int type) {
-        JOptionPane.showMessageDialog(null,
-                Bundle.getString(propertyKey),
-                Bundle.getString(propertyKey + ".Title"),
-                type);
+    private static String getTitle(String propertyKey, int messageType) {
+        return Bundle.containsKey(propertyKey)
+               ? Bundle.getString(propertyKey + ".Title")
+               : defaultTitleOfMessageType.get(messageType);
     }
 
     private MessageDisplayer() {
