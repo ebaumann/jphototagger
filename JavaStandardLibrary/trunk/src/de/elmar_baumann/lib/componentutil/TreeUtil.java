9330f29900b217c0519636ab6698cf98b644b5ec
ebaumann <ebaumann@localhost>
2009-07-15 15:46:54 +0200

    [svn] Added expandAll()

--- JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/componentutil/TreeUtil.java
+++ JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/componentutil/TreeUtil.java
@@ -368,6 +368,40 @@ public final class TreeUtil {
         return null;
     }
 
+    // Code: http://www.exampledepot.com/egs/javax.swing.tree/ExpandAll.html
+    /**
+     * If expand is true, expands all nodes in the tree.
+     * Otherwise, collapses all nodes in the tree.
+
+     * @param tree   tree
+     * @param expand true if expand all nodes
+     */
+    public static void expandAll(JTree tree, boolean expand) {
+        TreeNode root = (TreeNode) tree.getModel().getRoot();
+
+        // Traverse tree from root
+        expandAll(tree, new TreePath(root), expand);
+    }
+
+    private static void expandAll(JTree tree, TreePath parent, boolean expand) {
+        // Traverse children
+        TreeNode node = (TreeNode) parent.getLastPathComponent();
+        if (node.getChildCount() >= 0) {
+            for (Enumeration e = node.children(); e.hasMoreElements();) {
+                TreeNode n = (TreeNode) e.nextElement();
+                TreePath path = parent.pathByAddingChild(n);
+                expandAll(tree, path, expand);
+            }
+        }
+
+        // Expansion or collapse must be done bottom-up
+        if (expand) {
+            tree.expandPath(parent);
+        } else {
+            tree.collapsePath(parent);
+        }
+    }
+
     private TreeUtil() {
     }
 }
