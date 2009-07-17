7ee4401aa3fba64481481733425f7e32c8ce1b93
ebaumann <ebaumann@localhost>
2009-07-17 09:04:07 +0200

    [svn] Static method shutdown() instead app exit listener: So having control when to shut down the threads

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/tasks/Cleanup.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/tasks/Cleanup.java
@@ -1,23 +1,37 @@
 package de.elmar_baumann.imv.tasks;
 
-import de.elmar_baumann.imv.event.listener.AppExitListener;
-import de.elmar_baumann.imv.resource.GUI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Cleans up when the application will exit.
+ * Shuts down all Tasks. Should be called when the application exits.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009/07/16
  */
-public final class Cleanup implements AppExitListener {
+public final class Cleanup {
 
-    public Cleanup() {
-        GUI.INSTANCE.getAppFrame().addAppExitListener(this);
-    }
+    /**
+     * Wait time in milliseconds before giving control to the caller, so that
+     * the threads can complete their current action before they check for
+     * interruption.
+     */
+    private static long MILLISECONDS_WAIT = 2000;
 
-    @Override
-    public void appWillExit() {
+    /**
+     * Shuts down all Tasks.
+     */
+    public static void shutdown() {
         AutomaticTask.INSTANCE.shutdown();
         UserTasksQueue.INSTANCE.shutdown();
+        try {
+            // Let the tasks a little bit time to complete until they can interrupt
+            Thread.sleep(MILLISECONDS_WAIT);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Cleanup.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private Cleanup() {
     }
 }
