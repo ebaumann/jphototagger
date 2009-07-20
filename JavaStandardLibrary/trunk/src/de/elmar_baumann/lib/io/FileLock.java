6f9bb4161a7344db7f5dc911e781092c5ffbed04
ebaumann <ebaumann@localhost>
2009-07-20 09:14:27 +0200

    [svn] * Added getOwner() * Documented methods

--- JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/io/FileLock.java
+++ JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/io/FileLock.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Locks files. Requires that <em>every</em> objects which manipulate files
+ * Locks  <em>internally</em> a file (other applications doesn't regognize
+ * the lock).
+ *
+ * Requires that <em>every</em> objects which manipulate files
  * uses this class to access files. Should be used when changing files or
  * using libraries which fail when opening the same file twice (for reading).
- *
- * <em>This class does not lock the file using the file system! It locks them
- * virtually for objects using this class.</em>
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009-06-22
@@ -19,24 +19,48 @@ import java.util.Map;
 public final class FileLock {
 
     public static final FileLock INSTANCE = new FileLock();
-    private final Map<File, Object> objectOfLockedFile =
+    private final Map<File, Object> ownerOfLockedFile =
             Collections.synchronizedMap(new HashMap<File, Object>());
 
+    /**
+     * Locks a file.
+     *
+     * @param  file  file to lock
+     * @param  owner owner of the file
+     * @return       true if locked. false if the file is already locked.
+     */
     public boolean lock(File file, Object owner) {
-        synchronized (objectOfLockedFile) {
-            if (!objectOfLockedFile.containsKey(file)) {
-                objectOfLockedFile.put(file, owner);
+        synchronized (ownerOfLockedFile) {
+            if (!ownerOfLockedFile.containsKey(file)) {
+                ownerOfLockedFile.put(file, owner);
                 return true;
             }
             return false;
         }
     }
 
+    /**
+     * Returns the owner of a locked file.
+     *
+     * @param  file file
+     * @return      owner or null if no object owns a lock of that file
+     */
+    public Object getOwner(File file) {
+        return ownerOfLockedFile.get(file);
+    }
+
+    /**
+     * Unlocks a locked file.
+     *
+     * @param  file  file to unlock
+     * @param  owner owner of the lock - only the owner can unlock a file
+     * @return true if unlocked
+     */
     public boolean unlock(File file, Object owner) {
-        synchronized (objectOfLockedFile) {
-            if (objectOfLockedFile.containsKey(file) &&
-                    objectOfLockedFile.get(file).equals(owner)) {
-                objectOfLockedFile.remove(file);
+        synchronized (ownerOfLockedFile) {
+            if (ownerOfLockedFile.containsKey(file) &&
+                    ownerOfLockedFile.get(file).equals(owner)) {
+                ownerOfLockedFile.remove(file);
                 return true;
             }
             return false;
