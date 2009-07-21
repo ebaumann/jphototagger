6d57773ab6f1f13ccce5e87076878405650f84f3
ebaumann <ebaumann@localhost>
2009-07-21 10:56:03 +0200

    [svn] Start

---
+++ JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/generics/Util.java
@@ -0,0 +1,63 @@
+package de.elmar_baumann.lib.generics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Utils for generics.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2008-09-18
+ */
+public final class Util {
+
+    /**
+     * Returns the first elements of a collection of pairs.
+     * 
+     * @param <A>   type of first elements
+     * @param <B>   type of second elements
+     * @param pairs pairs
+     * @return      all first elements of that pairs in iterator order of that
+     *              collection
+     * @throws      NullPointerException if pairs is null
+     */
+    public static <A, B> List<? extends A> firstOfPairs(
+            Collection<Pair<? extends A, ? extends B>> pairs) {
+
+        if (pairs == null)
+            throw new NullPointerException("pairs == null"); // NOI18N
+
+        List<A> list = new ArrayList<A>(pairs.size());
+        for (Pair<? extends A, ? extends B> pair : pairs) {
+            list.add(pair.getFirst());
+        }
+        return list;
+    }
+
+    /**
+     * Returns the second elements of a collection of pairs.
+     *
+     * @param <A>   type of first elements
+     * @param <B>   type of second elements
+     * @param pairs pairs
+     * @return      all second elements of that pairs in iterator order of that
+     *              collection
+     * @throws      NullPointerException if pairs is null
+     */
+    public static <A, B> List<? extends B> secondOfPairs(
+            Collection<Pair<? extends A, ? extends B>> pairs) {
+
+        if (pairs == null)
+            throw new NullPointerException("pairs == null"); // NOI18N
+
+        List<B> list = new ArrayList<B>(pairs.size());
+        for (Pair<? extends A, ? extends B> pair : pairs) {
+            list.add(pair.getSecond());
+        }
+        return list;
+    }
+
+    private Util() {
+    }
+}
