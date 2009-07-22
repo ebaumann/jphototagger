d37bc331099ec9a03f05243c46c41e8c7741d091
ebaumann <ebaumann@localhost>
2009-07-22 19:52:53 +0200

    [svn] Version information 0.6.10 2009-07-22

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/AppInfo.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/app/AppInfo.java
@@ -18,7 +18,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.6.9 2009-07-21";  // NOI18N
+    public static final String APP_VERSION = "0.6.10 2009-07-22";  // NOI18N
     /**
      * The application's name
      */
