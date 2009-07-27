01f755adc2ad558deac196de639b43de6d3358ef
ebaumann <ebaumann@localhost>
2009-07-27 11:28:59 +0200

    [svn] Popup menu is displayed even if the mouse position isn't obove a selected tree item

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/impl/MouseListenerDirectories.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/impl/MouseListenerDirectories.java
@@ -2,6 +2,7 @@ package de.elmar_baumann.imv.event.listener.impl;
 
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuDirectories;
 import de.elmar_baumann.lib.componentutil.TreeUtil;
+import de.elmar_baumann.lib.event.util.MouseEventUtil;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
@@ -17,54 +18,26 @@ import javax.swing.tree.TreePath;
  */
 public final class MouseListenerDirectories extends MouseAdapter {
 
-    private final PopupMenuDirectories popupMenu =
-            PopupMenuDirectories.INSTANCE;
+    private final PopupMenuDirectories popupMenu = PopupMenuDirectories.INSTANCE;
 
     @Override
     public void mousePressed(MouseEvent e) {
-        JTree tree = (JTree) e.getSource();
-        int x = e.getX();
-        int y = e.getY();
-        TreePath path = tree.getPathForLocation(x, y);
-        if ((e.isPopupTrigger() || e.getModifiers() == 4)) {
-            boolean isSelectedItem = TreeUtil.isSelectedItemPosition(e);
-            boolean isRootItem = TreeUtil.isRootItemPosition(e);
-            if (isSelectedItem && !isRootItem) {
-                Object o = tree.getSelectionPath().getLastPathComponent();
-                Object o2 = path.getLastPathComponent();
-                if (o instanceof DefaultMutableTreeNode &&
-                        o2 instanceof DefaultMutableTreeNode) {
-                    Object userObj1 =
-                            ((DefaultMutableTreeNode) o).getUserObject();
-                    Object userObj2 = ((DefaultMutableTreeNode) o2).
+        TreePath path = TreeUtil.getTreePath(e);
+        if (path == null) return;
+        if (MouseEventUtil.isPopupTrigger(e)) {
+            if (!TreeUtil.isRootItemPosition(e)) {
+                Object lastPathComponent = path.getLastPathComponent();
+                if (lastPathComponent instanceof DefaultMutableTreeNode) {
+                    Object usrOb = ((DefaultMutableTreeNode) lastPathComponent).
                             getUserObject();
-                    if (userObj1 instanceof File & userObj2 instanceof File) {
-                        File selectedDirectory = (File) userObj1;
-                        File popupDirectory = (File) userObj2;
-                        if (selectedDirectory.equals(popupDirectory)) {
-                            popupMenu.setDirectoryName(selectedDirectory.
-                                    getAbsolutePath());
-                            popupMenu.setTreePath(path);
-                            isSelectedItem = true;
-                        }
+                    if (usrOb instanceof File) {
+                        File dir = (File) usrOb;
+                        popupMenu.setDirectoryName(dir.getAbsolutePath());
+                        popupMenu.setTreePath(path);
                     }
                 }
             }
-            setFileItemsEnabled(isSelectedItem);
-            popupMenu.show(tree, x, y);
-        } else {
-            if (path != null) {
-                popupMenu.setTreeSelected(true);
-                tree.setSelectionPath(path);
-                popupMenu.setTreeSelected(false);
-            }
+            popupMenu.show((JTree) e.getSource(), e.getX(), e.getY());
         }
-    }
-
-    private void setFileItemsEnabled(boolean enabled) {
-        popupMenu.getItemAddToFavorites().setEnabled(enabled);
-        popupMenu.getItemCreateDirectory().setEnabled(enabled);
-        popupMenu.getItemRenameDirectory().setEnabled(enabled);
-        popupMenu.getItemDeleteDirectory().setEnabled(enabled);
     }
 }
