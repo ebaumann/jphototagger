483e2eceaea18913985e2710496b27330ab38e35
ebaumann <ebaumann@localhost>
2009-07-27 13:18:51 +0200

    [svn] Popup menu saved searches acts even on not selected nodes

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/impl/MouseListenerSavedSearches.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/impl/MouseListenerSavedSearches.java
@@ -2,7 +2,8 @@ package de.elmar_baumann.imv.event.listener.impl;
 
 import de.elmar_baumann.imv.data.SavedSearch;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuSavedSearches;
-import java.awt.Point;
+import de.elmar_baumann.lib.componentutil.ListUtil;
+import de.elmar_baumann.lib.event.util.MouseEventUtil;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JList;
@@ -20,25 +21,19 @@ public final class MouseListenerSavedSearches extends MouseAdapter {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        JList list = (JList) e.getSource();
-        int x = e.getX();
-        int y = e.getY();
-        int index = list.locationToIndex(new Point(x, y));
-        if ((e.isPopupTrigger() || e.getModifiers() == 4)) {
-            boolean isItem = index >= 0 && index == list.getSelectedIndex();
-            Object selectedValue = list.getSelectedValue();
-            if (selectedValue instanceof SavedSearch) {
-                SavedSearch savedSearch = (SavedSearch) selectedValue;
+        int index = ListUtil.getItemIndex(e);
+        if (MouseEventUtil.isPopupTrigger(e)) {
+            boolean isItem = index >= 0;
+            JList list = (JList) e.getSource();
+            Object element = list.getModel().getElementAt(index);
+            if (element instanceof SavedSearch) {
+                SavedSearch savedSearch = (SavedSearch) element;
                 popupMenu.setSavedSearch(savedSearch);
             }
             popupMenu.getItemEdit().setEnabled(isItem);
             popupMenu.getItemDelete().setEnabled(isItem);
             popupMenu.getItemRename().setEnabled(isItem);
-            popupMenu.show(list, x, y);
-        } else {
-            if (index >= 0) {
-                list.setSelectedIndex(index);
-            }
+            popupMenu.show(list, e.getX(), e.getY());
         }
     }
 }
