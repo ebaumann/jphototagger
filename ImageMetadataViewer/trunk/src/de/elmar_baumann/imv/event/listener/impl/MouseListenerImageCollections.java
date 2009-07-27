1e219266476745fbdb6f9773f0c34df02e00185a
ebaumann <ebaumann@localhost>
2009-07-27 14:48:46 +0200

    [svn] Disabled items on special collections

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/impl/MouseListenerImageCollections.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/event/listener/impl/MouseListenerImageCollections.java
@@ -1,5 +1,6 @@
 package de.elmar_baumann.imv.event.listener.impl;
 
+import de.elmar_baumann.imv.model.ListModelImageCollections;
 import de.elmar_baumann.imv.view.popupmenus.PopupMenuImageCollections;
 import de.elmar_baumann.lib.componentutil.ListUtil;
 import de.elmar_baumann.lib.event.util.MouseEventUtil;
@@ -23,15 +24,26 @@ public final class MouseListenerImageCollections extends MouseAdapter {
         int index = ListUtil.getItemIndex(e);
         popupMenu.setItemIndex(index);
         if (MouseEventUtil.isPopupTrigger(e)) {
+            JList list = (JList) e.getSource();
             boolean isItem = index >= 0;
-            popupMenu.getItemDelete().setEnabled(isItem);
-            popupMenu.getItemRename().setEnabled(isItem);
-            popupMenu.show((JList) e.getSource(), e.getX(), e.getY());
+            boolean isSpecialCollection = isSpecialCollection(list, index);
+            popupMenu.getItemDelete().setEnabled(isItem && !isSpecialCollection);
+            popupMenu.getItemRename().setEnabled(isItem && !isSpecialCollection);
+            popupMenu.show(list, e.getX(), e.getY());
         }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
         ((JList) e.getSource()).setCursor(Cursor.getDefaultCursor());
+    }
+
+    private boolean isSpecialCollection(JList list, int index) {
+        if (index < 0) return false;
+        Object o = list.getModel().getElementAt(index);
+        if (o != null) {
+            return ListModelImageCollections.isSpecialCollection(o.toString());
+        }
+        return false;
     }
 }
