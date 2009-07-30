81fc9b1b41823b33f7270f675a0bbdd5dd90eb2c
ebaumann <ebaumann@localhost>
2009-07-30 11:54:57 +0200

    [svn] Added space between buttons and progress bar

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/view/panels/ActionsPanel.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/view/panels/ActionsPanel.java
@@ -233,7 +233,7 @@ public final class ActionsPanel extends javax.swing.JPanel {
                 .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 222, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(buttonExecute)
                     .addComponent(buttonNew)
