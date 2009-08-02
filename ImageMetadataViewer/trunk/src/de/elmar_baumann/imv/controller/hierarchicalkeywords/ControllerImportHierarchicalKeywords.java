47d185240a89ec4c5df50581e26f9f867a7ea337
ebaumann <ebaumann@localhost>
2009-08-02 02:48:43 +0200

    [svn] Showing progress

--- ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/hierarchicalkeywords/ControllerImportHierarchicalKeywords.java
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/controller/hierarchicalkeywords/ControllerImportHierarchicalKeywords.java
@@ -5,10 +5,12 @@ import de.elmar_baumann.imv.model.TreeModelHierarchicalKeywords;
 import de.elmar_baumann.imv.resource.GUI;
 import de.elmar_baumann.imv.view.dialogs.HierarchicalKeywordsImportDialog;
 import de.elmar_baumann.imv.view.frames.AppFrame;
+import de.elmar_baumann.imv.view.panels.ProgressBarUserTasks;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.util.List;
+import javax.swing.JProgressBar;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeModel;
 
@@ -44,35 +46,71 @@ public final class ControllerImportHierarchicalKeywords
             HierarchicalKeywordsImporter importer = dlg.getImporter();
             assert importer != null : "Importer is null!";
             if (importer != null) {
-                Collection<List<String>> paths =
-                        importer.getPaths(dlg.getFile());
-                for (List<String> path : paths) {
-                    addKeywordPath(path);
-                }
+                new ImportTask(importer.getPaths(dlg.getFile())).start();
             }
         }
     }
 
-    private void addKeywordPath(List<String> path) {
-        TreeModel m = GUI.INSTANCE.getAppPanel().getTreeHierarchicalKeywords().
-                getModel();
-        assert m instanceof TreeModelHierarchicalKeywords : "Model is not a " +
-                TreeModelHierarchicalKeywords.class + " but a " + m.getClass();
+    private class ImportTask extends Thread {
 
-        if (m instanceof TreeModelHierarchicalKeywords) {
-            TreeModelHierarchicalKeywords model =
-                    (TreeModelHierarchicalKeywords) m;
-            DefaultMutableTreeNode node =
-                    (DefaultMutableTreeNode) model.getRoot();
-            for (String keyword : path) {
-                DefaultMutableTreeNode existingNode =
-                        model.findChildByName(node, keyword);
-                if (existingNode == null) {
-                    model.addKeyword(node, keyword);
-                    node = model.findChildByName(node, keyword);
-                } else {
-                    node = existingNode;
+        private final Collection<List<String>> paths;
+        private final TreeModel treeModel = GUI.INSTANCE.getAppPanel().
+                getTreeHierarchicalKeywords().getModel();
+        private final JProgressBar progressBar;
+
+        public ImportTask(Collection<List<String>> paths) {
+            this.paths = paths;
+            setName("Importing keywords @ " + getClass().getName());
+            progressBar = (JProgressBar) ProgressBarUserTasks.INSTANCE.
+                    getResource(this);
+        }
+
+        @Override
+        public void run() {
+            assert treeModel instanceof TreeModelHierarchicalKeywords :
+                    "Model is not a " + TreeModelHierarchicalKeywords.class +
+                    " but a " + treeModel.getClass();
+            if (treeModel instanceof TreeModelHierarchicalKeywords) {
+                TreeModelHierarchicalKeywords model =
+                        (TreeModelHierarchicalKeywords) treeModel;
+                initProgressBar();
+                for (List<String> path : paths) {
+                    DefaultMutableTreeNode node =
+                            (DefaultMutableTreeNode) model.getRoot();
+                    for (String keyword : path) {
+                        DefaultMutableTreeNode existingNode =
+                                model.findChildByName(node, keyword);
+                        if (existingNode == null) {
+                            model.addKeyword(node, keyword);
+                            node = model.findChildByName(node, keyword);
+                        } else {
+                            node = existingNode;
+                        }
+                    }
+                    setProgressBarNextValue();
                 }
+                releaseProgressBar();
+            }
+        }
+
+        private void initProgressBar() {
+            if (progressBar != null) {
+                progressBar.setMinimum(0);
+                progressBar.setMaximum(paths.size());
+                progressBar.setValue(0);
+            }
+        }
+
+        private void setProgressBarNextValue() {
+            if (progressBar != null) {
+                progressBar.setValue(progressBar.getValue() + 1);
+            }
+        }
+
+        private void releaseProgressBar() {
+            if (progressBar != null) {
+                progressBar.setValue(paths.size());
+                ProgressBarUserTasks.INSTANCE.releaseResource(this);
             }
         }
     }
