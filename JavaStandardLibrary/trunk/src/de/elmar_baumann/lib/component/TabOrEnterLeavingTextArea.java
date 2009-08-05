fb5b836efe8b78041e705772fe25778e836d3338
ebaumann <ebaumann@localhost>
2009-08-05 12:17:51 +0200

    [svn] renamed TabLeavingTextArea to TabOrEnterLeavingTextArea and leafing the area also when enter was pressed

--- JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/component/TabOrEnterLeavingTextArea.java
+++ JavaStandardLibrary/trunk/src/de/elmar_baumann/lib/component/TabOrEnterLeavingTextArea.java
@@ -4,17 +4,19 @@ import java.awt.event.KeyEvent;
 import javax.swing.JTextArea;
 
 /**
- * Textarea, die mit der Tabulatortaste verlassen wird.
+ * Text area where the tab or enter key transferring the focus.
+ *
+ * This text area is thought as a replacement for a text field but with the
+ * capabilities of a text area.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2008-09-19
  */
-public final class TabLeavingTextArea extends JTextArea {
+public final class TabOrEnterLeavingTextArea extends JTextArea {
 
     @Override
     protected void processComponentKeyEvent(KeyEvent e) {
-        if (e.getID() == KeyEvent.KEY_PRESSED &&
-            e.getKeyCode() == KeyEvent.VK_TAB) {
+        if (e.getID() == KeyEvent.KEY_PRESSED && isLeave(e.getKeyCode())) {
             e.consume();
             if (e.isShiftDown()) {
                 transferFocusBackward();
@@ -24,5 +26,9 @@ public final class TabLeavingTextArea extends JTextArea {
         } else {
             super.processComponentKeyEvent(e);
         }
+    }
+
+    private static boolean isLeave(int keyCode) {
+        return keyCode == KeyEvent.VK_TAB || keyCode == KeyEvent.VK_ENTER;
     }
 }
