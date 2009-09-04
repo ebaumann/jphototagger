ffe53dcaaad6fb5ac9959b197616dca9a68f0243
ebaumann <ebaumann@localhost>
2009-09-05 00:31:39 +0200

    [svn] Start

---
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/io/CharEncoding.java
@@ -0,0 +1,20 @@
+package de.elmar_baumann.imv.io;
+
+/**
+ * Application wide character encodings.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2009-09-05
+ */
+public final class CharEncoding {
+
+    /**
+     * Character encoding of exported Adobe Photoshop Lightroom hierarchical
+     * keywords.
+     */
+    public static final String LIGHTROOM_KEYWORDS = "UTF8";
+
+    private CharEncoding() {
+    }
+
+}
---
+++ ImageMetadataViewer/trunk/src/de/elmar_baumann/imv/io/FilenameSuffixes.java
@@ -0,0 +1,19 @@
+package de.elmar_baumann.imv.io;
+
+/**
+ * Application wide filename suffixes.
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2009-09-05
+ */
+public final class FilenameSuffixes {
+
+    /**
+     * Filename suffix of exported Adobe Photoshop Lightroom hierarchical
+     * keywords.
+     */
+    public static final String LIGHTROOM_KEYWORDS = "txt";
+
+    private FilenameSuffixes() {
+    }
+}
