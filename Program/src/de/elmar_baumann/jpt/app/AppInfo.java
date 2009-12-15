b7233cde64abebda13260b44c37dbd6e3ea4e86a
ebaumann <ebaumann@localhost>
2009-12-15 21:22:39 +0100

    Added version info 0.7.6

--- Program/src/de/elmar_baumann/jpt/app/AppInfo.java
+++ Program/src/de/elmar_baumann/jpt/app/AppInfo.java
@@ -41,7 +41,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.7.5 2009-12-13";  // NOI18N
+    public static final String APP_VERSION = "0.7.6 2009-12-14";  // NOI18N
     /**
      * The application's name
      */
