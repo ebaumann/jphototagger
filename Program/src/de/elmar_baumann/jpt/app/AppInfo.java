f993b7793aadac014333ae1623b83f43b4438a5b
ebaumann <ebaumann@localhost>
2009-12-26 02:20:12 +0100

    Online Help and User Manual: New Screenshots

--- Program/src/de/elmar_baumann/jpt/app/AppInfo.java
+++ Program/src/de/elmar_baumann/jpt/app/AppInfo.java
@@ -41,7 +41,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.7.17 2009-12-25"; 
+    public static final String APP_VERSION = "0.7.18 2009-12-26";
     /**
      * The application's name
      */
