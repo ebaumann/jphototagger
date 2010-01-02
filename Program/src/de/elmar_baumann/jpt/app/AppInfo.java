7088b905f38509eecedb8eaae85f2f45959ee992
ebaumann <ebaumann@localhost>
2010-01-02 05:54:30 +0100

    Version information 0.7.24

--- Program/src/de/elmar_baumann/jpt/app/AppInfo.java
+++ Program/src/de/elmar_baumann/jpt/app/AppInfo.java
@@ -41,7 +41,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.7.24 2010-01-01";
+    public static final String APP_VERSION = "0.7.24 2010-01-02";
     /**
      * The application's name
      */
