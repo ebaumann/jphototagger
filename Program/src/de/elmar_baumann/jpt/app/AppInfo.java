d276ab5eb9813c909f1b1e8de67cf47177f11abb
ebaumann <ebaumann@localhost>
2010-01-03 04:38:53 +0100

    Updating XMP in database more efficient

--- Program/src/de/elmar_baumann/jpt/app/AppInfo.java
+++ Program/src/de/elmar_baumann/jpt/app/AppInfo.java
@@ -41,7 +41,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.7.25 2010-01-03";
+    public static final String APP_VERSION = "0.7.26 2010-01-03";
     /**
      * The application's name
      */
