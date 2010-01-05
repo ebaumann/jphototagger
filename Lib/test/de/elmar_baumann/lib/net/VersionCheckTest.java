1e59ad49b82f03e21a1ddc6cb2b57ce5750cfd76
ebaumann <ebaumann@localhost>
2010-01-05 02:35:06 +0100

    Started adding HTTP support

---
+++ Lib/test/de/elmar_baumann/lib/net/VersionCheckTest.java
@@ -0,0 +1,53 @@
+package de.elmar_baumann.lib.net;
+
+import de.elmar_baumann.lib.util.Version;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the Class {@link de.elmar_baumann. }.
+ *
+ * @author Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2010/01/05
+ */
+public class VersionCheckTest {
+
+    public VersionCheckTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Test of existsNewer method, of class VersionCheck.
+     * @throws Exception
+     */
+    @Test
+    public void testExistsNewer() throws Exception {
+
+        System.out.println("existsNewer");
+
+        final String urlHtml = "http://localhost/fotografie/tipps/computer/lightroom/imagemetadataviewer.html";
+        final String versionDelimiter = ".";
+
+        Version compareToVersion = new Version(0, 7, 2);
+        boolean expResult        = true;
+        boolean result           = VersionCheck.existsNewer(urlHtml, versionDelimiter, compareToVersion);
+
+        assertEquals(expResult, result);
+
+        compareToVersion = new Version(999999, 0, 0);
+        expResult        = false;
+        result           = VersionCheck.existsNewer(urlHtml, versionDelimiter, compareToVersion);
+
+        assertEquals(expResult, result);
+    }
+
+}
