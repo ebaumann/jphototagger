4cc88f80f55ba0eb42a51aa6fc7856a9f384565a
ebaumann <ebaumann@localhost>
2010-01-05 01:22:15 +0100

    Added support for 2 additional minor version numbers; added #parseVersion() from String

--- Lib/src/de/elmar_baumann/lib/util/Version.java
+++ Lib/src/de/elmar_baumann/lib/util/Version.java
@@ -19,6 +19,7 @@
 package de.elmar_baumann.lib.util;
 
 import java.text.MessageFormat;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -29,11 +30,29 @@ import java.text.MessageFormat;
 public final class Version implements Comparable<Version> {
 
     private final int major;
-    private final int minor;
+    private final int minor1;
+    private final int minor2;
+    private final int minor3;
 
     public Version(int major, int minor) {
-        this.major = major;
-        this.minor = minor;
+        this.major  = major;
+        this.minor1 = minor;
+        this.minor2 = 0;
+        this.minor3 = 0;
+    }
+
+    public Version(int major, int minor1, int minor2) {
+        this.major  = major;
+        this.minor1 = minor1;
+        this.minor2 = minor2;
+        this.minor3 = 0;
+    }
+
+    public Version(int major, int minor1, int minor2, int minor3) {
+        this.major  = major;
+        this.minor1 = minor1;
+        this.minor2 = minor2;
+        this.minor3 = minor3;
     }
 
     public int getMajor() {
@@ -41,20 +60,70 @@ public final class Version implements Comparable<Version> {
     }
 
     public int getMinor() {
-        return minor;
+        return minor1;
+    }
+
+    /**
+     * Creates a {@link Version} object from a string.
+     *
+     * @param version   version string: 2 up to 4 delimited <em>integer numbers</em>,
+     *                  e.g. <code>"0.9.1.5"</code>
+     * @param delimiter number delimiters, e.g. <code>"."</code>
+     * @return          version string
+     * @throws          IllegalArgumentException if the number count is invalid
+     * @throws          NumberFormatException    if the numbers are not parsable
+     *                                           as integers
+     */
+    public static Version parseVersion(String version, String delimiter
+            ) throws IllegalArgumentException, NumberFormatException {
+
+        int             major  = 0;
+        int             minor1 = 0;
+        int             minor2 = 0;
+        int             minor3 = 0;
+        StringTokenizer st     = new StringTokenizer(version, delimiter);
+        int             count  = st.countTokens();
+
+        if (count < 2 || count > 4) throw new IllegalArgumentException("Invalid count");
+
+        int index = 0;
+        while (st.hasMoreTokens()) {
+            int number = Integer.parseInt(st.nextToken().trim());
+            switch (index++) {
+                case 0 : major  = number; break;
+                case 1 : minor1 = number; break;
+                case 2 : minor2 = number; break;
+                case 3 : minor3 = number; break;
+                default: assert false;
+            }
+        }
+        return new Version(major, minor1, minor2, minor3);
     }
 
     @Override
     public int compareTo(Version o) {
-        return major > o.major ? 1
-                : major < o.major ? -1
-                : minor > o.minor ? 1
-                : minor < o.minor ? -1
-                : 0;
+
+        if (major > o.major) return  1;
+        if (major < o.major) return -1;
+
+        // Both major versions are equal
+        if (minor1 > o.minor1) return  1;
+        if (minor1 < o.minor1) return -1;
+
+        // Both first minor versions are equal
+        if (minor2 > o.minor2) return  1;
+        if (minor2 < o.minor2) return -1;
+
+        // Both second minor versions are equal
+        if (minor3 > o.minor3) return  1;
+        if (minor3 < o.minor3) return -1;
+
+        // Both third minor versions are equal
+        return 0;
     }
 
     @Override
     public String toString() {
-        return MessageFormat.format("{0}.{1}", new Object[]{major, minor});
+        return MessageFormat.format("{0}.{1}", new Object[]{major, minor1});
     }
 }
