33448f36161b1e75135e0e65435f2f7386be7c0b
ebaumann <ebaumann@localhost>
2010-01-05 20:56:10 +0100

    Checking for and dowloading a newer version uses the progress bar and a different version information file

--- Program/src/de/elmar_baumann/jpt/app/AppInfo.java
+++ Program/src/de/elmar_baumann/jpt/app/AppInfo.java
@@ -41,7 +41,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.7.28 2010-01-05";
+    public static final String APP_VERSION = "0.7.29 2010-01-06";
     /**
      * The application's name
      */
