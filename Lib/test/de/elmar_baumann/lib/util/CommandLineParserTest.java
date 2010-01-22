12f98c1f53981898054a9afa031e87d543f5ef9f
ebaumann <ebaumann@localhost>
2010-01-22 20:44:46 +0100

    Start: Simple command line parsing class.

---
+++ Lib/test/de/elmar_baumann/lib/util/CommandLineParserTest.java
@@ -0,0 +1,87 @@
+package de.elmar_baumann.lib.util;
+
+import de.elmar_baumann.lib.util.CommandLineParser.Option;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the Class {@link de.elmar_baumann. }.
+ *
+ * @author Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2010/01/22
+ */
+public class CommandLineParserTest {
+
+    public CommandLineParserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Test of hasOption method, of class CommandLineParser.
+     */
+    @Test
+    public void testHasOption() {
+        System.out.println("hasOption");
+        
+        CommandLineParser parser = new CommandLineParser(new String[] { "-delta=0", "-quaffel", "-beta = 7" }, "-", "=");
+
+        String name = "delta";
+        boolean expResult = true;
+        boolean result = parser.hasOption(name);
+        assertEquals(expResult, result);
+
+        name = "gamma";
+        expResult = false;
+        result = parser.hasOption(name);
+        assertEquals(expResult, result);
+
+        name = "beta";
+        expResult = true;
+        result = parser.hasOption(name);
+        assertEquals(expResult, result);
+
+        name = "quaffel";
+        expResult = true;
+        result = parser.hasOption(name);
+        assertEquals(expResult, result);
+
+        parser = new CommandLineParser(new String[] {}, "-", "=");
+        expResult = false;
+        result = parser.hasOption(name);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getOption method, of class CommandLineParser.
+     */
+    @Test
+    public void testGetOption() {
+        System.out.println("getOption");
+
+        CommandLineParser parser = new CommandLineParser(new String[] { "-delta=0", "-quaffel", "-beta = 7 " }, "-", "=");
+
+        String name = "delta";
+        Option result = parser.getOption(name);
+        assertEquals(name, result.getName());
+        assertEquals("0", result.getValues().get(0));
+
+        name = "quaffel";
+        result = parser.getOption(name);
+        assertEquals(name, result.getName());
+
+        name = "beta";
+        result = parser.getOption(name);
+        assertEquals(name, result.getName());
+        assertEquals("7", result.getValues().get(0));
+    }
+
+}
