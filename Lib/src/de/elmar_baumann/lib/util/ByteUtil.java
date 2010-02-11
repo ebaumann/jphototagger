cd2c38bbabe24a9cccfc6eb54b2244751432f8c8
ebaumann <ebaumann@localhost>
2010-02-11 14:49:36 +0100

    Started ByteUtil

---
+++ Lib/src/de/elmar_baumann/lib/util/ByteUtil.java
@@ -0,0 +1,36 @@
+/*
+ * JavaStandardLibrary JSL - subproject of JPhotoTagger
+ * Copyright (C) 2009 by the developer team, resp. Elmar Baumann<eb@elmar-baumann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.elmar_baumann.lib.util;
+
+/**
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2010-02-11
+ */
+public final class ByteUtil {
+
+    public static int toInt(byte b) {
+        int i = b;
+        i &= 0x000000FF;
+        return i;
+    }
+
+    private ByteUtil() {
+    }
+}
