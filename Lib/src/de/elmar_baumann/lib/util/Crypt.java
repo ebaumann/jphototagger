c086f989f3f2b6612cb611b59bae7f2a7d39ae51
ebaumann <ebaumann@localhost>
2010-02-14 18:57:33 +0100

    public not package private

--- Lib/src/de/elmar_baumann/lib/util/Crypt.java
+++ Lib/src/de/elmar_baumann/lib/util/Crypt.java
@@ -9,11 +9,11 @@ import javax.crypto.SecretKey;
 // Code based on: http://www.exampledepot.com/egs/javax.crypto/desstring.html
 
 /**
- * En- and decrypts strings with <code>DESede</code> and BASE64 so that the
- * encrypted string can stored in a plain text file.
- *
+ * En- and decrypts strings with <code>DESede</code> and <code>Base64</code> so
+ * that the encrypted string can be stored into a plain text file, e.g. a
+ * {@link java.util.Properties} file.
+ * <p>
  * Usage example:
- *
  * <pre>
  * String key         = "d?=30sd#fgvjie}[]§$aslkg";
  * byte[] secKeyBytes = new byte[24];
@@ -21,7 +21,7 @@ import javax.crypto.SecretKey;
  * for (int i = 0; i &lt; secKeyBytes.length; i++) {
  *     secKeyBytes[i] = (byte) key.charAt(i);
  * }
- * SecretKey secKey  = new SecretKeySpec(secKeyBytes, "DESede");
+ * SecretKey secKey = new SecretKeySpec(secKeyBytes, "DESede");
  *
  * // Encrypt a string
  * try {
@@ -41,17 +41,18 @@ import javax.crypto.SecretKey;
  * </pre>
  *
  * You can generate a temporary key:
- * {@code SecretKey key = KeyGenerator.getInstance("DESede").generateKey();}.
+ * {@code SecretKey secKey = KeyGenerator.getInstance("DESede").generateKey();}.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2010-02-14
+ * @see     Base64
  */
-final class Crypt {
+public final class Crypt {
 
     private Cipher ecipher;
     private Cipher dcipher;
 
-    Crypt(SecretKey key) {
+    public Crypt(SecretKey key) {
         try {
             ecipher = Cipher.getInstance("DESede");
             dcipher = Cipher.getInstance("DESede");
@@ -66,9 +67,9 @@ final class Crypt {
      * Encrypts a string.
      *
      * @param  str string to encrypt
-     * @return     encrypted string BASE64 encoded or null on errors
+     * @return     encrypted string <code>Base64</code> encoded or null on errors
      */
-    String encrypt(String str) {
+    public String encrypt(String str) {
         try {
             byte[] utf8 = str.getBytes("UTF8");
             byte[] enc = ecipher.doFinal(utf8);
@@ -82,10 +83,10 @@ final class Crypt {
     /**
      * Decrypts a string.
      *
-     * @param  str BASE64 encoded string to dencrypt
+     * @param  str <code>Base64</code> encoded string to dencrypt
      * @return     decrpyted string or null on errors
      */
-    String decrypt(String str) {
+    public String decrypt(String str) {
         try {
             byte[] dec = Base64.decode(str);
             byte[] utf8 = dcipher.doFinal(dec);
