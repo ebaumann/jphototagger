9d27827ff79d73ac2a55b974811cff6a7b461290
ebaumann <ebaumann@localhost>
2010-03-01 20:36:27 +0100

    Added #selectString()

--- Lib/src/de/elmar_baumann/lib/componentutil/ComboBoxUtil.java
+++ Lib/src/de/elmar_baumann/lib/componentutil/ComboBoxUtil.java
@@ -19,6 +19,7 @@
 package de.elmar_baumann.lib.componentutil;
 
 import javax.swing.ComboBoxModel;
+import javax.swing.SwingUtilities;
 
 /**
  * Werkzeuge für Comboboxen.
@@ -42,10 +43,8 @@ public final class ComboBoxUtil {
      * @see            #getItem(ComboBoxModel, String)
      */
     public static boolean hasItem(ComboBoxModel comboBox, String string) {
-        if (comboBox == null)
-            throw new NullPointerException("comboBox == null");
-        if (string == null)
-            throw new NullPointerException("string == null");
+        if (comboBox == null) throw new NullPointerException("comboBox == null");
+        if (string   == null) throw new NullPointerException("string == null");
 
         for (int index = 0; index < comboBox.getSize(); index++) {
             Object currentItem = comboBox.getElementAt(index);
@@ -66,10 +65,8 @@ public final class ComboBoxUtil {
      * @see                 #hasItem(ComboBoxModel, String)
      */
     public static Object getItem(ComboBoxModel comboBoxModel, String string) {
-        if (comboBoxModel == null)
-            throw new NullPointerException("comboBoxModel == null");
-        if (string == null)
-            throw new NullPointerException("string == null");
+        if (comboBoxModel == null) throw new NullPointerException("comboBoxModel == null");
+        if (string        == null) throw new NullPointerException("string == null");
 
         for (int index = 0; index < comboBoxModel.getSize(); index++) {
             Object currentItem = comboBoxModel.getElementAt(index);
@@ -78,6 +75,39 @@ public final class ComboBoxUtil {
             }
         }
         return null;
+    }
+
+    /**
+     * Selects in a combo box model the first item matching a string.
+     * <p>
+     * The <code>toString()</code> method of the combo box items will be
+     * compared against the string.
+     *
+     * @param model  model
+     * @param string string to select
+     */
+    public static void selectString(final ComboBoxModel model, final String string) {
+        if (model  == null) throw new NullPointerException("model == null");
+        if (string == null) throw new NullPointerException("string == null");
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                int    size    = model.getSize();
+                Object selItem = model.getSelectedItem();
+
+                for (int i = 0; i < size; i++) {
+                    Object element = model.getElementAt(i);
+                    if (element instanceof String && ((String) element).equals(string)) {
+                        if (element != selItem) {
+                            model.setSelectedItem(element);
+                        }
+                        return;
+                    }
+                }
+            }
+        });
     }
 
     private ComboBoxUtil() {
