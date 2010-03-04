fd4dd133d1fe2bbbf538e3c24a94d865e965fef6
ebaumann <ebaumann@localhost>
2010-03-04 11:38:43 +0100

    Favorites database notifies listeners on updates

---
+++ Program/src/de/elmar_baumann/jpt/event/DatabaseFavoritesEvent.java
@@ -0,0 +1,73 @@
+/*
+ * JPhotoTagger tags and finds images fast
+ * Copyright (C) 2009 by the developer team, resp. Elmar Baumann<eb@elmar-baumann.de>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.elmar_baumann.jpt.event;
+
+import de.elmar_baumann.jpt.data.Favorite;
+
+/**
+ *
+ *
+ * @author  Elmar Baumann <eb@elmar-baumann.de>
+ * @version 2010-03-04
+ */
+public final class DatabaseFavoritesEvent {
+
+    public enum Type {
+        FAVORITE_INSERTED,
+        FAVORITE_UPDATED,
+        FAVORITE_DELETED;
+    }
+
+    private final Type     type;
+    private final Favorite favorite;
+    private       Favorite oldFavorite;
+
+    public DatabaseFavoritesEvent(Type type, Favorite favorite) {
+        this.type     = type;
+        this.favorite = favorite == null ? null : new Favorite(favorite);
+    }
+
+    public Favorite getFavorite() {
+        return favorite == null ? null : new Favorite(favorite);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Favorite getOldFavorite() {
+        return oldFavorite;
+    }
+
+    public void setOldFavorite(Favorite oldFavorite) {
+        this.oldFavorite = oldFavorite;
+    }
+
+    public boolean isFavoriteInserted() {
+        return type.equals(Type.FAVORITE_INSERTED);
+    }
+
+    public boolean isFavoriteUpdated() {
+        return type.equals(Type.FAVORITE_UPDATED);
+    }
+
+    public boolean isFavoriteDeleted() {
+        return type.equals(Type.FAVORITE_DELETED);
+    }
+}
