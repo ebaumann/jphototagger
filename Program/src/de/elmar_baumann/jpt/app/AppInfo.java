4ae7e948f3ab4b51390bc707ba0cbeb821b7643a
ebaumann <ebaumann@localhost>
2010-03-09 20:04:07 +0100

    Windows: After downloading a newer version, the user will be ask, whether JPhototagger shall call the downloaded installer file

--- Program/src/de/elmar_baumann/jpt/app/AppInfo.java
+++ Program/src/de/elmar_baumann/jpt/app/AppInfo.java
@@ -44,7 +44,7 @@ public final class AppInfo {
     /**
      * The application's version
      */
-    public static final String APP_VERSION = "0.7.92";
+    public static final String APP_VERSION = "0.7.93";
 
     /**
      * The application's name
