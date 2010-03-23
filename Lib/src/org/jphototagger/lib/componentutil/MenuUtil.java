b6ffddb0cf6b48de1f82eaa6ed954fe0cd165d4b
ebaumann <ebaumann@localhost>
2010-03-23 21:34:57 +0100

    No mnemonics on Macintosh systems

--- Lib/src/org/jphototagger/lib/componentutil/MenuUtil.java
+++ Lib/src/org/jphototagger/lib/componentutil/MenuUtil.java
@@ -21,6 +21,8 @@
 
 package org.jphototagger.lib.componentutil;
 
+import org.jphototagger.lib.system.SystemUtil;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -33,9 +35,15 @@ import javax.swing.JMenuItem;
 public final class MenuUtil {
     private static final String AMPERSAND = "&";
 
+    private MenuUtil() {}
+
     /**
      * Calls {@link #setMnemonics(javax.swing.JMenuItem)} for every menu in a
      * menu bar and for all it's items.
+     * <p>
+     * According to <a href="http://developer.apple.com/mac/library/documentation/Java/Conceptual/Java14Development/07-NativePlatformIntegration/NativePlatformIntegration.html">Mac OS X Integration for Java</a>
+     * no mnemonics will be set, if the virtual machine runs on a macintosh
+     * operating system, the mnemonic ampersand will be removed.
      *
      * @param  menuBar menu bar
      * @throws         NullPointerException if <code>menuBar</code> is null
@@ -57,6 +65,10 @@ public final class MenuUtil {
     /**
      * Calls {@link #setMnemonics(javax.swing.JMenuItem)} to a menu and all it's
      * items.
+     * <p>
+     * According to <a href="http://developer.apple.com/mac/library/documentation/Java/Conceptual/Java14Development/07-NativePlatformIntegration/NativePlatformIntegration.html">Mac OS X Integration for Java</a>
+     * no mnemonics will be set, if the virtual machine runs on a macintosh
+     * operating system, the mnemonic ampersand will be removed.
      *
      * @param  menu menu
      * @throws      NullPointerException if <code>menu</code> is null
@@ -84,6 +96,10 @@ public final class MenuUtil {
     /**
      * Sets to a menu item the char behind the first ampersand ({@code &}) as
      * mnemonic and removes the ampersand.
+     * <p>
+     * According to <a href="http://developer.apple.com/mac/library/documentation/Java/Conceptual/Java14Development/07-NativePlatformIntegration/NativePlatformIntegration.html">Mac OS X Integration for Java</a>
+     * no mnemonics will be set, if the virtual machine runs on a macintosh
+     * operating system, the mnemonic ampersand will be removed.
      *
      * @param  item menu item
      * @throws      NullPointerException if <code>item</code> is null
@@ -98,14 +114,32 @@ public final class MenuUtil {
         int    ampersandIndex = itemText.indexOf(AMPERSAND);
 
         if ((ampersandIndex >= 0) && (ampersandIndex < itemTextLength - 1)) {
-            char mnemonic = itemText.charAt(ampersandIndex + 1);
-
-            itemText = itemText.substring(0, ampersandIndex)
-                       + itemText.substring(ampersandIndex + 1, itemTextLength);
-            item.setText(itemText);
-            item.setMnemonic(mnemonic);
+            if (SystemUtil.isMac()) {
+                removeAmpersand(item, itemText, ampersandIndex);
+            } else {
+                removeAmpersandSetMnemonic(item, itemText, ampersandIndex);
+            }
         }
     }
 
-    private MenuUtil() {}
+    private static void removeAmpersandSetMnemonic(JMenuItem item,
+            String itemText, int ampersandIndex) {
+        char mnemonic       = itemText.charAt(ampersandIndex + 1);
+        int  itemTextLength = itemText.length();
+
+        itemText = itemText.substring(0, ampersandIndex)
+                   + itemText.substring(ampersandIndex + 1, itemTextLength);
+        item.setText(itemText);
+        item.setMnemonic(mnemonic);
+    }
+
+    private static void removeAmpersand(JMenuItem item, String itemText,
+            int ampersandIndex) {
+        if (ampersandIndex == 0) {
+            item.setText(itemText.substring(1));
+        } else {
+            item.setText(itemText.substring(0, ampersandIndex)
+                         + itemText.substring(ampersandIndex + 1));
+        }
+    }
 }
