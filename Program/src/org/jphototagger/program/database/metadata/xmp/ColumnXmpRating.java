c5f40d5e46a9c1304cf5f5584ad181f58cac1069
ebaumann <ebaumann@localhost>
2010-04-23 15:50:30 +0200

    ColumnXmpRating returns 0 as minimum rating

--- Program/src/org/jphototagger/program/database/metadata/xmp/ColumnXmpRating.java
+++ Program/src/org/jphototagger/program/database/metadata/xmp/ColumnXmpRating.java
@@ -49,17 +49,17 @@ public final class ColumnXmpRating extends Column {
      * Returns the minimum rating value. Lower values are treated as not
      * rated and should be set to null.
      *
-     * @return minimum rating value.
+     * @return minimum rating value
      */
     public static int getMinValue() {
-        return 1;
+        return 0;
     }
 
     /**
      * Returns the minimum rating value. Higher values shoul be set to this
      * value.
      *
-     * @return minimum rating value.
+     * @return minimum rating value
      */
     public static int getMaxValue() {
         return 5;
