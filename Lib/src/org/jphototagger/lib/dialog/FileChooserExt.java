a144c58772014d15da76ab438b410e1d6b8a7646
ebaumann <ebaumann@localhost>
2010-10-01 15:27:21 +0200

    FileChooserExt returns an applied extension in Save Mode

--- Lib/src/org/jphototagger/lib/dialog/FileChooserExt.java
+++ Lib/src/org/jphototagger/lib/dialog/FileChooserExt.java
@@ -30,20 +30,30 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 /**
- * An extended {@link JFileChooser}:
+ * {@link JFileChooser} with the following extensions (<strong>Save
+ * Mode</strong> means, {@link #getDialogType()} is
+ * {@link JFileChooser#SAVE_DIALOG}):
  *
  * <ul>
- * <li>When saving, the user must confirm if a file exists and
- *    {@link #isConfirmOverwrite()} is true
+ * <li>If an extension, e.g. <code>".xml"</code>, was set through
+ *     {@link #setSaveFilenameExtension(String)}, in <strong>Save Mode</strong>,
+ *     files returned from {@link #getSelectedFile()} or
+ *     {@link #getSelectedFiles()} are having that extension if the user did not
+ *     input it
+ * </li>
+ * <li>In <strong>Save Mode</strong>, the user can be forced to
+ *     <strong>confirm</strong> overwriting existing files through
+ *     {@link #setConfirmOverwrite(boolean)}
  * </li>
  * </ul>
+ * <p>
  *
  * @author Elmar Baumann
  */
 public final class FileChooserExt extends JFileChooser {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2263451265476542816L;
     private boolean           confirmOverwrite;
-    private String            addedFilenameExtension;
+    private String            saveFilenameExtension;
 
     public FileChooserExt(String currentDirectoryPath, FileSystemView fsv) {
         super(currentDirectoryPath, fsv);
@@ -68,9 +78,9 @@ public final class FileChooserExt extends JFileChooser {
     public FileChooserExt() {}
 
     /**
-     * Returns, whether to confirm overwriting existing files.
-     * 
-     * @return true, if confirm
+     * Returns, whether to confirm overwrite existing files in Save Mode.
+     *
+     * @return true, if the user has to confirm overwrite existing files
      * @see    #setConfirmOverwrite(boolean)
      */
     public boolean isConfirmOverwrite() {
@@ -78,47 +88,59 @@ public final class FileChooserExt extends JFileChooser {
     }
 
     /**
-     * Sets the behavior if {@link #getDialogType()} is
-     * {@link JFileChooser#SAVE_DIALOG}, the user selects an existing file and
-     * approves.
+     * Sets whether to confirm overwriting existing files in Save Mode.
      *
-     * @param confirmOverwrite if true, {@link #approveSelection()} displays a
-     *                         confirm dialog to confirm overwriting existing
-     *                         files. Only if overwriting is confirmed, the
-     *                         base class implementation of
-     *                         {@link #approveSelection()} will be called.
-     *                         Default: false.
-     * @see #setAddedFilenameExtension(java.lang.String)
+     * @param confirmOverwrite if true, the user has to confirm overwrite
+     *                         existing files. Default: false.
      */
     public void setConfirmOverwrite(boolean confirmOverwrite) {
         this.confirmOverwrite = confirmOverwrite;
     }
 
     /**
-     * Sets extension which will be added after selecting a file with a save
-     * dialog and the selected file does not have that extension. This extension
-     * will be regognized within {@link #setConfirmOverwrite(boolean)}.
+     * Sets an extension which shall be added to files in Save Mode if the user
+     * did not input that extension.
+     * <p>
+     * {@link #getSelectedFile()} or {@link #getSelectedFiles()} are returning
+     * files having that extension in Save Mode.
+     * <p>
+     * Extensions are treated as <strong>case insensitive</strong> because some
+     * file systems do so: A file named <code>"a.xml"</code> and a file named
+     * <code>"a.XML"</code> are both having the extension <code>".xml"</code>.
      *
-     * @param extension case insensitive extension, e.g. <code>".xml"</code> (a
-     *                  dot will <em>not</em> be added automatically!). Null if
-     *                  no extension will be added to a selected file
+     * @param extension extension, e.g. <code>".xml"</code>. A leading dot will
+     *                  <em>not</em> be added automatically! Null if no
+     *                  extension shall be added to a selected file.
+     *                  Default: null.
      */
-    public void setAddedFilenameExtension(String extension) {
-        addedFilenameExtension = (extension == null)
-                                 ? null
-                                 : extension.trim();
+    public void setSaveFilenameExtension(String extension) {
+        saveFilenameExtension = (extension == null)
+                                ? null
+                                : extension;
+
+        if ((saveFilenameExtension != null)
+                && saveFilenameExtension.isEmpty()) {
+            saveFilenameExtension = null;
+        }
     }
 
     @Override
     public void approveSelection() {
-        if ((getDialogType() == JFileChooser.SAVE_DIALOG) && confirmOverwrite
-                && selSaveFileExists()) {
+        if (isSave() && confirmOverwrite && selSaveFileExists()) {
+
+            // Has to be here, not in the if clause (else
+            // super.approveSelection() in the else branch would be called if
+            // not confirmed)!
             if (confirmOverwrite()) {
                 super.approveSelection();
             }
         } else {
             super.approveSelection();
         }
+    }
+
+    private boolean isSave() {
+        return getDialogType() == JFileChooser.SAVE_DIALOG;
     }
 
     private boolean selSaveFileExists() {
@@ -140,32 +162,82 @@ public final class FileChooserExt extends JFileChooser {
     }
 
     private boolean saveFileExists(File file) {
-        if ((addedFilenameExtension == null)
-                || addedFilenameExtension.isEmpty()) {
+        if (saveFilenameExtension == null) {
             return file.exists();
         }
 
-        String lcFilename = file.getName().toLowerCase();
+        final String path = file.getPath();
 
-        if (lcFilename.endsWith(addedFilenameExtension)) {
+        // Case insensitivity because some file systems are treating the same
+        // lower and upper case characters as equals (A == a, B == b, ...)
+        if (path.toLowerCase().endsWith(saveFilenameExtension.toLowerCase())) {
+
+            // Can lead to multiple files with the same extension but different
+            // cases on case sensitive file systems
             return file.exists();
         } else {
-            return new File(file.getAbsolutePath()
-                            + addedFilenameExtension).exists();
+            return new File(path + saveFilenameExtension).exists();
         }
     }
 
     private boolean confirmOverwrite() {
-        boolean multiSelection = isMultiSelectionEnabled();
-        String  question       = multiSelection
-                                 ? JslBundle.INSTANCE.getString(
-                                     "FileChooserExt.Confirm.OverwriteMultiSel")
-                                 : JslBundle.INSTANCE.getString(
-                                     "FileChooserExt.Confirm.OverwriteSingleSel");
+        boolean multiSel = isMultiSelectionEnabled();
+        String  question = multiSel
+                           ? JslBundle.INSTANCE.getString(
+                               "FileChooserExt.Confirm.OverwriteMultiSel")
+                           : JslBundle.INSTANCE.getString(
+                               "FileChooserExt.Confirm.OverwriteSingleSel");
         String title = JslBundle.INSTANCE.getString(
                            "FileChooserExt.Confirm.OverwriteTitle");
 
         return JOptionPane.showConfirmDialog(null, question, title,
                 JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+    }
+
+    /**
+     * Ensures in Save Mode - if set through
+     * {@link #setSaveFilenameExtension(String)} - that all selected files are
+     * having the set extension.
+     *
+     * @return selected files
+     */
+    @Override
+    public File[] getSelectedFiles() {
+        File[] selFiles = super.getSelectedFiles();
+        int    length   = selFiles.length;
+
+        for (int i = 0; i < length; i++) {
+            selFiles[i] = ensureSaveFilenameExtension(selFiles[i]);
+        }
+
+        return selFiles;
+    }
+
+    /**
+     * Ensures in Save Mode - if set through
+     * {@link #setSaveFilenameExtension(String)} - that the selected file has
+     * the set extension.
+     *
+     * @return selected file
+     */
+    @Override
+    public File getSelectedFile() {
+        return ensureSaveFilenameExtension(super.getSelectedFile());
+    }
+
+    private File ensureSaveFilenameExtension(File file) {
+        if ((file != null) && (saveFilenameExtension != null) && isSave()) {
+            final String path = file.getPath();
+
+            // Case insensitivity because some file systems are treating the
+            // same lower and upper case characters as equals (A == a, B == b,
+            // ...)
+            if (!path.toLowerCase().endsWith(
+                    saveFilenameExtension.toLowerCase())) {
+                return new File(path + saveFilenameExtension);
+            }
+        }
+
+        return file;
     }
 }
