e3ad47192c4e0c9be4819addae4372f698c5dee4
ebaumann <ebaumann@localhost>
2011-05-07 22:18:37 +0200

    Added utility classes for script thumbnail creators

---
+++ ExternalThumbnailCreators/DefaultExternalThumbnailCreators/src/org/jphototagger/dtncreators/FileChooser.java
@@ -0,0 +1,98 @@
+package org.jphototagger.dtncreators;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import org.jphototagger.lib.io.FileUtil;
+import org.jphototagger.lib.io.FileUtil.FileChooserProperties;
+import org.jphototagger.lib.io.filefilter.AcceptExactFilenameNameFileFilter;
+
+/**
+ *
+ *
+ * @author Elmar Baumann
+ */
+public final class FileChooser {
+
+    private final String fixedFileFilename;
+    private final String fileDescription;
+    private final String fileChooserTitle;
+    private String fileChooserDirPath;
+
+    private FileChooser(Builder builder) {
+        fixedFileFilename = builder.fixedFileName;
+        fileDescription = builder.fileDescription;
+        fileChooserTitle = builder.fileChooserTitle;
+        fileChooserDirPath = builder.fileChooserDirPath;
+    }
+    
+    public File chooseFileFixedName() {
+        FileChooserProperties fcProps = new FileChooserProperties();
+
+        fcProps.dialogTitle(fileChooserTitle);
+        fcProps.currentDirectoryPath(fileChooserDirPath);
+        fcProps.multiSelectionEnabled(false);
+        fcProps.fileFilter(createFileFilter());
+        fcProps.fileSelectionMode(JFileChooser.FILES_ONLY);
+
+        File file = FileUtil.chooseFile(fcProps);
+        
+        if (file != null) {
+            fileChooserDirPath = file.getParentFile().getAbsolutePath();
+        }
+        
+        return file;
+    }
+    
+    private FileFilter createFileFilter() {
+        AcceptExactFilenameNameFileFilter filter = new AcceptExactFilenameNameFileFilter(fixedFileFilename);
+
+        return filter.forFileChooser(fileDescription);
+    }
+    
+    public static class Builder {
+        private final String fixedFileName;
+        private String fileDescription = "";
+        private String fileChooserTitle = "";
+        private String fileChooserDirPath = "";
+
+        public Builder(String fixedFileName) {
+            if (fixedFileName == null) {
+                throw new NullPointerException("fixedFileName == null");
+            }
+            
+            this.fixedFileName = fixedFileName;
+        }
+
+        public Builder fileChooserDirPath(String fileChooserDirPath) {
+            if (fileChooserDirPath == null) {
+                throw new NullPointerException("fileChooserDirPath == null");
+            }
+            
+            this.fileChooserDirPath = fileChooserDirPath;
+            return this;
+        }
+
+        public Builder fileChooserTitle(String fileChooserTitle) {
+            if (fileChooserTitle == null) {
+                throw new NullPointerException("fileChooserTitle == null");
+            }
+            
+            this.fileChooserTitle = fileChooserTitle;
+            return this;
+        }
+
+        public Builder fileDescription(String fileDescription) {
+            if (fileDescription == null) {
+                throw new NullPointerException("fileDescription == null");
+            }
+            
+            this.fileDescription = fileDescription;
+            return this;
+        }
+        
+        public FileChooser build() {
+            return new FileChooser(this);
+        }
+    }
+}
