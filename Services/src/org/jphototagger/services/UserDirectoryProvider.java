011fab678d97576036016c2282d48b8d14231455
ebaumann <ebaumann@localhost>
2011-05-08 00:04:35 +0200

    Added UserDirectoryProvider + Implementation

---
+++ Services/src/org/jphototagger/services/UserDirectoryProvider.java
@@ -0,0 +1,13 @@
+package org.jphototagger.services;
+
+import java.io.File;
+
+/**
+ *
+ *
+ * @author Elmar Baumann
+ */
+public interface UserDirectoryProvider {
+
+    File getUserDirectory();
+}
