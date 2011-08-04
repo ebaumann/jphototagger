a15c4505522570ec904be20e741b75ecc94f00be
ebaumann <ebaumann@localhost>
2011-08-04 18:06:28 +0200

    ImagePanel can display text if the image is absent

--- Lib/src/org/jphototagger/lib/component/ImagePanel.java
+++ Lib/src/org/jphototagger/lib/component/ImagePanel.java
@@ -1,64 +1,155 @@
 package org.jphototagger.lib.component;
 
+import java.awt.Dimension;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.JPanel;
 
 /**
- * Displays an image centered whitin it's previous set area. Draws the empty
- * background if the image is null.
- *
- * All functions with object-reference-parameters are throwing a
- * <code>NullPointerException</code> if an object reference is null and it is
- * not documentet that it can be null.
+ * Displays an image centered or the background color if the image is null.
  *
  * @author Elmar Baumann
  */
 public class ImagePanel extends JPanel {
+
     private static final long serialVersionUID = 6103175417678650145L;
     private Image image;
-    private Align align = Align.CENTER;
-
-    public static enum Align { CENTER, LEFT_TOP, }
+    private String imageIsAbsentText;
 
     /**
-     * Sets the image.
      *
-     * @param image  image, can be null
+     * @param image can be null
      */
     public void setImage(Image image) {
+        Image oldImage = this.image;
         this.image = image;
+        firePropertyChange("image", oldImage, image);
     }
 
-    public void setAlign(Align align) {
-        if (align == null) {
-            throw new NullPointerException("align == null");
-        }
+    public Image getImage() {
+        return image;
+    }
 
-        this.align = align;
+    public String getImageIsAbsentText() {
+        return imageIsAbsentText;
+    }
+
+    /**
+     *
+     * @param imageIsAbsentText can be null
+     */
+    public void setImageIsAbsentText(String imageIsAbsentText) {
+        String oldText = this.imageIsAbsentText;
+        this.imageIsAbsentText = imageIsAbsentText;
+        firePropertyChange("imageIsAbsentText", oldText, imageIsAbsentText);
     }
 
     @Override
     public void paint(Graphics g) {
-        g.setColor(getBackground());
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
 
-        int width = getWidth();
-        int height = getHeight();
+        eraseBackground(g, panelWidth, panelHeight);
 
         if (image != null) {
-            int imgHeight = image.getHeight(this);
-            int imgWidth = image.getWidth(this);
-            int x = ((imgWidth < width) && align.equals(Align.CENTER)
-                     ? (width - imgWidth) / 2
-                     : 0);
-            int y = ((imgHeight < height) && align.equals(Align.CENTER)
-                     ? (height - imgHeight) / 2
-                     : 0);
-
-            g.fillRect(0, 0, width, height);
-            g.drawImage(image, x, y, this);
+            drawImage(panelWidth, panelHeight, g);
         } else {
-            g.drawRect(0, 0, width, height);
+            drawImageIsAbsentText(g);
         }
+    }
+
+    private void eraseBackground(Graphics g, int panelWidth, int panelHeight) {
+        g.setColor(getBackground());
+        g.fillRect(0, 0, panelWidth, panelHeight);
+    }
+
+    private void drawImage(int panelWidth, int panelHeight, Graphics g) {
+        Dimension imgageDimension = scaleImageIfSizeExceedsPanel();
+        int imageWidth = imgageDimension.width;
+        int imageHeight = imgageDimension.height;
+        int x = getImageX(imageWidth, panelWidth);
+        int y = getImageY(imageHeight, panelHeight);
+
+        g.drawImage(image, x, y, this);
+    }
+
+    private int getImageX(int imageWidth, int panelWidth) {
+        return imageWidth < panelWidth
+                ? (panelWidth - imageWidth) / 2
+                : 0;
+    }
+
+    private int getImageY(int imageHeight, int panelHeight) {
+        return imageHeight < panelHeight
+                ? (panelHeight - imageHeight) / 2
+                : 0;
+    }
+
+    private Dimension scaleImageIfSizeExceedsPanel() {
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+        int imageHeight = image.getHeight(this);
+        int imageWidth = image.getWidth(this);
+
+        if (imageHeight > panelHeight || imageWidth > panelWidth) {
+            double scaleFactor = getMaxScaleFactor(image);
+            if (scaleFactor != 0) {
+                int scaledWidth = (int) (imageWidth * scaleFactor);
+                int scaledHeight = (int) (imageHeight * scaleFactor);
+                image = image.getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+                imageHeight = image.getHeight(this);
+                imageWidth = image.getWidth(this);
+            }
+        }
+
+        return new Dimension(imageWidth, imageHeight);
+    }
+
+    private double getMaxScaleFactor(Image image) {
+        double imageWidth = image.getWidth(this);
+        double imageHeight = image.getHeight(this);
+        double panelWidth = getWidth();
+        double panelHeight = getHeight();
+        boolean panelIsLandscape = panelWidth > panelHeight;
+
+        if (panelIsLandscape) {
+            double scaleFactor = panelHeight / imageHeight;
+            double newImageWidth = imageWidth * scaleFactor;
+            return newImageWidth <= panelWidth
+                    ? scaleFactor
+                    : panelWidth / imageWidth;
+        } else {
+            double scaleFactor = panelWidth / imageWidth;
+            double newImageHeight = imageHeight * scaleFactor;
+            return newImageHeight <= panelHeight
+                    ? scaleFactor
+                    : panelHeight / imageHeight;
+        }
+    }
+
+    private void drawImageIsAbsentText(Graphics g) {
+        if (imageIsAbsentText == null) {
+            return;
+        }
+
+        double panelWidth = getWidth();
+        double panelHeight = getHeight();
+        FontMetrics fm = g.getFontMetrics();
+        double textWidth = fm.stringWidth(imageIsAbsentText);
+        double textHeight = fm.getAscent();
+        int x = 0;
+        int y = (int) textHeight;
+
+        if (textWidth < panelWidth) {
+            x = (int) ((panelWidth - textWidth) / 2.0 + 0.5);
+        }
+
+        if (textHeight < panelHeight) {
+            y = (int) (textHeight + (panelHeight - textHeight) / 2.0 + 0.5);
+        }
+
+        g.setColor(getForeground());
+        g.drawString(imageIsAbsentText, x, y);
     }
 }
