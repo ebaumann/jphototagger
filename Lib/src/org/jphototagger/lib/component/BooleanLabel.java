2db1ca8c932bda35ed73b46fc59418c3bd88ecfa
ebaumann <ebaumann@localhost>
2011-08-06 02:28:55 +0200

    BooleanLabel can set different texts for true and false

--- Lib/src/org/jphototagger/lib/component/BooleanLabel.java
+++ Lib/src/org/jphototagger/lib/component/BooleanLabel.java
@@ -1,11 +1,12 @@
 package org.jphototagger.lib.component;
 
 import javax.swing.Icon;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import org.jphototagger.lib.image.util.IconUtil;
 
 /**
- * Displays an icon indicating whether something is true or not before a text
+ * Displays special icons and/or texts indicating whether something is true
  * and shall be a repacement for a {@link JCheckBox} which is used only for
  * that approach (in it's disabled state the text is greyed out).
  *
@@ -16,6 +17,8 @@ public final class BooleanLabel extends JLabel {
     private static final long serialVersionUID = 1L;
     private Icon trueIcon = IconUtil.getImageIcon(BooleanLabel.class, "icon_boolean_true.png");
     private Icon falseIcon = IconUtil.getImageIcon(BooleanLabel.class, "icon_boolean_false.png");
+    private String trueText;
+    private String falseText;
     private Boolean isTrue;
 
     public BooleanLabel() {
@@ -34,7 +37,7 @@ public final class BooleanLabel extends JLabel {
     public void setFalseIcon(Icon falseIcon) {
         Icon old = this.falseIcon;
         this.falseIcon = falseIcon;
-        firePropertyChange("falseIcon", old, falseIcon);
+        firePropertyChange("falseIcon", old, this.falseIcon);
     }
 
     public Icon getTrueIcon() {
@@ -44,7 +47,7 @@ public final class BooleanLabel extends JLabel {
     public void setTrueIcon(Icon trueIcon) {
         Icon old = this.trueIcon;
         this.trueIcon = trueIcon;
-        firePropertyChange("trueIcon", old, trueIcon);
+        firePropertyChange("trueIcon", old, this.trueIcon);
     }
 
     public Boolean isTrue() {
@@ -55,6 +58,48 @@ public final class BooleanLabel extends JLabel {
         Boolean old = this.isTrue;
         this.isTrue = isTrue;
         setIcon(isTrue ? trueIcon : falseIcon);
-        firePropertyChange("isTrue", old, isTrue);
+        firePropertyChange("isTrue", old, this.isTrue);
+    }
+
+    @Override
+    public String getText() {
+        if (trueText != null && falseText != null) {
+            return isTrue == null ? falseText : isTrue ? trueText : falseText;
+        }
+        return super.getText();
+    }
+
+    public String getTrueText() {
+        return trueText;
+    }
+
+    /**
+     * Will be used only if {@link #getFalseText()} is not null.
+     *
+     * @param trueText
+     */
+    public void setTrueText(String trueText) {
+        String oldTrueText = this.trueText;
+        String oldText = getText();
+        this.trueText = trueText;
+        firePropertyChange("trueText", oldTrueText, this.trueText);
+        firePropertyChange("text", oldText, getText());
+    }
+
+    public String getFalseText() {
+        return falseText;
+    }
+
+    /**
+     * Will be used only if {@link #getTrueText()} is not null.
+     *
+     * @param falseText
+     */
+    public void setFalseText(String falseText) {
+        String oldFalseText = this.falseText;
+        String oldText = getText();
+        this.falseText = falseText;
+        firePropertyChange("falseText", oldFalseText, this.falseText);
+        firePropertyChange("text", oldText, getText());
     }
 }
