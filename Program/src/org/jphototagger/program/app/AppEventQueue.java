94ca7b0e28623f248d60f9e9f1c76992b9ca96b1
ebaumann <ebaumann@localhost>
2011-09-08 21:10:01 +0200

    AppEventQueue does only log the Exception. The ErrorLogHandler class displays an error label in the AppFrame.

--- Program/src/org/jphototagger/program/app/AppEventQueue.java
+++ Program/src/org/jphototagger/program/app/AppEventQueue.java
@@ -1,20 +1,9 @@
 package org.jphototagger.program.app;
 
 import java.awt.AWTEvent;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.jphototagger.lib.dialog.LongMessageDialog;
-import org.jphototagger.lib.io.FileUtil;
-import org.jphototagger.lib.util.Bundle;
-import org.jphototagger.lib.util.SystemProperties;
-import org.jphototagger.program.app.logging.AppLogUtil;
-import org.jphototagger.program.app.logging.AppLoggingSystem;
-import org.jphototagger.program.resource.GUI;
 import org.jphototagger.program.view.WaitDisplay;
 
 /**
@@ -26,51 +15,13 @@ import org.jphototagger.program.view.WaitDisplay;
  */
 public final class AppEventQueue extends java.awt.EventQueue {
 
-    private static final String NEWLINE = SystemProperties.getLineSeparator();
-    private static final String FILE_ENCODING = SystemProperties.getFileEncoding();
-
     @Override
     protected void dispatchEvent(AWTEvent event) {
         try {
             super.dispatchEvent(event);
         } catch (Throwable t) {
             Logger.getLogger(AppEventQueue.class.getName()).log(Level.SEVERE, null, t);
-            getDialog(t).setVisible(true);
             hideWaitDisplay();
-        }
-    }
-
-    private LongMessageDialog getDialog(Throwable t) {
-        LongMessageDialog dlg = new LongMessageDialog(GUI.getAppFrame(), true);
-
-        dlg.setTitle(Bundle.getString(AppEventQueue.class, "AppEventQueue.Error.Title"));
-        dlg.setErrorIcon();
-        dlg.setMail(AppInfo.MAIL_TO_ADDRESS_BUGS, AppInfo.MAIL_SUBJECT_BUGS);
-        dlg.setShortMessage(Bundle.getString(AppEventQueue.class, "AppEventQueue.Error.Message"));
-        dlg.setLongMessage(createMessage(t));
-
-        return dlg;
-    }
-
-    private String createMessage(Throwable t) {
-        String message = AppLogUtil.createMessage(t);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-
-        t.printStackTrace(ps);
-
-        return message + NEWLINE + baos.toString() + NEWLINE + getAllMessages();
-    }
-
-    private String getAllMessages() {
-        File logfileAllMessages = new File(AppLoggingSystem.geLogfilePathAllMessages());
-
-        try {
-            return FileUtil.getContentAsString(logfileAllMessages, FILE_ENCODING);
-        } catch (IOException ex) {
-            Logger.getLogger(AppEventQueue.class.getName()).log(Level.SEVERE, null, ex);
-
-            return "";
         }
     }
 
