edb039cc09edfd72575445c990080c253ea0ef01
ebaumann <ebaumann@localhost>
2011-09-13 19:01:01 +0200

    Added doc how to retrieve the maximum width

--- API/src/org/jphototagger/api/image/ThumbnailCreator.java
+++ API/src/org/jphototagger/api/image/ThumbnailCreator.java
@@ -4,7 +4,14 @@ import java.awt.Image;
 import java.io.File;
 
 /**
- *
+ * Creates a thumbnail, can extend JPhotoTagger's capability
+ * generating thumbnails for various file formats.
+ * <p>
+ * Hint getting the preferred maximum width from the user settings:
+ * <pre>
+ * Storage storage = Lookup.getDefault().lookup(Storage.class);
+ * int width = storage.getInt(Storage.KEY_MAX_THUMBNAIL_WIDTH);
+ * </pre>
  *
  * @author Elmar Baumann
  */
