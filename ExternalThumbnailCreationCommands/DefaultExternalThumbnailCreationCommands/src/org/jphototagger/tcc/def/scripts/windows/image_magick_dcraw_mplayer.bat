0d899a65ce9ddf4b47f029765edb5067fc39790b
ebaumann <ebaumann@localhost>
2011-09-18 21:38:43 +0200

    Bugfix: image_magick_dcraw_mplayer.bat

--- ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/scripts/windows/image_magick_dcraw_mplayer.bat
+++ ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/scripts/windows/image_magick_dcraw_mplayer.bat
@@ -9,13 +9,6 @@ set MAX_DIM=%2
 rem Extracting image suffix
 for %%i in (%IMAGE%) DO set THUMB_SUFFIX=%%~xi
 
-rem Deciding whether RAW or not 
-if /I "%THUMB_SUFFIX%" ==".jpg" GOTO OTHER
-if /I "%THUMB_SUFFIX%" ==".tif" GOTO OTHER
-if /I "%THUMB_SUFFIX%" ==".gif" GOTO OTHER
-if /I "%THUMB_SUFFIX%" ==".png" GOTO OTHER
-if /I "%THUMB_SUFFIX%" ==".psd" GOTO OTHER
-
 rem Video
 if /I "%THUMB_SUFFIX%" ==".m2ts" GOTO VIDEO
 if /I "%THUMB_SUFFIX%" ==".mts" GOTO VIDEO
@@ -27,6 +20,8 @@ if /I "%THUMB_SUFFIX%" ==".mov" GOTO VIDEO
 if /I "%THUMB_SUFFIX%" ==".wmv" GOTO VIDEO
 if /I "%THUMB_SUFFIX%" ==".avi" GOTO VIDEO
 
+GOTO IMAGES
+
 :VIDEO
 set TEMP_VIDEO_FILENAME=00000001.jpg
 cd "%TMP%"
@@ -34,6 +29,14 @@ cd "%TMP%"
 type %TEMP_VIDEO_FILENAME%
 del %TEMP_VIDEO_FILENAME%
 goto END
+
+:IMAGES
+rem Deciding whether RAW or not 
+if /I "%THUMB_SUFFIX%" ==".jpg" GOTO OTHER
+if /I "%THUMB_SUFFIX%" ==".tif" GOTO OTHER
+if /I "%THUMB_SUFFIX%" ==".gif" GOTO OTHER
+if /I "%THUMB_SUFFIX%" ==".png" GOTO OTHER
+if /I "%THUMB_SUFFIX%" ==".psd" GOTO OTHER
 
 "${dcraw.exe}" -e -c %IMAGE% | "${convert.exe}" - -thumbnail %MAX_DIM%x%MAX_DIM% -auto-orient jpg:-
 goto END
