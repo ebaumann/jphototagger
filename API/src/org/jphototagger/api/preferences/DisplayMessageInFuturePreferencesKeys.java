b5b5c9b57e4fdabb0d663d8545d613cc44185752
ebaumann <ebaumann@localhost>
2011-09-19 19:50:21 +0200

    Added KeyInfo

--- API/src/org/jphototagger/api/preferences/DisplayMessageInFuturePreferencesKeys.java
+++ API/src/org/jphototagger/api/preferences/DisplayMessageInFuturePreferencesKeys.java
@@ -10,5 +10,32 @@ import java.util.Set;
  */
 public interface DisplayMessageInFuturePreferencesKeys {
 
-    Set<String> getPreferencesKeys();
+    public static final class KeyInfo {
+
+        private final String key;
+        private final String localizedDisplayName;
+
+        public KeyInfo(String key, String localizedDisplayName) {
+            if (key == null) {
+                throw new NullPointerException("key == null");
+            }
+
+            if (localizedDisplayName == null) {
+                throw new NullPointerException("localizedDisplayName == null");
+            }
+
+            this.key = key;
+            this.localizedDisplayName = localizedDisplayName;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getLocalizedDisplayName() {
+            return localizedDisplayName;
+        }
+    }
+
+    Set<KeyInfo> getKeyInfos();
 }
