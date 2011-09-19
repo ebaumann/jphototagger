019b3eed327b271f70033acae0709c564c746d61
ebaumann <ebaumann@localhost>
2011-09-19 19:39:47 +0200

    Added DisplayInFuturePreferencesKeys

---
+++ API/src/org/jphototagger/api/preferences/DisplayInFuturePreferencesKeys.java
@@ -0,0 +1,14 @@
+package org.jphototagger.api.preferences;
+
+import java.util.Set;
+
+/**
+ * Keys whith a boolean value of the meaning: Display something
+ * in future or not.
+ *
+ * @author Elmar Baumann
+ */
+public interface DisplayInFuturePreferencesKeys {
+
+    Set<String> getPreferencesKeys();
+}
