6edeefb67901ecd68934ce9f9225345b97c6c59b
ebaumann <ebaumann@localhost>
2011-09-23 00:41:56 +0200

    Added DeleteOutOfDateFiles

---
+++ Lib/src/org/jphototagger/lib/io/DeleteOutOfDateFiles.java
@@ -0,0 +1,63 @@
+package org.jphototagger.lib.io;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jphototagger.api.concurrent.CancelRequest;
+
+/**
+ * Based on File.lastModified()
+ *
+ * @author Elmar Baumann
+ */
+public final class DeleteOutOfDateFiles {
+
+    private final long deleteIfNMillisecondsOlderThanNow;
+    private final Collection<? extends File> files;
+    private static final Logger LOGGER = Logger.getLogger(DeleteOutOfDateFiles.class.getName());
+
+    public DeleteOutOfDateFiles(Collection<? extends File> files, long deleteIfNMillisecondsOlderThanNow) {
+        if (files == null) {
+            throw new NullPointerException("files == null");
+        }
+
+        if (deleteIfNMillisecondsOlderThanNow < 0) {
+            throw new IllegalArgumentException("Negative millisecond difference: " + deleteIfNMillisecondsOlderThanNow);
+        }
+
+        this.files = new ArrayList<File>(files);
+        this.deleteIfNMillisecondsOlderThanNow = deleteIfNMillisecondsOlderThanNow;
+    }
+
+    /**
+     *
+     * @param cancelRequest  may be null
+     * @return count of deleted files
+     */
+    public int start(CancelRequest cancelRequest) {
+        int countDeleted = 0;
+        for (File file : files) {
+            if (cancelRequest != null && cancelRequest.isCancel()) {
+                return countDeleted;
+            }
+
+            long lastModifiedInMilliseconds = file.lastModified();
+            long nowInMilliseconds = System.currentTimeMillis();
+            long minMilliseconds = nowInMilliseconds - deleteIfNMillisecondsOlderThanNow;
+            boolean isTooOld = lastModifiedInMilliseconds < minMilliseconds;
+
+            if (isTooOld) {
+                LOGGER.log(Level.FINEST, "Deleting file ''{0}'' because it is older than {1} milliseconds", new Object[]{file, deleteIfNMillisecondsOlderThanNow});
+                boolean deleted = file.delete();
+                if (deleted) {
+                    countDeleted++;
+                } else {
+                    LOGGER.log(Level.WARNING, "Could not delete file ''{0}''", file);
+                }
+            }
+        }
+        return countDeleted;
+    }
+}
---
+++ Lib/test/org/jphototagger/lib/io/DeleteOutOfDateFilesTest.java
@@ -0,0 +1,41 @@
+package org.jphototagger.lib.io;
+
+import org.junit.Test;
+import java.io.File;
+import java.util.Arrays;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Elmar Baumann
+ */
+public class DeleteOutOfDateFilesTest {
+
+    @Test
+    public void testStart() throws Exception {
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        String filename = tmpDir + File.separator + "DeleteOutOfDateFilesTest";
+        File file = new File(filename);
+
+        try {
+            file.createNewFile();
+            long createLastModified = file.lastModified();
+
+            // The following block may fail, shall not under "usual" circumstances
+            DeleteOutOfDateFiles deleteOutOfDateFiles = new DeleteOutOfDateFiles(Arrays.asList(file), 3000L);
+            assertTrue(file.exists());
+            int countDeleted = deleteOutOfDateFiles.start(null);
+            assertEquals(0, countDeleted);
+            assertTrue(file.exists());
+
+            Thread.sleep(3500);
+            long nextLastModified = file.lastModified();
+            assertEquals(createLastModified, nextLastModified);
+            countDeleted = deleteOutOfDateFiles.start(null);
+            assertEquals(1, countDeleted);
+            assertFalse(file.exists());
+        } finally {
+            file.delete();
+        }
+    }
+}
