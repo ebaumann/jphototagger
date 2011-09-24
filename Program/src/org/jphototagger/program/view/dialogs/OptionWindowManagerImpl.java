5d3a3833f11166d51c027f8d3f99c0ef7a3b6459
ebaumann <ebaumann@localhost>
2011-09-25 01:23:13 +0200

    Bugfix: addSettingsComponent() within EDT

--- Program/src/org/jphototagger/program/view/dialogs/OptionWindowManagerImpl.java
+++ Program/src/org/jphototagger/program/view/dialogs/OptionWindowManagerImpl.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import org.openide.util.lookup.ServiceProvider;
 
 import org.jphototagger.api.windows.OptionWindowManager;
+import org.jphototagger.lib.awt.EventQueueUtil;
 
 /**
  *
@@ -15,7 +16,13 @@ import org.jphototagger.api.windows.OptionWindowManager;
 public final class OptionWindowManagerImpl implements OptionWindowManager {
 
     @Override
-    public void addSettingsComponent(Component component, String title) {
-        SettingsDialog.INSTANCE.addTabToMiscSettings(component, title);
+    public void addSettingsComponent(final Component component, final String title) {
+        EventQueueUtil.invokeInDispatchThread(new Runnable() {
+
+            @Override
+            public void run() {
+                SettingsDialog.INSTANCE.addTabToMiscSettings(component, title);
+            }
+        });
     }
 }
