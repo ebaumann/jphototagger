c5b2244f93fbb840f9bf6b889e7e647147398d39
ebaumann <ebaumann@localhost>
2011-10-04 17:30:52 +0200

    ActionsAfterSavesOrUpdatesInRepositoryListModel listens and acts to events rather than producing them due repository insertions

--- Program/src/org/jphototagger/program/model/ActionsAfterSavesOrUpdatesInRepositoryListModel.java
+++ Program/src/org/jphototagger/program/model/ActionsAfterSavesOrUpdatesInRepositoryListModel.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 import javax.swing.DefaultListModel;
 
+import org.openide.util.Lookup;
+
 import org.bushe.swing.event.annotation.AnnotationProcessor;
 import org.bushe.swing.event.annotation.EventSubscriber;
-
-import org.openide.util.Lookup;
 
 import org.jphototagger.domain.programs.Program;
 import org.jphototagger.domain.repository.ActionsAfterRepoUpdatesRepository;
 import org.jphototagger.domain.repository.Repository;
 import org.jphototagger.domain.repository.event.programs.ProgramDeletedEvent;
 import org.jphototagger.domain.repository.event.programs.ProgramUpdatedEvent;
+import org.jphototagger.domain.repository.event.repoupdates.ActionAfterRepoUpdateDeletedEvent;
+import org.jphototagger.domain.repository.event.repoupdates.ActionAfterRepoUpdateInsertedEvent;
+import org.jphototagger.domain.repository.event.repoupdates.ActionsAfterRepoUpdateReorderedEvent;
 import org.jphototagger.lib.awt.EventQueueUtil;
-import org.jphototagger.lib.componentutil.ListUtil;
-import org.jphototagger.lib.dialog.MessageDisplayer;
-import org.jphototagger.lib.util.Bundle;
 
 /**
  *
@@ -31,93 +31,7 @@ public final class ActionsAfterSavesOrUpdatesInRepositoryListModel extends Defau
 
     public ActionsAfterSavesOrUpdatesInRepositoryListModel() {
         addElements();
-        AnnotationProcessor.process(this);
-    }
-
-    public void insert(final Program action) {
-        if (action == null) {
-            throw new NullPointerException("action == null");
-        }
-
-        assert action.isAction() : action;
-
-        if (!contains(action) && actionsRepo.saveAction(action, getSize())) {
-            addElement(action);
-        } else {
-            errorMessageInsert(action);
-        }
-    }
-
-    public void moveUp(final int index) {
-        if (canMoveUp(index)) {
-            swapElements(index, index - 1);
-        }
-    }
-
-    public void moveDown(final int index) {
-        if (canMoveDown(index)) {
-            swapElements(index, index + 1);
-        }
-    }
-
-    public boolean canMoveUp(int index) {
-        return (index > 0) && (index < getSize());
-    }
-
-    public boolean canMoveDown(int index) {
-        return (index >= 0) && (index < getSize() - 1);
-    }
-
-    private List<Program> getActions() {
-        Object[] array = toArray();
-        List<Program> actions = new ArrayList<Program>(array.length);
-
-        for (Object o : array) {
-            actions.add((Program) o);
-        }
-
-        return actions;
-    }
-
-    private void swapElements(int indexFirstElement, int indexSecondElement) {
-        if (ListUtil.swapModelElements(this, indexFirstElement, indexSecondElement)) {
-            fireContentsChanged(this, indexFirstElement, indexFirstElement);
-            fireContentsChanged(this, indexSecondElement, indexSecondElement);
-
-            if (!actionsRepo.setActionOrder(getActions(), 0)) {
-                errorMessageSwap(indexFirstElement);
-            }
-        }
-    }
-
-    public void delete(final Program action) {
-        if (action == null) {
-            throw new NullPointerException("action == null");
-        }
-
-        if (contains(action) && actionsRepo.deleteAction(action)) {
-            removeElement(action);
-        } else {
-            errorMessageDelete(action);
-        }
-    }
-
-    private void updateProgram(Program program) {
-        int index = indexOf(program);
-
-        if (index >= 0) {
-            set(index, program);
-            fireContentsChanged(this, index, index);
-        }
-    }
-
-    private void deleteProgram(Program program) {
-        int index = indexOf(program);
-
-        if (index >= 0) {
-            removeElementAt(index);
-            fireIntervalRemoved(this, index, index);
-        }
+        listen();
     }
 
     private void addElements() {
@@ -134,20 +48,54 @@ public final class ActionsAfterSavesOrUpdatesInRepositoryListModel extends Defau
         }
     }
 
-    private void errorMessageDelete(Program action) {
-        String message = Bundle.getString(ActionsAfterSavesOrUpdatesInRepositoryListModel.class, "ActionsAfterSavesOrUpdatesInRepositoryListModel.Error.Remove", action);
-        MessageDisplayer.error(null, message);
+    private void listen() {
+        AnnotationProcessor.process(this);
+        }
+
+    @EventSubscriber(eventClass = ActionAfterRepoUpdateInsertedEvent.class)
+    public void actionInserted(ActionAfterRepoUpdateInsertedEvent event) {
+        Program action = event.getAction();
+        addElement(action);
+        }
+
+    @EventSubscriber(eventClass = ActionAfterRepoUpdateDeletedEvent.class)
+    public void actionDeleted(ActionAfterRepoUpdateDeletedEvent event) {
+        Program action = event.getAction();
+        removeElement(action);
     }
 
-    private void errorMessageSwap(int indexFirstElement) {
-        Program program = (Program) get(indexFirstElement);
-        String message = Bundle.getString(ActionsAfterSavesOrUpdatesInRepositoryListModel.class, "ActionsAfterSavesOrUpdatesInRepositoryListModel.Error.Swap", program);
-        MessageDisplayer.error(null, message);
+    @EventSubscriber(eventClass = ActionsAfterRepoUpdateReorderedEvent.class)
+    public void actionsReordered(ActionsAfterRepoUpdateReorderedEvent evt) {
+        removeAllElements();
+        addElements();
     }
 
-    private void errorMessageInsert(Program action) {
-        String message = Bundle.getString(ActionsAfterSavesOrUpdatesInRepositoryListModel.class, "ActionsAfterSavesOrUpdatesInRepositoryListModel.Error.Add", action);
-        MessageDisplayer.error(null, message);
+    public List<Program> getActions() {
+        Object[] array = toArray();
+        List<Program> actions = new ArrayList<Program>(array.length);
+
+        for (Object o : array) {
+            actions.add((Program) o);
+        }
+
+        return actions;
+    }
+
+    private void updateProgram(Program program) {
+        int index = indexOf(program);
+
+        if (index >= 0) {
+            set(index, program);
+            fireContentsChanged(this, index, index);
+        }
+    }
+
+    private void deleteProgram(Program program) {
+        int index = indexOf(program);
+
+        if (index >= 0) {
+            removeElementAt(index);
+        }
     }
 
     @EventSubscriber(eventClass = ProgramDeletedEvent.class)
