4c7026b57cbb3e0998ce7dd4519123a97f275a9e
ebaumann <ebaumann@localhost>
2011-10-06 19:09:42 +0200

    Moved Cleanup into AppLifeCycle (was and should not used from others)

--- Program/src/org/jphototagger/program/app/AppLifeCycle.java
+++ Program/src/org/jphototagger/program/app/AppLifeCycle.java
@@ -13,6 +13,7 @@ import org.bushe.swing.event.EventBus;
 
 import org.openide.util.Lookup;
 
+import org.jphototagger.api.concurrent.ReplaceableTask;
 import org.jphototagger.api.concurrent.SerialTaskExecutor;
 import org.jphototagger.api.modules.Module;
 import org.jphototagger.api.preferences.Preferences;
@@ -22,8 +23,8 @@ import org.jphototagger.domain.repository.RepositoryMaintainance;
 import org.jphototagger.lib.dialog.MessageDisplayer;
 import org.jphototagger.lib.util.Bundle;
 import org.jphototagger.program.factory.MetaFactory;
-import org.jphototagger.program.helper.Cleanup;
 import org.jphototagger.program.resource.GUI;
+import org.jphototagger.program.tasks.ScheduledTasks;
 import org.jphototagger.program.view.frames.AppFrame;
 
 /**
@@ -315,5 +316,47 @@ public final class AppLifeCycle {
         }
 
         public abstract void execute();
+    }
+
+    private static class Cleanup {
+
+        /**
+         * Sleep time in milliseconds before giving control to the caller, so that
+         * the threads can complete their current action before they check for
+         * interruption.
+         */
+        private static long MILLISECONDS_SLEEP = 2000;
+
+        /**
+         * Shuts down all Tasks.
+         */
+        public static void shutdown() {
+            SerialTaskExecutor serialTaskExecutor = Lookup.getDefault().lookup(SerialTaskExecutor.class);
+            ReplaceableTask replaceableTask = Lookup.getDefault().lookup(ReplaceableTask.class);
+
+            ScheduledTasks.INSTANCE.cancelCurrentTasks();
+            replaceableTask.cancelRunningTask();
+            serialTaskExecutor.cancelAllTasks();
+
+            boolean serialTasksRunning = serialTaskExecutor.getTaskCount() > 0;
+            boolean sleep = (ScheduledTasks.INSTANCE.getCount() > 0) || serialTasksRunning;
+
+            if (sleep) {
+                sleep();
+            }
+        }
+
+        private static void sleep() {
+            try {
+
+                // Let the tasks a little bit time to complete until they can interrupt
+                Thread.sleep(MILLISECONDS_SLEEP);
+            } catch (Exception ex) {
+                Logger.getLogger(Cleanup.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        private Cleanup() {
+        }
     }
 }
