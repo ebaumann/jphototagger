5da463fcd38d44ebafebedfa2d676b139a094685
ebaumann <eb@elmar-baumann.de>
2011-10-10 18:32:08 +0200

    Continued: Encapsulating EXIF; Added some FileSorters; Restructered some code

---
+++ Exif/src/org/jphototagger/exif/ExifPreferencesKeys.java
@@ -0,0 +1,12 @@
+package org.jphototagger.exif;
+
+/**
+ * @author Elmar Baumann
+ */
+public final class ExifPreferencesKeys {
+
+    public static final String KEY_GPS_ADD_FILENAME_TO_GPS_LOCATION_EXPORT = "UserSettings.AddFilenameToGpsLocationExport";
+
+    private ExifPreferencesKeys() {
+    }
+}
