8622e1514454e01fabe74bb2184cf041582b3820
ebaumann <ebaumann@localhost>
2011-10-19 21:41:10 +0200

    Added StatusLineElementProvider

---
+++ API/src/org/jphototagger/api/windows/StatusLineElementProvider.java
@@ -0,0 +1,11 @@
+package org.jphototagger.api.windows;
+
+import java.awt.Component;
+
+/**
+ * @author Elmar Baumann
+ */
+public interface StatusLineElementProvider {
+
+    Component getStatusLineElement();
+}
