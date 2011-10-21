ee1413a1ee832c916e1dac89dcff40201a953316
ebaumann <ebaumann@localhost>
2011-10-21 15:32:37 +0200

    Renamed ActionsToMenus into MenuLayer

--- Modules/UserDefinedFileFilters/src/org/jphototagger/userdefinedfilters/MenuLayer.java
+++ Modules/UserDefinedFileFilters/src/org/jphototagger/userdefinedfilters/MenuLayer.java
@@ -1,12 +1,11 @@
-package org.jphototagger.program.app.update;
+package org.jphototagger.userdefinedfilters;
 
 import java.util.Arrays;
 import java.util.Collection;
-
+import org.jphototagger.api.windows.MenuItemProvider;
 import org.openide.util.lookup.ServiceProvider;
 
 import org.jphototagger.api.windows.MainWindowMenuProvider;
-import org.jphototagger.api.windows.MenuItemProvider;
 import org.jphototagger.lib.api.MainWindowMenuProviderAdapter;
 import org.jphototagger.lib.api.MenuItemProviderImpl;
 
@@ -14,10 +13,10 @@ import org.jphototagger.lib.api.MenuItemProviderImpl;
  * @author Elmar Baumann
  */
 @ServiceProvider(service = MainWindowMenuProvider.class)
-public final class ActionsToMenus extends MainWindowMenuProviderAdapter {
+public final class MenuLayer extends MainWindowMenuProviderAdapter {
 
     @Override
-    public Collection<? extends MenuItemProvider> getHelpMenuItems() {
-        return Arrays.asList(new MenuItemProviderImpl(new UpdateCheckAction(), 1000, true));
+    public Collection<? extends MenuItemProvider> getWindowMenuItems() {
+        return Arrays.asList(new MenuItemProviderImpl(new ShowUserDefinedFileFiltersDialogAction(), 30, true));
     }
 }
--- Program/src/org/jphototagger/program/app/logging/MenuLayer.java
+++ Program/src/org/jphototagger/program/app/logging/MenuLayer.java
@@ -1,11 +1,12 @@
-package org.jphototagger.userdefinedfilters;
+package org.jphototagger.program.app.logging;
 
 import java.util.Arrays;
 import java.util.Collection;
-import org.jphototagger.api.windows.MenuItemProvider;
+
 import org.openide.util.lookup.ServiceProvider;
 
 import org.jphototagger.api.windows.MainWindowMenuProvider;
+import org.jphototagger.api.windows.MenuItemProvider;
 import org.jphototagger.lib.api.MainWindowMenuProviderAdapter;
 import org.jphototagger.lib.api.MenuItemProviderImpl;
 
@@ -13,10 +14,12 @@ import org.jphototagger.lib.api.MenuItemProviderImpl;
  * @author Elmar Baumann
  */
 @ServiceProvider(service = MainWindowMenuProvider.class)
-public final class ActionsToMenus extends MainWindowMenuProviderAdapter {
+public final class MenuLayer extends MainWindowMenuProviderAdapter {
 
     @Override
     public Collection<? extends MenuItemProvider> getWindowMenuItems() {
-        return Arrays.asList(new MenuItemProviderImpl(new ShowUserDefinedFileFiltersDialogAction(), 30, true));
+        return Arrays.asList(
+                new MenuItemProviderImpl(new ShowErrorLogfileAction(), 50, true),
+                new MenuItemProviderImpl(new ShowAllMessagesLogfileAction(), 60, false));
     }
 }
