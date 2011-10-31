45e4ad3221bc3944fcd5d469e6aa8006684ead2c
ebaumann <ebaumann@localhost>
2011-10-31 20:16:38 +0100

    Re-introduced persistence of last choosen help page

--- Program/src/org/jphototagger/program/help/HelpBrowserDisplayer.java
+++ Program/src/org/jphototagger/program/help/HelpBrowserDisplayer.java
@@ -1,9 +1,12 @@
 package org.jphototagger.program.help;
 
+import org.jphototagger.api.preferences.Preferences;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
 
 import org.jphototagger.lib.help.HelpBrowser;
+import org.jphototagger.lib.help.HelpBrowserEvent;
+import org.jphototagger.lib.help.HelpBrowserListener;
 import org.jphototagger.lib.help.HelpContentProvider;
 import org.jphototagger.lib.help.HelpDisplayer;
 import org.jphototagger.lib.help.HelpNode;
@@ -11,6 +14,7 @@ import org.jphototagger.lib.help.HelpUtil;
 import org.jphototagger.lib.swing.util.ComponentUtil;
 import org.jphototagger.lib.util.Bundle;
 import org.jphototagger.lib.util.StringUtil;
+import org.openide.util.Lookup;
 
 /**
  * @author Elmar Baumann
@@ -21,12 +25,15 @@ import org.jphototagger.lib.util.StringUtil;
 })
 public final class HelpBrowserDisplayer implements HelpDisplayer, HelpContentProvider {
 
+    private static final String KEY_CURRENT_URL = HelpBrowserDisplayer.class.getName() + ".CurrentURL";
     private static final HelpBrowser HELP_BROWSER;
 
     static {
         HelpNode rootNode = HelpUtil.createNodeFromHelpContentProviders();
         HELP_BROWSER = new HelpBrowser(rootNode);
         HELP_BROWSER.setTitle(Bundle.getString(HelpBrowserDisplayer.class, "HelpBrowserDisplayer.Title"));
+        restoreCurrentUrl();
+        HELP_BROWSER.addHelpBrowserListener(new ContentsHelpBrowserListener());
     }
 
     static void browseHelp(String helpPageUrl) {
@@ -51,5 +58,31 @@ public final class HelpBrowserDisplayer implements HelpDisplayer, HelpContentPro
     @Override
     public int getPosition() {
         return Integer.MIN_VALUE;
+    }
+
+    private static void restoreCurrentUrl() {
+        Preferences preferences = Lookup.getDefault().lookup(Preferences.class);
+        if (preferences.containsKey(KEY_CURRENT_URL)) {
+            String currentUrl = preferences.getString(KEY_CURRENT_URL);
+            synchronized (HelpBrowser.class) {
+                HELP_BROWSER.setDisplayUrl(currentUrl);
+            }
+        }
+    }
+
+    private static void persistCurrentUrl(String currentUrl) {
+        Preferences preferences = Lookup.getDefault().lookup(Preferences.class);
+        preferences.setString(KEY_CURRENT_URL, currentUrl);
+    }
+
+    private static class ContentsHelpBrowserListener implements HelpBrowserListener {
+
+        @Override
+        public void actionPerformed(HelpBrowserEvent evt) {
+            if (evt.getType().equals(HelpBrowserEvent.Type.URL_CHANGED)) {
+                String currentUrl = HelpBrowser.toJPhotoTaggerUrl(evt.getUrl());
+                persistCurrentUrl(currentUrl);
+            }
+        }
     }
 }
