ecdc3de796e7a045f34b893c5a8a666b77b32dbd
ebaumann <ebaumann@localhost>
2011-11-02 20:02:12 +0100

    Highlighting found words in Help

--- Program/src/org/jphototagger/program/app/AppInfo.java
+++ Program/src/org/jphototagger/program/app/AppInfo.java
@@ -18,7 +18,7 @@ public final class AppInfo {
      * The application's version
      */
     // Never change that scheme: "[0-9]+\.[0-9]+\.[0-9]+"!
-    public static final String APP_VERSION = "0.14.1";
+    public static final String APP_VERSION = "0.14.2";
     /**
      * ("Historical") Name of this project.
      * <p>
