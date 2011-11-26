b068620771f44b8a7c4777eb604dcc56d7e04cef
ebaumann <ebaumann@localhost>
2011-11-26 22:47:05 +0100

    Added ThumbnailsPanelBottomComponentProvider

---
+++ Domain/src/org/jphototagger/domain/thumbnails/ThumbnailsPanelBottomComponentProvider.java
@@ -0,0 +1,11 @@
+package org.jphototagger.domain.thumbnails;
+
+import java.awt.Component;
+
+/**
+ * @author Elmar Baumann
+ */
+public interface ThumbnailsPanelBottomComponentProvider {
+
+    Component getComponent();
+}
