39f54b5d8acb1d61b4ff45ee7c8f3f65392c2aac
ebaumann <ebaumann@localhost>
2012-01-29 00:02:39 +0100

    Added Settings to HTML Reports

---
+++ Plugins/HtmlReports/src/org/jphototagger/plugin/htmlreports/HtmlReportsPreferencesKeys.java
@@ -0,0 +1,15 @@
+package org.jphototagger.plugin.htmlreports;
+
+/**
+ * @author Elmar Baumann
+ */
+public final class HtmlReportsPreferencesKeys {
+
+    public static final String KEY_DIRECTORY = "HtmlReports.Directory";
+    public static final String KEY_INPUT_FILENAME_BEFORE_CREATING = "HtmlReports.InputFilename";
+    public static final String KEY_OPEN_REPORT_AFTER_CREATING = "HtmlReports.OpenReportAfterCreation";
+    public static final String KEY_SHOW_SETTINGS_BEFORE_CREATING = "HtmlReports.DisplaySettingsDialogBeforeCreation";
+
+    private HtmlReportsPreferencesKeys() {
+    }
+}
