d28cfa32c5b55a7f1c3d21d6ea0b4428d2a0d4cc
ebaumann <ebaumann@localhost>
2012-01-29 19:07:20 +0100

    Added #getTextResource()

--- Lib/src/org/jphototagger/lib/io/IoUtil.java
+++ Lib/src/org/jphototagger/lib/io/IoUtil.java
@@ -1,9 +1,14 @@
 package org.jphototagger.lib.io;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.jphototagger.lib.util.SystemProperties;
 
 /**
  * @author Elmar Baumann
@@ -12,7 +17,7 @@ public final class IoUtil {
 
     /**
      * Catches IOException.
-     * 
+     *
      * @param closable maybe null
      */
     public static void close(Closeable closable) {
@@ -22,6 +27,40 @@ public final class IoUtil {
             } catch (IOException ex) {
                 Logger.getLogger(IoUtil.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
+    }
+
+    /**
+     * @param path e.g. <code>"/org/jphototagger/lib/io/texttemplate.txt"</code>
+     * @return Resource as String with system dependend line separators
+     * @throws IOException
+     */
+    public static String getTextResource(String path) throws IOException {
+        BufferedReader br = null;
+        String newline = SystemProperties.getLineSeparator();
+
+        try {
+            InputStream is = IoUtil.class.getResourceAsStream(path);
+            InputStreamReader isr = new InputStreamReader(is);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            boolean isFirstLine = true;
+
+            br = new BufferedReader(isr);
+
+            while ((line = br.readLine()) != null) {
+                sb.append(isFirstLine ? "" : newline);
+                sb.append(line);
+                isFirstLine = false;
+            }
+
+            if (sb.length() > 0) {
+                sb.append(newline);
+            }
+
+            return sb.toString();
+        } finally {
+            close(br);
         }
     }
 
