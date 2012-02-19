534f8b5a1247fce578ef69ecab205f6d511db739
ebaumann <ebaumann@localhost>
2012-02-19 20:50:11 +0100

    Sending Bug Reports: Option to open the log file directory

--- Program/src/org/jphototagger/program/help/SendBugReportMailAction.java
+++ Program/src/org/jphototagger/program/help/SendBugReportMailAction.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
+import org.jphototagger.lib.awt.DesktopUtil;
 import org.jphototagger.lib.swing.IconUtil;
+import org.jphototagger.lib.swing.MessageDisplayer;
 import org.jphototagger.lib.util.Bundle;
 import org.jphototagger.program.app.AppInfo;
 import org.jphototagger.program.app.logging.AppLoggingSystem;
@@ -35,6 +37,14 @@ final class SendBugReportMailAction extends AbstractAction {
                 "SendBugReportMailAction.Info.AttachLogfile",
                 allMessagesLogfilePath, errorMessagesLogfilePath);
 
+        openLogfileDirectory();
         SendMail.sendMail(mailto, subject, message);
+    }
+
+    private void openLogfileDirectory() {
+        String message = Bundle.getString(SendBugReportMailAction.class, "SendBugReportMailAction.Confirm.OpenLogfileDir");
+        if (MessageDisplayer.confirmYesNo(null, message)) {
+            DesktopUtil.open(AppLoggingSystem.getLofileDirectory(), "SendBugReportMailAction.OpenLogfileDirectory");
+        }
     }
 }
