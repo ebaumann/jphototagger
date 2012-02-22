7a5d900a1aed6e7ee6c8f9021116fd5477704ae5
ebaumann <ebaumann@localhost>
2012-02-22 22:17:09 +0100

    Export/Import of repository data: Implemented Ex-/Import for Wordsets

--- Domain/src/org/jphototagger/domain/wordsets/Wordset.java
+++ Domain/src/org/jphototagger/domain/wordsets/Wordset.java
@@ -8,18 +8,37 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 import org.jphototagger.lib.util.ObjectUtil;
 import org.jphototagger.lib.util.StringUtil;
 
 /**
  * @author Elmar Baumann
  */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public final class Wordset {
 
     private long id = Integer.MIN_VALUE;
     private String name;
+    @XmlElementWrapper(name = "words")
+    @XmlElement(type = String.class, name="word")
     private List<String> words = new ArrayList<String>();
+    @XmlTransient
     private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    /**
+     * Required only for JAXB, do <em>not</em> use this constructor!
+     */
+    public Wordset() {
+        this("Invalid Wordset");
+    }
 
     public Wordset(String name) {
         if (!StringUtil.hasContent(name)) {
