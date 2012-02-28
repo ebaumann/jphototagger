51d5a5e7e5ab3c41d5fdfb640da6a93cf6140dc6
ebaumann <ebaumann@localhost>
2012-02-28 20:31:26 +0100

    Bundle uses dots for path instead of slashes

--- Lib/src/org/jphototagger/lib/util/Bundle.java
+++ Lib/src/org/jphototagger/lib/util/Bundle.java
@@ -11,46 +11,32 @@ import java.util.logging.Logger;
 public final class Bundle {
 
     /**
-     * Returns a string from the resource bundle {@code Bundle.properties} within the same package
-     * and logs {@code MissingResourceException}s rather than throwing it.
+     * Returns a string from the resource bundle {@code Bundle.properties} within the same package and logs {@code MissingResourceException}s
+     * rather than throwing it.
      *
-     * @param clazz
+     * @param classWithinSamePackageAsBundle
      * @param key
      * @param params optional params as described in the {@code MessageFormat} class documentation
-     * @return       Localized String in {@code Bundle.properties} in the same package as {@code clazz}
+     * @return Localized String in {@code Bundle.properties} in the same package as {@code clazz}
      */
-    public static String getString(Class<?> clazz, String key, Object... params) {
-        if (clazz == null) {
-            throw new NullPointerException("clazz == null");
+    public static String getString(Class<?> classWithinSamePackageAsBundle, String key, Object... params) {
+        if (classWithinSamePackageAsBundle == null) {
+            throw new NullPointerException("classWithinSamePackageAsBundle == null");
         }
-
         if (key == null) {
             throw new NullPointerException("key == null");
         }
-
-        // Javadoc: Resource bundle instances created by the getBundle factory methods are cached by default
-        String bundlePath = createDefaultBundlePath(clazz);
+        String bundlePath = classWithinSamePackageAsBundle.getPackage().getName() + ".Bundle";
         ResourceBundle bundle = ResourceBundle.getBundle(bundlePath);
-
         return getFormattedString(bundle, key, params);
     }
 
-    private static String createDefaultBundlePath(Class<?> clazz) {
-        String packagePath = StorageUtil.resolvePackagePathForResource(clazz);
-        String bundlePath = packagePath + '/' + "Bundle";
-
-        return bundlePath;
-    }
-
     private static String getFormattedString(ResourceBundle bundle, String key, Object... params) {
-
         try {
             String s = bundle.getString(key);
-
             return params == null || params.length == 0 ? s : MessageFormat.format(s, params);
-        } catch (Exception ex) {
-            Logger.getLogger(Bundle.class.getName()).log(Level.SEVERE, null, ex);
-
+        } catch (Throwable t) {
+            Logger.getLogger(Bundle.class.getName()).log(Level.SEVERE, null, t);
             return "?" + key + "?";
         }
     }
