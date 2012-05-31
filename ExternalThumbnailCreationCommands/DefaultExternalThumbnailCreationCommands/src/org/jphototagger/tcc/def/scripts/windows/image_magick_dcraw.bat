3aec684ee2cf17a66251441095f924850a0ec08c
ebaumann <ebaumann@localhost>
2012-05-31 23:16:31 +0200

    Added ".xcf" as file type to thumbnail generating scripts

--- ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/scripts/windows/image_magick_dcraw.bat
+++ ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/scripts/windows/image_magick_dcraw.bat
@@ -8,12 +8,13 @@ set MAX_DIM=%2
 rem Extracting image suffix
 for %%i in (%IMAGE%) DO set THUMB_SUFFIX=%%~xi
 
-rem Deciding whether RAW or not 
+rem Deciding whether RAW or not
 if /I "%THUMB_SUFFIX%" ==".jpg" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".tif" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".gif" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".png" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".psd" GOTO OTHER
+if /I "%THUMB_SUFFIX%" ==".xcf" GOTO OTHER
 
 "${dcraw.exe}" -e -c %IMAGE% | "${convert.exe}" - -thumbnail %MAX_DIM%x%MAX_DIM% -auto-orient jpg:-
 goto END
