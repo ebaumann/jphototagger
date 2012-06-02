3a97fae0df9529de537d2c537bf2f93430b06c3f
ebaumann <ebaumann@localhost>
2012-06-02 22:42:36 +0200

    ExifAscii validates the raw value (conversion to string only byte values are valid)

--- Exif/src/org/jphototagger/exif/datatype/ExifAscii.java
+++ Exif/src/org/jphototagger/exif/datatype/ExifAscii.java
@@ -1,20 +1,24 @@
 package org.jphototagger.exif.datatype;
 
+import java.nio.charset.Charset;
+
+import org.jphototagger.lib.util.ByteUtil;
+
 /**
- * EXIF data type ASCII as described in the standard: An 8-bit byte containing
- * one 7-bit ASCII code. The final byte is terminated with NULL.
+ * EXIF data type ASCII as described in the standard: An 8-bit byte containing one 7-bit ASCII code. The final byte is
+ * terminated with NULL.
  *
  * @author Elmar Baumann
  */
 public final class ExifAscii {
 
+    private static final Charset CHARSET = Charset.forName("ISO-8859-1"); // Allow more characters than ASCII contains, ISO-8859-1 contains all ASCII characters
     private final String value;
 
     public ExifAscii(byte[] rawValue) {
         if (rawValue == null) {
             throw new NullPointerException("rawValue == null");
         }
-
         value = convertRawValueToString(rawValue);
     }
 
@@ -22,13 +26,41 @@ public final class ExifAscii {
         if (rawValue == null) {
             throw new NullPointerException("rawValue == null");
         }
+        if (rawValue.length == 0) {
+            return "";
+        }
+        if (!isNullTerminated(rawValue)) {
+            return "?";
+        }
+        if (rawValue.length == 1) { // == "\0"
+            return "";
+        }
+        byte[] notNullTerminatedRawValue = new byte[rawValue.length - 1]; // rawValue.length > 1
+        System.arraycopy(rawValue, 0, notNullTerminatedRawValue, 0, notNullTerminatedRawValue.length);
+        if (!onlyCharacters(notNullTerminatedRawValue)) {
+            return "?";
+        }
+        String converted = new String(notNullTerminatedRawValue, CHARSET);
+        return converted;
+    }
 
-        String nullTerminatedValue = new String(rawValue);
-        int length = nullTerminatedValue.length();
+    private static boolean onlyCharacters(byte[] bytes) {
+        for (int index = 0; index < bytes.length - 1; index++) {
+            int intValue = ByteUtil.toInt(bytes[index]);
+            boolean isCharacter = intValue > 0 && intValue < 256; // Allow a more characters than ASCII contains, but conform to CHARSET
+            if (!isCharacter) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-        return (length > 0)
-                ? nullTerminatedValue.substring(0, length - 1)
-                : "";
+    private static boolean isNullTerminated(byte[] rawValue) {
+        if (rawValue.length < 1) {
+            return false;
+        }
+        byte lastByte = rawValue[rawValue.length - 1];
+        return ByteUtil.toInt(lastByte) == 0;
     }
 
     public static ExifDataType getExifDataType() {
@@ -41,21 +73,18 @@ public final class ExifAscii {
 
     /**
      *
-     * @param  obj
-     * @return     true if the values of both objects are equals
+     * @param obj
+     * @return true if the values of both objects are equals
      */
     @Override
     public boolean equals(Object obj) {
         if (obj == this) {
             return true;
         }
-
         if (!(obj instanceof ExifAscii)) {
             return false;
         }
-
         ExifAscii other = (ExifAscii) obj;
-
         return this.value == null
                 ? other.value == null
                 : this.value.equals(other.value);
@@ -64,11 +93,9 @@ public final class ExifAscii {
     @Override
     public int hashCode() {
         int hash = 7;
-
         hash = 37 * hash + this.value != null
                 ? this.value.hashCode()
                 : 0;
-
         return hash;
     }
 
