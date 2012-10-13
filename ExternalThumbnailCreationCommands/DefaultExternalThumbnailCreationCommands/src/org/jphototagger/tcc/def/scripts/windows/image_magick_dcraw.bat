0a2e50437dc3c603de57669ec8b3313096c71ce5
ebaumann <ebaumann@localhost>
2012-10-13 17:04:05 +0200

    Added DNG and file name suffix "tiff" to ImageMagick scripts; Documented requirement of JDK 1.7

--- ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/scripts/windows/image_magick_dcraw.bat
+++ ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/scripts/windows/image_magick_dcraw.bat
@@ -9,8 +9,10 @@ rem Extracting image suffix
 for %%i in (%IMAGE%) DO set THUMB_SUFFIX=%%~xi
 
 rem Deciding whether RAW or not
+if /I "%THUMB_SUFFIX%" ==".dng" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".jpg" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".tif" GOTO OTHER
+if /I "%THUMB_SUFFIX%" ==".tiff" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".gif" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".png" GOTO OTHER
 if /I "%THUMB_SUFFIX%" ==".psd" GOTO OTHER
