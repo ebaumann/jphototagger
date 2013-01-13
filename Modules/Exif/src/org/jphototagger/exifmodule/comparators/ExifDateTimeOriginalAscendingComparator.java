332099c00bb1a8aa7c50bc4365b73a8258431834
ebaumann <ebaumann@localhost>
2013-01-13 12:21:35 +0100

    ExifDateTimeOriginalAscendingComparator uses date created within XMP if EXIF is absent

--- Modules/Exif/src/org/jphototagger/exifmodule/comparators/ExifDateTimeOriginalAscendingComparator.java
+++ Modules/Exif/src/org/jphototagger/exifmodule/comparators/ExifDateTimeOriginalAscendingComparator.java
@@ -1,11 +1,17 @@
 package org.jphototagger.exifmodule.comparators;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Comparator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jphototagger.domain.metadata.exif.Exif;
+import org.jphototagger.domain.metadata.xmp.Xmp;
+import org.jphototagger.domain.metadata.xmp.XmpIptc4XmpCoreDateCreatedMetaDataValue;
 import org.jphototagger.domain.repository.ImageFilesRepository;
 import org.jphototagger.lib.util.ClassEquality;
+import org.jphototagger.xmp.XmpMetadata;
 import org.openide.util.Lookup;
 
 /**
@@ -20,7 +26,6 @@ public final class ExifDateTimeOriginalAscendingComparator extends ClassEquality
     public int compare(File fileLeft, File fileRight) {
         long timeLeft = getTimestampDateTimeOriginalFromRepository(fileLeft);
         long timeRight = getTimestampDateTimeOriginalFromRepository(fileRight);
-
         return timeLeft == timeRight
                 ? 0
                 : timeLeft < timeRight
@@ -30,12 +35,33 @@ public final class ExifDateTimeOriginalAscendingComparator extends ClassEquality
 
     private long getTimestampDateTimeOriginalFromRepository(File imageFile) {
         Exif exif = repo.findExifOfImageFile(imageFile);
-
         if (exif == null || exif.getDateTimeOriginal() == null) {
-            return imageFile.lastModified();
+            return findDateCreated(imageFile);
         }
-
         return exif.getDateTimeOriginal().getTime();
+    }
+
+    /**
+     * @param file
+     * @return {@link XmpIptc4XmpCoreDateCreatedMetaDataValue} if contained in XMP of file and valid, file's last
+     * modification date else
+     */
+    static long findDateCreated(File file) {
+        Xmp xmp;
+        try {
+            xmp = XmpMetadata.getXmpFromSidecarFileOf(file);
+        } catch (IOException ex) {
+            Logger.getLogger(ExifDateTimeOriginalAscendingComparator.class.getName()).log(Level.SEVERE, null, ex);
+            return file.lastModified();
+        }
+        if (xmp == null || !xmp.contains(XmpIptc4XmpCoreDateCreatedMetaDataValue.INSTANCE)) {
+            return file.lastModified();
+        }
+        String date = (String) xmp.getValue(XmpIptc4XmpCoreDateCreatedMetaDataValue.INSTANCE);
+        Long timestamp = XmpIptc4XmpCoreDateCreatedMetaDataValue.createTimestamp(date);
+        return timestamp == null
+                ? file.lastModified()
+                : timestamp;
     }
 
     @Override
