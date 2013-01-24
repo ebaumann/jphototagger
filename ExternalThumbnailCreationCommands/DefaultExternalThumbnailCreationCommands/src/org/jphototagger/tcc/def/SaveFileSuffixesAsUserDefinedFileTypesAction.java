3d6cbf4a5e4f7b5b1ddddd04dc8a4a918e0ec854
ebaumann <ebaumann@localhost>
2013-01-24 20:04:33 +0100

    Added to thumbnail creation script templates FLV, MKV, TS, BMP, TTF

---
+++ ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/SaveFileSuffixesAsUserDefinedFileTypesAction.java
@@ -0,0 +1,63 @@
+package org.jphototagger.tcc.def;
+
+import java.awt.event.ActionEvent;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import org.jphototagger.domain.filetypes.UserDefinedFileType;
+import org.jphototagger.domain.repository.UserDefinedFileTypesRepository;
+import org.jphototagger.lib.util.Bundle;
+import org.openide.util.Lookup;
+
+/**
+ * @author Elmar Baumann
+ */
+public class SaveFileSuffixesAsUserDefinedFileTypesAction extends AbstractAction {
+
+    private static final long serialVersionUID = 1L;
+    private final Collection<UserDefinedFileType> filetypes;
+
+    protected SaveFileSuffixesAsUserDefinedFileTypesAction(Collection<? extends UserDefinedFileType> filetypes) {
+        this.filetypes =  new ArrayList<>(filetypes);
+    }
+
+    static UserDefinedFileType createUserDefinedFileType(String suffix, String description) {
+        UserDefinedFileType fileType = new UserDefinedFileType();
+        fileType.setSuffix(suffix);
+        fileType.setDescription(description);
+        fileType.setExternalThumbnailCreator(true);
+        return fileType;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (confirmSave()) {
+            UserDefinedFileTypesRepository repo = Lookup.getDefault().lookup(UserDefinedFileTypesRepository.class);
+            int saveCount = 0;
+            for (UserDefinedFileType filetype : filetypes) {
+                if (!repo.existsUserDefinedFileTypeWithSuffix(filetype.getSuffix())) {
+                    repo.saveUserDefinedFileType(filetype);
+                    saveCount++;
+                }
+            }
+            showInfoAfterSave(saveCount);
+        }
+    }
+
+    private boolean confirmSave() {
+        String message = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveFileSuffixesAsUserDefinedFileTypesAction.ConfirmSave.Message");
+        String title = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveFileSuffixesAsUserDefinedFileTypesAction.ConfirmSave.Title");
+        int result = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
+        return result == JOptionPane.YES_OPTION;
+    }
+
+    private void showInfoAfterSave(int saveCount) {
+        String pattern = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveFileSuffixesAsUserDefinedFileTypesAction.InfoAfterSave.MessageTemplate");
+        String title = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveFileSuffixesAsUserDefinedFileTypesAction.InfoAfterSave.Title");
+        String message = MessageFormat.format(pattern, saveCount);
+        int messageType = JOptionPane.INFORMATION_MESSAGE;
+        JOptionPane.showMessageDialog(null, message, title, messageType);
+    }
+}
--- ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.java
+++ ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.java
@@ -1,83 +1,43 @@
 package org.jphototagger.tcc.def;
 
-import java.awt.event.ActionEvent;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
-import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.JOptionPane;
 import org.jphototagger.domain.filetypes.UserDefinedFileType;
-import org.jphototagger.domain.repository.UserDefinedFileTypesRepository;
 import org.jphototagger.lib.util.Bundle;
-import org.openide.util.Lookup;
 
 /**
  * @author Elmar Baumann
  */
-public final class SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction extends AbstractAction {
+public final class SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction extends SaveFileSuffixesAsUserDefinedFileTypesAction {
 
     private static final long serialVersionUID = 1L;
-    private static final Collection<UserDefinedFileType> VIDEO_FILES = new ArrayList<>(10);
+    private static final Collection<UserDefinedFileType> FILE_TYPES = new ArrayList<>();
     public static final SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction INSTANCE = new SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction();
 
     {
-        VIDEO_FILES.add(createUserDefinedFileType("avi", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.avi.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("m2ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.m2ts.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("mov", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mov.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("mp2", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp2.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("mp4", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp4.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("mpeg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpeg.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("mpg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpg.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("mts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mts.Description")));
-        VIDEO_FILES.add(createUserDefinedFileType("wmv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.wmv.Description")));
-    }
+        // Videos
+        FILE_TYPES.add(createUserDefinedFileType("avi", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.avi.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("flv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.flv.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("m2ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.m2ts.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mkv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mkv.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mov", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mov.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mp2", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp2.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mp4", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp4.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mpeg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpeg.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mpg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpg.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("mts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mts.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.ts.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("wmv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.wmv.Description")));
 
-    private static UserDefinedFileType createUserDefinedFileType(String suffix, String description) {
-        UserDefinedFileType fileType = new UserDefinedFileType();
-
-        fileType.setSuffix(suffix);
-        fileType.setDescription(description);
-        fileType.setExternalThumbnailCreator(true);
-
-        return fileType;
+        // Images not handled by JPhotoTagger
+        FILE_TYPES.add(createUserDefinedFileType("bmp", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.bmp.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("ttf", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.ttf.Description")));
+        FILE_TYPES.add(createUserDefinedFileType("xcf", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.xcf.Description")));
     }
 
     private SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction() {
+        super(FILE_TYPES);
         putValue(Action.NAME, Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.Name"));
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (confirmSave()) {
-            UserDefinedFileTypesRepository repo = Lookup.getDefault().lookup(UserDefinedFileTypesRepository.class);
-            int saveCount = 0;
-
-            for (UserDefinedFileType userDefinedFileType : VIDEO_FILES) {
-                if (!repo.existsUserDefinedFileTypeWithSuffix(userDefinedFileType.getSuffix())) {
-                    repo.saveUserDefinedFileType(userDefinedFileType);
-                    saveCount++;
                 }
-            }
-
-            showInfoAfterSave(saveCount);
-        }
-    }
-
-    private boolean confirmSave() {
-        String message = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.ConfirmSave.Message");
-        String title = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.ConfirmSave.Title");
-        int result = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
-
-        return result == JOptionPane.YES_OPTION;
-    }
-
-    private void showInfoAfterSave(int saveCount) {
-        String pattern = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.InfoAfterSave.MessageTemplate");
-        String title = Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.InfoAfterSave.Title");
-        String message = MessageFormat.format(pattern, saveCount);
-        int messageType = JOptionPane.INFORMATION_MESSAGE;
-
-        JOptionPane.showMessageDialog(null, message, title, messageType);
-    }
-}
