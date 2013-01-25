47fa5483c2def7cf0570301142d3f89145683bd2
ebaumann <ebaumann@localhost>
2013-01-25 19:21:23 +0100

    Using system display names for directories

--- Lib/src/org/jphototagger/lib/swing/AllSystemDirectoriesTreeCellRenderer.java
+++ Lib/src/org/jphototagger/lib/swing/AllSystemDirectoriesTreeCellRenderer.java
@@ -22,14 +22,15 @@ import org.jphototagger.lib.util.Bundle;
 public final class AllSystemDirectoriesTreeCellRenderer extends DefaultTreeCellRenderer {
 
     private static final long serialVersionUID = 1L;
-    private final FileSystemView FILE_SYSTEM_VIEW = FileSystemView.getFileSystemView();
-    private Icon rootIcon = IconUtil.getImageIcon("/org/jphototagger/lib/resource/icons/icon_workspace.png");
+    private static final Logger LOGGER = Logger.getLogger(AllSystemDirectoriesTreeCellRenderer.class.getName());
+    private static final FileSystemView FILE_SYSTEM_VIEW = FileSystemView.getFileSystemView();
+    private static final Icon ROOT_ICON = IconUtil.getImageIcon("/org/jphototagger/lib/resource/icons/icon_workspace.png");
     private static final String DISPLAY_NAME_ROOT = Bundle.getString(AllSystemDirectoriesTreeCellRenderer.class, "AllSystemDirectoriesTreeCellRenderer.DisplayName.Root");
-    private int tempSelRow = -1;
     private static final Color TREE_SELECTION_FOREGROUND = LookAndFeelUtil.getUiColor("Tree.selectionForeground");
     private static final Color TREE_SELECTION_BACKGROUND = LookAndFeelUtil.getUiColor("Tree.selectionBackground");
     private static final Color TREE_TEXT_BACKGROUND = LookAndFeelUtil.getUiColor("Tree.textBackground");
     private static final Color TREE_TEXT_FOREGROUND = LookAndFeelUtil.getUiColor("Tree.textForeground");
+    private int tempSelRow = -1;
 
     public AllSystemDirectoriesTreeCellRenderer() {
         setOpaque(true);
@@ -38,50 +39,40 @@ public final class AllSystemDirectoriesTreeCellRenderer extends DefaultTreeCellR
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         super.getTreeCellRendererComponent(tree, value, sel, expanded, false, row, hasFocus);
-
         if (value == tree.getModel().getRoot()) {
-            setIcon(rootIcon);
+            setIcon(ROOT_ICON);
             setText(DISPLAY_NAME_ROOT);
         } else if (value instanceof DefaultMutableTreeNode) {
             File file = null;
             Object userObject = ((DefaultMutableTreeNode) value).getUserObject();
-
             if (userObject instanceof File) {
                 file = (File) userObject;
             }
-
             if ((file != null) && file.exists()) {
                 synchronized (FILE_SYSTEM_VIEW) {
                     try {
                         setIcon(FILE_SYSTEM_VIEW.getSystemIcon(file));
-                    } catch (Exception ex) {
-                        Logger.getLogger(AllSystemDirectoriesTreeCellRenderer.class.getName()).log(Level.WARNING, null,
-                                ex);
+                    } catch (Throwable t) {
+                        Logger.getLogger(AllSystemDirectoriesTreeCellRenderer.class.getName()).log(Level.WARNING, null, t);
                     }
                 }
-
                 setText(getDirectoryName(file));
             }
         }
-
         renderSelectionPopup(row, selected);
-
         return this;
     }
 
     private String getDirectoryName(File file) {
         String name = file.getName();
-
         // Windows drive letters
         if (name.isEmpty()) {
             name = file.getAbsolutePath();
-
             if (name.endsWith("\\")) {
-                name = name.substring(0, name.length() - 2) + ":";
+                return name.substring(0, name.length() - 2) + ":";
             }
         }
-
-        return name;
+        return getDisplayname(file);
     }
 
     public void setTempSelectionRow(int index) {
@@ -98,5 +89,16 @@ public final class AllSystemDirectoriesTreeCellRenderer extends DefaultTreeCellR
         setBackground((isTempSelRow || (selected && !tempSelExists))
                 ? TREE_SELECTION_BACKGROUND
                 : TREE_TEXT_BACKGROUND);
+    }
+
+    private String getDisplayname(File file) {
+        synchronized(FILE_SYSTEM_VIEW) {
+            try {
+                return FILE_SYSTEM_VIEW.getSystemDisplayName(file);
+            } catch (Throwable t) {
+                LOGGER.log(Level.WARNING, null, t);
+                return file.getName();
+            }
+        }
     }
 }
