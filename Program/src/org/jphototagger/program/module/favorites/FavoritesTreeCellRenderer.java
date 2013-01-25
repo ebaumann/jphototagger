167e2beb63ff22ea8dd1a3c2869a83226ee439ed
ebaumann <ebaumann@localhost>
2013-01-25 19:45:13 +0100

    Using system display names for directories

--- Program/src/org/jphototagger/program/module/favorites/FavoritesTreeCellRenderer.java
+++ Program/src/org/jphototagger/program/module/favorites/FavoritesTreeCellRenderer.java
@@ -19,7 +19,8 @@ import org.jphototagger.program.app.ui.AppLookAndFeel;
 public final class FavoritesTreeCellRenderer extends DefaultTreeCellRenderer {
 
     private static final long serialVersionUID = 1L;
-    private final FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+    private static final Logger LOGGER = Logger.getLogger(FavoritesTreeCellRenderer.class.getName());
+    private static final FileSystemView FILE_SYSTEM_VIEW = FileSystemView.getFileSystemView();
     private int tempSelRow = -1;
 
     public FavoritesTreeCellRenderer() {
@@ -30,43 +31,35 @@ public final class FavoritesTreeCellRenderer extends DefaultTreeCellRenderer {
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
             boolean leaf, int row, boolean hasFocus) {
         super.getTreeCellRendererComponent(tree, value, sel, expanded, false, row, hasFocus);
-
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
         Object userObject = node.getUserObject();
-
         render(userObject, row);
-
         return this;
     }
 
     private void render(Object userObject, int row) {
         File file = null;
-
         if (userObject instanceof Favorite) {
             Favorite favoriteDirectory = (Favorite) userObject;
-
             file = favoriteDirectory.getDirectory();
             setText(favoriteDirectory.getName());
         } else if (userObject instanceof File) {
             file = (File) userObject;
             setText(getDirectoryName(file));
         }
-
         if (file != null) {
             if (file.exists()) {
-                synchronized (fileSystemView) {
+                synchronized (FILE_SYSTEM_VIEW) {
                     try {
-                        setIcon(fileSystemView.getSystemIcon(file));
+                        setIcon(FILE_SYSTEM_VIEW.getSystemIcon(file));
                     } catch (Exception ex) {
-                        Logger.getLogger(FavoritesTreeCellRenderer.class.getName()).log(Level.SEVERE, null, ex);
+                        LOGGER.log(Level.SEVERE, null, ex);
                     }
                 }
             }
         }
-
         boolean tempSelExists = tempSelRow >= 0;
         boolean isTempSelRow = row == tempSelRow;
-
         setForeground((isTempSelRow || (selected && !tempSelExists))
                 ? AppLookAndFeel.getTreeSelectionForeground()
                 : AppLookAndFeel.getTreeForeground());
@@ -77,17 +70,25 @@ public final class FavoritesTreeCellRenderer extends DefaultTreeCellRenderer {
 
     private String getDirectoryName(File file) {
         String name = file.getName();
-
         // Windows drive letters
         if (name.isEmpty()) {
             name = file.getAbsolutePath();
-
             if (name.endsWith("\\")) {
-                name = name.substring(0, name.length() - 2) + ":";
+                return name.substring(0, name.length() - 2) + ":";
             }
         }
+        return getDisplayname(file);
+    }
 
-        return name;
+    private String getDisplayname(File file) {
+        synchronized(FILE_SYSTEM_VIEW) {
+            try {
+                return FILE_SYSTEM_VIEW.getSystemDisplayName(file);
+            } catch (Throwable t) {
+                LOGGER.log(Level.WARNING, null, t);
+                return file.getName();
+            }
+        }
     }
 
     public void setTempSelectionRow(int index) {
