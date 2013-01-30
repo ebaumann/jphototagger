c9f0e7c403d77385e7f5df29e982eb0306efe1eb
ebaumann <ebaumann@localhost>
2013-01-30 13:16:34 +0100

    Listening to EXIF and XMP changes

--- Program/src/org/jphototagger/program/module/miscmetadata/MiscMetadataItemSelectedController.java
+++ Program/src/org/jphototagger/program/module/miscmetadata/MiscMetadataItemSelectedController.java
@@ -1,7 +1,7 @@
 package org.jphototagger.program.module.miscmetadata;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Collections;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -11,12 +11,22 @@ import org.bushe.swing.event.annotation.EventSubscriber;
 import org.jphototagger.api.windows.MainWindowManager;
 import org.jphototagger.api.windows.WaitDisplayer;
 import org.jphototagger.domain.metadata.MetaDataValue;
+import org.jphototagger.domain.metadata.exif.Exif;
+import org.jphototagger.domain.metadata.exif.ExifMetaDataValues;
+import org.jphototagger.domain.metadata.xmp.Xmp;
+import org.jphototagger.domain.metadata.xmp.XmpMetaDataValues;
 import org.jphototagger.domain.repository.ImageFilesRepository;
+import org.jphototagger.domain.repository.event.exif.ExifDeletedEvent;
+import org.jphototagger.domain.repository.event.exif.ExifInsertedEvent;
+import org.jphototagger.domain.repository.event.xmp.XmpDeletedEvent;
+import org.jphototagger.domain.repository.event.xmp.XmpInsertedEvent;
+import org.jphototagger.domain.repository.event.xmp.XmpUpdatedEvent;
 import org.jphototagger.domain.thumbnails.OriginOfDisplayedThumbnails;
 import org.jphototagger.domain.thumbnails.ThumbnailsPanelSettings;
 import org.jphototagger.domain.thumbnails.event.ThumbnailsPanelRefreshEvent;
 import org.jphototagger.lib.awt.EventQueueUtil;
 import org.jphototagger.lib.util.Bundle;
+import org.jphototagger.lib.util.ObjectUtil;
 import org.jphototagger.program.module.thumbnails.ThumbnailsPanel;
 import org.jphototagger.program.resource.GUI;
 import org.openide.util.Lookup;
@@ -25,6 +35,8 @@ import org.openide.util.Lookup;
  * @author Elmar Baumann
  */
 public final class MiscMetadataItemSelectedController implements TreeSelectionListener {
+
+    private TreePath selectedPath;
 
     public MiscMetadataItemSelectedController() {
         listen();
@@ -35,42 +47,163 @@ public final class MiscMetadataItemSelectedController implements TreeSelectionLi
         GUI.getMiscMetadataTree().addTreeSelectionListener(this);
     }
 
+    private boolean isItemSelected() {
+        return GUI.getMiscMetadataTree().getSelectionCount() == 1;
+    }
+
     @Override
     public void valueChanged(TreeSelectionEvent evt) {
+        selectedPath = null;
         if (evt.isAddedPath()) {
-            EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(evt.getNewLeadSelectionPath(), null));
+            selectedPath = evt.getNewLeadSelectionPath();
+            EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(selectedPath, null));
         }
     }
 
     @EventSubscriber(eventClass = ThumbnailsPanelRefreshEvent.class)
     public void refresh(ThumbnailsPanelRefreshEvent evt) {
-        if (GUI.getMiscMetadataTree().getSelectionCount() == 1) {
-            OriginOfDisplayedThumbnails origin = evt.getOriginOfDisplayedThumbnails();
-            if (origin.isFilesMatchingMiscMetadata()) {
-                EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(GUI.getMiscMetadataTree().getSelectionPath(), evt.getThumbnailsPanelSettings()));
-            }
+        if (evt.getOriginOfDisplayedThumbnails().isFilesMatchingMiscMetadata() && isItemSelected()) {
+            EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(selectedPath, evt.getThumbnailsPanelSettings()));
         }
     }
 
-    private class ShowThumbnails implements Runnable {
+    @EventSubscriber(eventClass = ExifInsertedEvent.class)
+    public void exifInserted(ExifInsertedEvent evt) {
+        if (selectedPath != null) {
+            updateExif(evt.getExif());
+        }
+    }
+
+    @EventSubscriber(eventClass = ExifDeletedEvent.class)
+    public void exifDeleted(ExifDeletedEvent evt) {
+        if (selectedPath != null) {
+            updateExif(evt.getExif());
+        }
+    }
+
+    private boolean updateExif(Exif exif) {
+        SelectedValue selectedValue = getSelectedValue();
+        if (selectedValue == null) {
+            return true;
+        }
+        if (!ExifMetaDataValues.isExifMetadataValue(selectedValue.mdValue)) {
+            return true;
+        }
+        if (exif.getValue(selectedValue.mdValue) != null) {
+            EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(selectedPath, null));
+        }
+        return false;
+    }
+
+    @EventSubscriber(eventClass = XmpInsertedEvent.class)
+    public void xmpInserted(XmpInsertedEvent evt) {
+        if (selectedPath != null) {
+            updateXmp(evt.getXmp());
+        }
+    }
+
+    @EventSubscriber(eventClass = XmpDeletedEvent.class)
+    public void xmpDeleted(XmpDeletedEvent evt) {
+        if (selectedPath != null) {
+            updateXmp(evt.getXmp());
+        }
+    }
+
+    private boolean updateXmp(Xmp xmp) {
+        SelectedValue selectedValue = getSelectedValue();
+        if (selectedValue == null) {
+            return true;
+        }
+        if (!XmpMetaDataValues.isXmpMetaDataValue(selectedValue.mdValue)) {
+            return true;
+        }
+        if (xmp.getValue(selectedValue.mdValue) != null) {
+            EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(selectedPath, null));
+        }
+        return false;
+    }
+
+    @EventSubscriber(eventClass = XmpUpdatedEvent.class)
+    public void xmpUpdated(XmpUpdatedEvent evt) {
+        if (selectedPath == null) {
+            return;
+        }
+        SelectedValue selectedValue = getSelectedValue();
+        if (selectedValue == null) {
+            return;
+        }
+        if (!XmpMetaDataValues.isXmpMetaDataValue(selectedValue.mdValue)) {
+            return;
+        }
+        Xmp oldXmp = evt.getOldXmp();
+        Xmp updatedXmp = evt.getUpdatedXmp();
+        Object oldValue = oldXmp.getValue(selectedValue.mdValue);
+        Object newValue = updatedXmp.getValue(selectedValue.mdValue);
+        boolean valueSelected = !selectedValue.isMetadataCategory && selectedValue.value != null;
+        boolean update = valueSelected && !ObjectUtil.equals(selectedValue.value, newValue)
+                || !ObjectUtil.equals(oldValue, newValue);
+        if (update) {
+            EventQueueUtil.invokeInDispatchThread(new ShowThumbnails(selectedPath, null));
+        }
+    }
+
+    private static final class SelectedValue {
+
+        private final MetaDataValue mdValue;
+        private final boolean isMetadataCategory;
+        private final Object value;
+
+        private SelectedValue(MetaDataValue mdValue, boolean isMetadataCategory, Object value) {
+            this.mdValue = mdValue;
+            this.isMetadataCategory = isMetadataCategory;
+            this.value = value;
+        }
+    }
+
+    private SelectedValue getSelectedValue() {
+        if (selectedPath == null) {
+            return null;
+        }
+        Object lpc = selectedPath.getLastPathComponent();
+        if (!(lpc instanceof DefaultMutableTreeNode)) {
+            return null;
+        }
+        DefaultMutableTreeNode selNode = (DefaultMutableTreeNode) lpc;
+        Object userObject = selNode.getUserObject();
+        Object parentUserObject = ((DefaultMutableTreeNode) selNode.getParent()).getUserObject();
+        boolean isMetadataCategory = userObject instanceof MetaDataValue;
+        boolean containsValue = parentUserObject instanceof MetaDataValue;
+        if (!isMetadataCategory && !containsValue) {
+            return null;
+        }
+        MetaDataValue md5Value = isMetadataCategory
+                ? (MetaDataValue) userObject
+                : (MetaDataValue) parentUserObject;
+        Object value = containsValue
+                ? userObject
+                : null;
+        return new SelectedValue(md5Value, isMetadataCategory, value);
+    }
+
+    private static class ShowThumbnails implements Runnable {
 
         private final ImageFilesRepository repo = Lookup.getDefault().lookup(ImageFilesRepository.class);
         private final ThumbnailsPanelSettings tnPanelSettings;
-        private final TreePath treePath;
+        private final TreePath selectedTreePath;
 
-        ShowThumbnails(TreePath treePath, ThumbnailsPanelSettings settings) {
-            if (treePath == null) {
-                throw new NullPointerException("treePath == null");
-            }
-            this.treePath = treePath;
+        private ShowThumbnails(TreePath treePath, ThumbnailsPanelSettings settings) {
+            this.selectedTreePath = treePath;
             tnPanelSettings = settings;
         }
 
         @Override
         public void run() {
+            if (selectedTreePath == null) {
+                return;
+            }
             WaitDisplayer waitDisplayer = Lookup.getDefault().lookup(WaitDisplayer.class);
             waitDisplayer.show();
-            Object lastPathComponent = treePath.getLastPathComponent();
+            Object lastPathComponent = selectedTreePath.getLastPathComponent();
             setFilesOfPossibleNodeToThumbnailsPanel(lastPathComponent);
             waitDisplayer.hide();
         }
@@ -93,6 +226,7 @@ public final class MiscMetadataItemSelectedController implements TreeSelectionLi
                             userObject.toString()), OriginOfDisplayedThumbnails.FILES_MATCHING_MISC_METADATA);
                     tnPanel.applyThumbnailsPanelSettings(tnPanelSettings);
                 } else {
+                    tnPanel.setFiles(Collections.<File>emptyList(), OriginOfDisplayedThumbnails.FILES_MATCHING_MISC_METADATA);
                     setTitle();
                 }
             } else if (userObject instanceof MetaDataValue) {
@@ -101,7 +235,7 @@ public final class MiscMetadataItemSelectedController implements TreeSelectionLi
                 tnPanel.setFiles(repo.findImageFilesContainingAVauleInMetaDataValue(mdValue), OriginOfDisplayedThumbnails.FILES_MATCHING_MISC_METADATA);
                 tnPanel.applyThumbnailsPanelSettings(tnPanelSettings);
             } else {
-                tnPanel.setFiles(new ArrayList<File>(), OriginOfDisplayedThumbnails.FILES_MATCHING_MISC_METADATA);
+                tnPanel.setFiles(Collections.<File>emptyList(), OriginOfDisplayedThumbnails.FILES_MATCHING_MISC_METADATA);
                 tnPanel.applyThumbnailsPanelSettings(tnPanelSettings);
                 setTitle();
             }
