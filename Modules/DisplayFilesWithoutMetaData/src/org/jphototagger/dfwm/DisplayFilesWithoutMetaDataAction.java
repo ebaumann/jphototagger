53fc763a6e1718daccaa0c8889ccf32db1d10ead
ebaumann <ebaumann@localhost>
2013-01-30 15:20:49 +0100

    Listening to XMP changes

--- Modules/DisplayFilesWithoutMetaData/src/org/jphototagger/dfwm/DisplayFilesWithoutMetaDataAction.java
+++ Modules/DisplayFilesWithoutMetaData/src/org/jphototagger/dfwm/DisplayFilesWithoutMetaDataAction.java
@@ -8,12 +8,17 @@ import org.bushe.swing.event.annotation.EventSubscriber;
 import org.jphototagger.api.windows.MainWindowManager;
 import org.jphototagger.api.windows.WaitDisplayer;
 import org.jphototagger.domain.metadata.MetaDataValue;
+import org.jphototagger.domain.metadata.xmp.Xmp;
 import org.jphototagger.domain.repository.ImageFilesRepository;
+import org.jphototagger.domain.repository.event.xmp.XmpDeletedEvent;
+import org.jphototagger.domain.repository.event.xmp.XmpInsertedEvent;
+import org.jphototagger.domain.repository.event.xmp.XmpUpdatedEvent;
 import org.jphototagger.domain.thumbnails.OriginOfDisplayedThumbnails;
 import org.jphototagger.domain.thumbnails.ThumbnailsDisplayer;
 import org.jphototagger.domain.thumbnails.event.ThumbnailsPanelRefreshEvent;
 import org.jphototagger.lib.util.Bundle;
 import org.jphototagger.lib.util.CollectionUtil;
+import org.jphototagger.lib.util.ObjectUtil;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
@@ -26,6 +31,7 @@ public final class DisplayFilesWithoutMetaDataAction implements LookupListener {
     private static final long serialVersionUID = 1L;
     private final Lookup.Result<? extends MetaDataValue> lookupResult;
     private final ImageFilesRepository repo = Lookup.getDefault().lookup(ImageFilesRepository.class);
+    private final ThumbnailsDisplayer tnDisplayer = Lookup.getDefault().lookup(ThumbnailsDisplayer.class);
     private MetaDataValue selectedMetaDataValue;
 
     public DisplayFilesWithoutMetaDataAction(Lookup lookup) {
@@ -79,5 +85,38 @@ public final class DisplayFilesWithoutMetaDataAction implements LookupListener {
         mainWindowManager.setMainWindowTitle(
                 Bundle.getString(DisplayFilesWithoutMetaDataAction.class, "FilesWithoutMetaDataDisplayAction.MainWindowTitle",
                 selectedMetaDataValue.getDescription()));
+    }
+
+    @EventSubscriber(eventClass = XmpInsertedEvent.class)
+    public void xmpInserted(XmpInsertedEvent evt) {
+        update(evt.getImageFile(), evt.getXmp(), false);
+    }
+
+    @EventSubscriber(eventClass = XmpDeletedEvent.class)
+    public void xmpDeleted(XmpDeletedEvent evt) {
+        update(evt.getImageFile(), evt.getXmp(), true);
+    }
+
+    private void update(File imageFile, Xmp xmp, boolean deleted) {
+        if (selectedMetaDataValue == null || tnDisplayer == null) {
+            return;
+        }
+        boolean containsMdv = !deleted && xmp.contains(selectedMetaDataValue);
+        boolean imageDisplayed = tnDisplayer.isDisplayFile(imageFile);
+        if (imageDisplayed && containsMdv || !imageDisplayed && !containsMdv) {
+            displaySelectedMetaDataValue();
+        }
+    }
+
+    @EventSubscriber(eventClass = XmpUpdatedEvent.class)
+    public void xmpUpdated(XmpUpdatedEvent evt) {
+        if (selectedMetaDataValue == null) {
+            return;
+        }
+        Object oldValue = evt.getOldXmp().getValue(selectedMetaDataValue);
+        Object updatedValue = evt.getUpdatedXmp().getValue(selectedMetaDataValue);
+        if (!ObjectUtil.equals(oldValue, updatedValue)) {
+            displaySelectedMetaDataValue();
+        }
     }
 }
