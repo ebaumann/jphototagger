1ebc60645765efb86585b9707b5904139cf81053
ebaumann <ebaumann@localhost>
2013-02-12 15:49:55 +0100

    Formatting zero or less with "?" (e.g. a f-stop or focal length of zero or less does not make sense)

--- Exif/src/org/jphototagger/exif/formatter/ExifFormatterFocalLength.java
+++ Exif/src/org/jphototagger/exif/formatter/ExifFormatterFocalLength.java
@@ -4,8 +4,8 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import org.jphototagger.exif.Ensure;
 import org.jphototagger.exif.ExifTag;
-import org.jphototagger.exif.datatype.ExifValueUtil;
 import org.jphototagger.exif.datatype.ExifRational;
+import org.jphototagger.exif.datatype.ExifValueUtil;
 
 /**
  * Formats an EXIF entry of the dataType {@code ExifTag.Properties#FOCAL_LENGTH}.
@@ -16,28 +16,25 @@ public final class ExifFormatterFocalLength extends ExifFormatter {
 
     public static final ExifFormatterFocalLength INSTANCE = new ExifFormatterFocalLength();
 
-    private ExifFormatterFocalLength() {
-    }
-
     @Override
     public String format(ExifTag exifTag) {
         if (exifTag == null) {
             throw new NullPointerException("exifTag == null");
         }
-
         Ensure.exifTagId(exifTag, ExifTag.Properties.FOCAL_LENGTH);
-
         byte[] rawValue = exifTag.getRawValue();
-
         if (ExifRational.isValid(rawValue, exifTag.convertByteOrderIdToByteOrder())) {
             ExifRational er = new ExifRational(rawValue, exifTag.convertByteOrderIdToByteOrder());
+            if (ExifValueUtil.convertExifRationalToDouble(er) <= 0) {
+                return "?";
+            }
             DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance();
-
             df.applyPattern("#.# mm");
-
             return df.format(ExifValueUtil.convertExifRationalToDouble(er));
         }
-
         return "?";
+    }
+
+    private ExifFormatterFocalLength() {
     }
 }
