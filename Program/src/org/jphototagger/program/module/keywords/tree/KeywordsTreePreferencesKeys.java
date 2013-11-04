6e11ecbf281e9de129088c025a137f4b864f48b5
ebaumann <ebaumann@localhost>
2013-11-04 18:45:44 +0100

    Added option to disable automatic population of the Hierarchical keywords tree

---
+++ Program/src/org/jphototagger/program/module/keywords/tree/KeywordsTreePreferencesKeys.java
@@ -0,0 +1,12 @@
+package org.jphototagger.program.module.keywords.tree;
+
+/**
+ * @author Elmar Baumann
+ */
+public final class KeywordsTreePreferencesKeys {
+
+    public static final String KEY_AUTO_INSERT_UNKNOWN_KEYWORDS = "KeywordsTree.AutoInsertUnknownKeywords";
+
+    private KeywordsTreePreferencesKeys() {
+    }
+}
