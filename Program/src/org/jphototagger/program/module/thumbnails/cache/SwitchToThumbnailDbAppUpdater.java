0851d9d494af36465868d10b8fa6eae5a4d25fb8
ebaumann <ebaumann@localhost>
2014-01-04 14:40:19 +0100

    Moved PersistentThumbnails as inner class to SwitchToThumbnailDbAppUpdater (shouldn't be used elsewhere)

--- Program/src/org/jphototagger/program/module/thumbnails/cache/SwitchToThumbnailDbAppUpdater.java
+++ Program/src/org/jphototagger/program/module/thumbnails/cache/SwitchToThumbnailDbAppUpdater.java
@@ -2,8 +2,10 @@ package org.jphototagger.program.module.thumbnails.cache;
 
 import java.awt.Image;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import org.jphototagger.api.applifecycle.AppUpdater;
 import org.jphototagger.api.applifecycle.generics.Functor;
 import org.jphototagger.api.progress.ProgressEvent;
@@ -11,7 +13,10 @@ import org.jphototagger.api.progress.ProgressHandle;
 import org.jphototagger.api.progress.ProgressHandleFactory;
 import org.jphototagger.domain.repository.ApplicationPropertiesRepository;
 import org.jphototagger.domain.repository.ImageFilesRepository;
+import org.jphototagger.domain.thumbnails.ThumbnailsDirectoryProvider;
 import org.jphototagger.lib.awt.EventQueueUtil;
+import org.jphototagger.lib.io.FileUtil;
+import org.jphototagger.lib.io.IoUtil;
 import org.jphototagger.lib.swing.MessageDisplayer;
 import org.jphototagger.lib.util.Bundle;
 import org.openide.util.Lookup;
@@ -124,5 +129,92 @@ public final class SwitchToThumbnailDbAppUpdater implements AppUpdater {
 
     private String getProgressMessage(int filesProcessedCount, int fileCount) {
         return Bundle.getString(SwitchToThumbnailDbAppUpdater.class, "SwitchToThumbnailDbAppUpdater.ProgressMessage", filesProcessedCount, fileCount);
+    }
+
+    /**
+     * Persistent stored (cached) thumbnails.
+     *
+     * @author Martin Pohlack, Elmar Baumann
+     */
+    private static final class PersistentThumbnails {
+
+        private static final Logger LOGGER = Logger.getLogger(PersistentThumbnails.class.getName());
+        private static final String THUMBNAILS_DIRECTORY_NAME;
+
+        static {
+            ThumbnailsDirectoryProvider provider = Lookup.getDefault().lookup(ThumbnailsDirectoryProvider.class);
+            File thumbnailsDirectory = provider.getThumbnailsDirectory();
+            THUMBNAILS_DIRECTORY_NAME = thumbnailsDirectory.getAbsolutePath();
+        }
+
+        private PersistentThumbnails() {
+        }
+
+        static boolean deleteThumbnail(File imageFile) {
+            if (imageFile == null) {
+                throw new NullPointerException("imageFile == null");
+            }
+            File tnFile = getThumbnailFile(imageFile);
+            if ((tnFile != null) && !tnFile.delete()) {
+                LOGGER.log(Level.WARNING,
+                        "Thumbnail ''{0}'' of image file ''{1}'' couldn''t be deleted!",
+                        new Object[]{tnFile, imageFile});
+                return false;
+            }
+
+            return true;
+        }
+
+        private static Image getThumbnail(String md5Filename) {
+            Image thumbnail = null;
+            FileInputStream fis = null;
+            try {
+                File tnFile = getThumbnailfile(md5Filename);
+                if (tnFile == null) {
+                    return null;
+                }
+                if (tnFile.exists()) {
+                    fis = new FileInputStream(tnFile);
+                    int bytecount = fis.available();
+                    byte[] bytes = new byte[bytecount];
+                    fis.read(bytes, 0, bytecount);
+                    ImageIcon icon = new ImageIcon(bytes);
+                    thumbnail = icon.getImage();
+                }
+            } catch (Throwable t) {
+                LOGGER.log(Level.SEVERE, null, t);
+            } finally {
+                IoUtil.close(fis);
+            }
+            return thumbnail;
+        }
+
+        static Image getThumbnail(File imageFile) {
+            if (imageFile == null) {
+                throw new NullPointerException("imageFile == null");
+            }
+            String md5Filename = FileUtil.getMd5FilenameOfAbsolutePath(imageFile);
+            return getThumbnail(md5Filename);
+        }
+
+        private static File getThumbnailFile(File imageFile) {
+            if (imageFile == null) {
+                throw new NullPointerException("imageFile == null");
+            }
+            String md5Filename = FileUtil.getMd5FilenameOfAbsolutePath(imageFile);
+            return (md5Filename == null)
+                    ? null
+                    : getThumbnailfile(md5Filename);
+        }
+
+        /**
+         * Compute final name of thumbnail on disk.
+         *
+         * @param md5Filename hash
+         * @return thumbnail file
+         */
+        private static File getThumbnailfile(String md5Filename) {
+            return new File(THUMBNAILS_DIRECTORY_NAME + File.separator + md5Filename + ".jpeg");
+        }
     }
 }
