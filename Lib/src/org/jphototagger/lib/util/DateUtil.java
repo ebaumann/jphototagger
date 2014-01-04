0974124a9c034b0ff2c182d3112b4c0b67e5b650
ebaumann <ebaumann@localhost>
2014-01-04 15:37:42 +0100

    Auto fixed IDE warning hints: Utility class w/o constructor

--- Lib/src/org/jphototagger/lib/util/DateUtil.java
+++ Lib/src/org/jphototagger/lib/util/DateUtil.java
@@ -51,4 +51,7 @@ public final class DateUtil {
     public static boolean isLeapYear(int year) {
         return year > 0 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
     }
+
+    private DateUtil() {
+    }
 }
