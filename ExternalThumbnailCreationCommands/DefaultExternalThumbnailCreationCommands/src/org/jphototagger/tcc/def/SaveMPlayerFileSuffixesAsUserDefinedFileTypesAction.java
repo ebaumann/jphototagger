3316aee9c05fa4270f4d6f2e9a237b6cb650692f
elmar <elmar@PC-Arbeitszimmer.fritz.box>
2015-08-20 17:12:04 +0200

    Bugfix: Automatic creation of "User defined File Types" did not add any file type

--- ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.java
+++ ExternalThumbnailCreationCommands/DefaultExternalThumbnailCreationCommands/src/org/jphototagger/tcc/def/SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.java
@@ -12,32 +12,35 @@ import org.jphototagger.lib.util.Bundle;
 public final class SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction extends SaveFileSuffixesAsUserDefinedFileTypesAction {
 
     private static final long serialVersionUID = 1L;
-    private static final Collection<UserDefinedFileType> FILE_TYPES = new ArrayList<>();
     public static final SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction INSTANCE = new SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction();
 
-    {
+    private static Collection<UserDefinedFileType> createFileTypes() {
+        Collection<UserDefinedFileType> fileTypes = new ArrayList<>();
+
         // Videos
-        FILE_TYPES.add(createUserDefinedFileType("avi", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.avi.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("flv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.flv.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("m2ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.m2ts.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mkv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mkv.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mov", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mov.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mp2", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp2.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mp4", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp4.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mpeg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpeg.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mpg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpg.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("mts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mts.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.ts.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("wmv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.wmv.Description")));
+        fileTypes.add(createUserDefinedFileType("avi", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.avi.Description")));
+        fileTypes.add(createUserDefinedFileType("flv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.flv.Description")));
+        fileTypes.add(createUserDefinedFileType("m2ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.m2ts.Description")));
+        fileTypes.add(createUserDefinedFileType("mkv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mkv.Description")));
+        fileTypes.add(createUserDefinedFileType("mov", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mov.Description")));
+        fileTypes.add(createUserDefinedFileType("mp2", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp2.Description")));
+        fileTypes.add(createUserDefinedFileType("mp4", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mp4.Description")));
+        fileTypes.add(createUserDefinedFileType("mpeg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpeg.Description")));
+        fileTypes.add(createUserDefinedFileType("mpg", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mpg.Description")));
+        fileTypes.add(createUserDefinedFileType("mts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.mts.Description")));
+        fileTypes.add(createUserDefinedFileType("ts", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.ts.Description")));
+        fileTypes.add(createUserDefinedFileType("wmv", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.wmv.Description")));
 
         // Images not handled by JPhotoTagger
-        FILE_TYPES.add(createUserDefinedFileType("bmp", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.bmp.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("ttf", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.ttf.Description")));
-        FILE_TYPES.add(createUserDefinedFileType("xcf", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.xcf.Description")));
+        fileTypes.add(createUserDefinedFileType("bmp", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.bmp.Description")));
+        fileTypes.add(createUserDefinedFileType("ttf", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.ttf.Description")));
+        fileTypes.add(createUserDefinedFileType("xcf", Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "FileType.xcf.Description")));
+
+        return fileTypes;
     }
 
     private SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction() {
-        super(FILE_TYPES);
+        super(createFileTypes());
         putValue(Action.NAME, Bundle.getString(SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.class, "SaveMPlayerFileSuffixesAsUserDefinedFileTypesAction.Name"));
     }
-                }
+}
