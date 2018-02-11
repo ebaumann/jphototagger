a87e42688058bc6c271d3a0bb594df2ad1825ef6
Elmar Baumann <eb@elmar-baumann.de>
2018-02-11 17:56:35 +0100

    Added icon to ExportGPSToKMLFileProcessorPlugin.

--- Exif/src/org/jphototagger/exif/kmlexport/ExportGPSToKMLFileProcessorPlugin.java
+++ Exif/src/org/jphototagger/exif/kmlexport/ExportGPSToKMLFileProcessorPlugin.java
@@ -3,18 +3,17 @@ package org.jphototagger.exif.kmlexport;
 import java.awt.Component;
 import java.io.File;
 import java.util.Collection;
+import javax.swing.Icon;
 import org.jphototagger.api.plugin.fileprocessor.FileProcessorPlugin;
-import org.jphototagger.lib.plugin.AbstractFileProcessorPlugin;
 import org.jphototagger.lib.util.Bundle;
+import org.jphototagger.resources.Icons;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
  * @author Elmar Baumann
  */
 @ServiceProvider(service = FileProcessorPlugin.class, position = 10000)
-public final class ExportGPSToKMLFileProcessorPlugin extends AbstractFileProcessorPlugin {
-
-    private static final long serialVersionUID = 1L;
+public final class ExportGPSToKMLFileProcessorPlugin implements FileProcessorPlugin {
 
     @Override
     public void processFiles(Collection<? extends File> files) {
@@ -41,5 +40,15 @@ public final class ExportGPSToKMLFileProcessorPlugin extends AbstractFileProcess
     @Override
     public Component getSettingsComponent() {
         return new ExportGPSToKMLSettingsPanel();
+    }
+
+    @Override
+    public Icon getSmallIcon() {
+        return Icons.getIcon("icon_gps.png");
+    }
+
+    @Override
+    public Icon getLargeIcon() {
+        return Icons.getIcon("icon_gps-48.png");
     }
 }
