8b4f72bca11b8c712c88245659cb36b13e70fd3b
Elmar Baumann <eb@elmar-baumann.de>
2018-12-01 23:13:54 +0100

    Increased maximum thumbnail size from 400 pixels per side to 800 pixels.

--- Image/src/org/jphototagger/image/thumbnail/ThumbnailDefaults.java
+++ Image/src/org/jphototagger/image/thumbnail/ThumbnailDefaults.java
@@ -6,6 +6,6 @@ package org.jphototagger.image.thumbnail;
 public interface ThumbnailDefaults {
 
     public static final int MIN_THUMBNAIL_WIDTH = 50;
-    public static final int MAX_THUMBNAIL_WIDTH = 400;
+    public static final int MAX_THUMBNAIL_WIDTH = 800;
     public static final int DEFAULT_THUMBNAIL_WIDTH = 150;
 }
