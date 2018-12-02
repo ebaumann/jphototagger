b31fbd0ee6aa5c15a71bb698dc003661d5b613d5
Elmar Baumann <eb@elmar-baumann.de>
2018-12-02 18:46:51 +0100

    Added ComponentUtil#registerForSetUnvisibleOnEscape()

--- Lib/src/org/jphototagger/lib/swing/util/ComponentUtil.java
+++ Lib/src/org/jphototagger/lib/swing/util/ComponentUtil.java
@@ -8,6 +8,7 @@ import java.awt.Frame;
 import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -16,6 +17,7 @@ import java.util.Objects;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.KeyStroke;
 
 /**
@@ -263,6 +265,34 @@ public final class ComponentUtil {
                 JComponent jComponent = (JComponent) component;
                 jComponent.registerKeyboardAction(escapeAction, strokeEscape, JComponent.WHEN_IN_FOCUSED_WINDOW);
             }
+        }
+    }
+
+    public static void registerForSetUnvisibleOnEscape(JDialog dialog) {
+        Objects.requireNonNull(dialog, "dialog == null");
+
+        KeyStroke keyStrokeEscape = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+        EscapeActionListener escapeActionListener = new EscapeActionListener(dialog);
+
+        for (Component component : dialog.getComponents()) {
+            if (component instanceof JComponent) {
+                JComponent jComponent = (JComponent) component;
+                jComponent.registerKeyboardAction(escapeActionListener, keyStrokeEscape, JComponent.WHEN_IN_FOCUSED_WINDOW);
+            }
+        }
+    }
+
+    private static final class EscapeActionListener implements ActionListener {
+
+        private final JDialog dialog;
+
+        private EscapeActionListener(JDialog dialog) {
+            this.dialog = dialog;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dialog.setVisible(false);
         }
     }
 
